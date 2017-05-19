1f035789726c3945e56052d3cb7af8043ae74d63
Aaron Hopkinson <aaron.hopkinson@metoffice.gov.uk>
2017-05-19 13:29:25 +0100

    Added napoleon plugin and added lib/improver to path for Sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,10 +15,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../lib/improver/'))
 
 # -- General configuration ------------------------------------------------
 
@@ -31,6 +31,7 @@
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
