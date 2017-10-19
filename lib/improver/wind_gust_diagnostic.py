5d207d938545e88aa1b63a645fe9de08b358b19d
Caroline Jones <caroline.jones@metoffice.gov.uk>
2017-10-19 08:22:37 +0100

    Adding changes

--- lib/improver/wind_gust_diagnostic.py
+++ lib/improver/wind_gust_diagnostic.py
@@ -28,7 +28,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""Module containing plugin for WindGustDiagnositic."""
+"""Module containing plugin for WindGustDiagnostic."""
 
 import warnings
 
