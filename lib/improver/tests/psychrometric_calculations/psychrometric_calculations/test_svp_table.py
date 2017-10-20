613d52cafdc900fefb21cbcf5922846de6df8b19
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2017-10-20 10:50:10 +0100

    Additional unit tests requested by reviewer.

---
+++ lib/improver/tests/psychrometric_calculations/psychrometric_calculations/test_svp_table.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Unit tests for saturated vapour pressure table."""
+
+import unittest
+import numpy as np
+from iris.cube import Cube
+from iris.tests import IrisTest
+from iris.coords import DimCoord
+from cf_units import Unit
+from improver.psychrometric_calculations import svp_table
+from improver.utilities.ancillary_creation import SaturatedVapourPressureTable
+
+
+class Test_svp_table(IrisTest):
+
+    """Test that the values in the static svp_table are as expected,
+    agreeing with values produced by the creation plugin. Does this piecewise
+    to avoid the testing being too slow."""
+
+    def test_cube_values_bottom(self):
+        """Test the lower end of the SVP table"""
+        t_min, t_max, t_increment = 183.15, 185.15, 0.1
+        expected = svp_table.DATA[0:21]
+        result = SaturatedVapourPressureTable(
+            T_min=t_min, T_max=t_max, T_increment=t_increment).process()
+
+        self.assertArrayAlmostEqual(result.data, expected)
+
+    def test_cube_values_middle(self):
+        """Test the middle of the SVP table"""
+        t_min, t_max, t_increment = 273.15, 275.15, 0.1
+        expected = svp_table.DATA[900:921]
+        result = SaturatedVapourPressureTable(
+            T_min=t_min, T_max=t_max, T_increment=t_increment).process()
+
+        np.testing.assert_allclose(result.data, expected, rtol=1.e-5)
+
+    def test_cube_values_top(self):
+        """Test the upper end of the SVP table"""
+        t_min, t_max, t_increment = 336.15, 338.15, 0.1
+        expected = svp_table.DATA[1530:]
+        result = SaturatedVapourPressureTable(
+            T_min=t_min, T_max=t_max, T_increment=t_increment).process()
+
+        np.testing.assert_allclose(result.data, expected, rtol=1.e-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ lib/improver/tests/utilities/ancillary_creation/test_SaturatedVapourPressureTable.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""
+Unit tests for the SaturatedVapourPressureTable utility.
+
+"""
+import unittest
+import warnings
+
+from cf_units import Unit
+import iris
+from iris.tests import IrisTest
+import numpy as np
+
+from improver.utilities.ancillary_creation import SaturatedVapourPressureTable
+
+
+class Test__repr__(IrisTest):
+
+    """Test the repr method."""
+
+    def test_basic(self):
+        """Test that the __repr__ returns the expected string."""
+        result = str(SaturatedVapourPressureTable())
+        msg = ('<SaturatedVapourPressureTable: T_min: {}; T_max: {}; '
+               'T_increment: {}>'.format(183.15, 338.15, 0.1))
+        self.assertEqual(result, msg)
+
+
+class Test_process(IrisTest):
+
+    """Test that the plugin functions as expected."""
+
+    def test_cube_attributes(self):
+        """Test that returned cube has appropriate attributes."""
+        t_min, t_max, t_increment = 200.15, 220.15, 10.
+        result = SaturatedVapourPressureTable(
+            T_min=t_min, T_max=t_max, T_increment=t_increment).process()
+        self.assertEqual(result.attributes['minimum_temperature'], t_min)
+        self.assertEqual(result.attributes['maximum_temperature'], t_max)
+        self.assertEqual(result.attributes['temperature_increment'],
+                         t_increment)
+        self.assertEqual(result.units, Unit('Pa'))
+
+    def test_cube_values(self):
+        """Test that returned cube has appropriate values."""
+        t_min, t_max, t_increment = 183.15, 338.15, 10.
+        expected = [0.0096646, 0.0546844, 0.2613554, 1.0799927, 3.9333663,
+                    12.8286096, 37.9714586, 103.1532749, 259.6617372,
+                    610.6359361, 1227.0888425, 2337.0801979, 4242.7259947,
+                    7377.3294046, 12338.9996048, 19925.4362844]
+        result = SaturatedVapourPressureTable(
+            T_min=t_min, T_max=t_max, T_increment=t_increment).process()
+
+        self.assertArrayAlmostEqual(result.data, expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
