f0b0bf8a7ae3e777f61b469f3757ac16accdccdd
Gavin Evans <gavin.evans@metoffice.gov.uk>
2017-12-15 14:19:53 +0000

    Correction to spelling.

--- lib/improver/tests/utilities/test_load.py
+++ lib/improver/tests/utilities/test_load.py
@@ -216,7 +216,7 @@ class Test_load_cubelist(IrisTest):
 
     def test_wildcard_files_with_constraint(self):
         """Test that the loading works correctly, if a wildcarded filepath is
-        provided and a constraint is provide that is only valid for a subset
+        provided and a constraint is provided that is only valid for a subset
         of the available files."""
         low_cloud_cube = self.cube.copy()
         low_cloud_cube.rename("low_type_cloud_area_fraction")
