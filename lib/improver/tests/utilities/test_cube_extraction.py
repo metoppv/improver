1f85db5b4702d6fb4dec780c505434af0be82dfc
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2018-02-15 17:02:29 +0000

    Moved functions out into a cube_extraction utility module and started writing unit tests.

---
+++ lib/improver/tests/utilities/test_cube_extraction.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+""" Unit tests for cube extraction utilities """
+
+import unittest
+import warnings
+import os
+import numpy as np
+from tempfile import mkdtemp
+from subprocess import call
+
+import iris
+from iris.tests import IrisTest
+from iris.coords import DimCoord
+
+from improver.utilities.cube_extraction import (parse_constraint_list,
+                                                extract_subcube)
+
+
+def set_up_precip_probability_cube():
+    """
+    Set up a cube with spatial probabilities of precipitation at three
+    exceedance thresholds
+    """
+    data = np.array([[[0.85, 0.95, 0.73],
+                      [0.75, 0.85, 0.65],
+                      [0.70, 0.80, 0.62]],
+                     [[0.18, 0.20, 0.15],
+                      [0.11, 0.16, 0.09],
+                      [0.10, 0.14, 0.03]],
+                     [[0.03, 0.04, 0.01],
+                      [0.02, 0.02, 0.00],
+                      [0.01, 0.00, 0.00]]])
+
+    mmh_to_ms = 0.001/3600.    
+    threshold = DimCoord(mmh_to_ms*np.array([0.03, 0.1, 1.0]),
+                         long_name="threshold", units="m s-1")
+    ycoord = DimCoord(np.arange(3), "projection_y_coordinate")
+    xcoord = DimCoord(np.arange(3), "projection_x_coordinate")
+
+    cube = iris.cube.Cube(data, long_name="probability_of_precipitation",
+                          dim_coords_and_dims=[(threshold, 0), (ycoord, 1), 
+                                               (xcoord, 2)], units="1")
+    return cube
+
+
+class Test_parse_constraint_list(IrisTest):
+
+    def setUp(self):
+        self.constraints = ["percentile=10", "threshold=0.1"]
+
+    def test_basic_no_units(self):
+        """ Test simple key-value splitting with no units """
+        cdict, udict = parse_constraint_list(self.constraints, None)
+        self.assertEqual(cdict["percentile"], 10)
+        self.assertEqual(cdict["threshold"], 0.1)
+        self.assertFalse(udict)
+
+    def test_some_units(self):
+        """ Test units list containing "None" elements is correctly parsed """
+        units = ["None", "mm h-1"]
+        cdict, udict = parse_constraint_list(self.constraints, units)
+        self.assertEqual(udict["threshold"], "mm h-1")
+        with self.assertRaises(KeyError):
+            udict["percentile"]
+
+    def test_unmatched_units(self):
+        """ Test for ValueError if units list does not match constraints """
+        units = ["mm h-1"]
+        msg = "units list must match constraints"
+        with self.assertRaisesRegexp(ValueError, msg):
+            parse_constraint_list(self.constraints, units)
+
+
+class Test_extract_subcube(IrisTest):
+
+    def setUp(self):
+        """ Save temporary input cube to file """
+        self.precip_cube = set_up_precip_probability_cube()
+        self.directory = mkdtemp()
+        self.filepath = os.path.join(self.directory, 'tmp_precip_cube.nc')
+        iris.save(self.precip_cube, self.filepath)
+
+    def tearDown(self):
+        """ Remove temporary path created for testing """
+        call(['rm', '-f', self.filepath])
+        call(['rmdir', self.directory])
+
+    def test_basic_no_units(self):
+        """ Test cube extraction for single constraint without units """
+        constraint_dict = {"name" : "probability_of_precipitation"}
+        cube = extract_subcube(self.filepath, constraint_dict, None)
+        self.assertTrue(isinstance(cube, iris.cube.Cube))
+        reference_data = self.precip_cube.data
+        self.assertTrue(np.array_equal(cube.data, reference_data))
+
+    def test_basic_with_units(self):
+        """ Test cube extraction for single constraint with units """
+        constraint_dict = {"threshold" : 0.1}
+        units_dict = {"threshold" : "mm h-1"}
+        cube = extract_subcube(self.filepath, constraint_dict, units_dict)
+        self.assertTrue(isinstance(cube, iris.cube.Cube))
+        self.assertEqual(cube.coord("threshold").units, "mm h-1")
+        reference_data = self.precip_cube.data[1,:,:]
+        self.assertTrue(np.array_equal(cube.data, reference_data))
+
+    def test_raises_mismatch_error(self):
+        """ Test error is raised if the output cube is empty """
+        constraint_dict = {"threshold" : 6}
+        msg = "can't merge an empty CubeList"
+        with self.assertRaisesRegexp(ValueError, msg):
+            extract_subcube(self.filepath, constraint_dict, None)
+        
+
+    # test list of constraints with mixed units
+
+
+
+
+    # test error raised for single non-coord constraint with units
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ lib/improver/utilities/cube_extraction.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+""" Utilities to parse a list of constraints and extract matching subcube """
+
+import numpy as np
+from ast import literal_eval
+
+import iris
+
+iris.FUTURE.netcdf_no_unlimited = True
+iris.FUTURE.netcdf_promote = True
+
+
+def parse_constraint_list(constraints, units):
+    """
+    Takes a list of string constraints and converts to key-value pairs
+
+    Args:
+        constraints (list):
+            Space separated list of constraints with no space between key and
+            value in each pair: e.g: kw1=val1 kw2=val2 kw3=val3
+        units (list or None):
+            Space separated list of units for each coordinate in the list of
+            constraints.  One or more "units" may be None, and units can only
+            be associated with coordinate constraints.
+
+    Returns a dictionary of constraints and units
+    """
+
+    if units is None:
+        list_units = len(constraints)*[None]
+        units_dict = None
+    else:
+        if len(units) != len(constraints):
+            msg = 'units list must match constraints'
+            raise ValueError(msg)
+        list_units = units
+        units_dict = {}
+
+    constraints_dict = {}
+    for cons_pair, units in zip(constraints, list_units):
+        [key, value] = cons_pair.split('=')
+
+        # Not exactly a great way of doing this, but let's try using
+        # literal_eval to guess the type of the value...
+        try:
+            parsed_value = literal_eval(value)
+        except:
+            # if it doesn't work, just store the original string...
+            parsed_value = value
+
+        constraints_dict[key] = parsed_value
+        if units is not None and units != 'None':
+            units_dict[key] = units
+
+    return constraints_dict, units_dict
+
+
+def extract_subcube(input_filename, constraints, units):
+    """
+    Using a set of constraints, extract a subcube from the provided cube or
+    cubelist if it is available. Returns a single merged cube or raises
+    iris.ConstraintMismatchError.
+
+    Args:
+        cube (iris.cube.Cube or iris.cube.CubeList):
+            The cube or cubelist from which a subcube is to be extracted.
+        constraints (dictionary):
+            A dictionary of constraints that define the subcube to be
+            extracted.
+
+    Kwargs:
+        units (dictionary):
+            A dictionary of units for the constraints.  Supplied if units are
+            different from those of the input cube (eg precip in mm/h for cube
+            data in m/s).
+
+    """
+    constraint = iris.Constraint(**constraints)
+    cube = None
+
+    if units is not None:
+        cubes = iris.load(input_filename)
+        for cube in cubes:
+            for coord in units.keys():
+                cube.coord(coord).convert_units(units[coord])
+        cubes = cubes.extract(constraint)     
+    else:
+        cubes = iris.load(input_filename, constraint)
+    cube = cubes.merge_cube()
+
+    return cube
+
