e14843894a9d777a82df32e0c6435b2fdded2d48
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2018-02-20 09:33:55 +0000

    Updated tests in response to review.

--- lib/improver/tests/utilities/test_save.py
+++ lib/improver/tests/utilities/test_save.py
@@ -43,27 +43,22 @@ from iris.fileformats.cf import CFReader
 
 from improver.utilities.load import load_cube
 from improver.utilities.save import save_netcdf
+from improver.tests.ensemble_calibration.ensemble_calibration.\
+    helper_functions import set_up_cube
 
 
 def set_up_test_cube():
-    """Create a cube with metadata and values suitable for air temperature."""
-    data = (np.linspace(-45.0, 45.0, 9).reshape(1, 3, 3) + 273.15)
-    realization = DimCoord(1, "realization")
-    y_coord = DimCoord(np.linspace(-45.0, 45.0, 3),
-                       'latitude', units='degrees')
-    x_coord = DimCoord(np.linspace(120, 180, 3),
-                       'longitude', units='degrees')
-    attributes = {'Conventions': 'CF-1.5', 'source_realizations': 12}
-    cube = iris.cube.Cube(data, 'air_temperature', units='K',
-                          attributes=attributes,
-                          dim_coords_and_dims=[(realization, 0), (y_coord, 1),
-                                               (x_coord, 2)])
+    """ Set up a temperature cube with additional global attributes. """
+    data = (np.linspace(-45.0, 45.0, 9).reshape(1, 1, 3, 3) + 273.15)
+    cube = set_up_cube(data, 'air_temperature', 'K', realizations=([0]))
+    cube.attributes['Conventions'] = 'CF-1.5'
+    cube.attributes['source_realizations'] = np.arange(12)
     return cube
 
 
 class Test_save_netcdf(IrisTest):
 
-    """ Test function to save iris cubes as netcdf. """
+    """ Test function to save iris cubes as NetCDF files. """
 
     def setUp(self):
         """ Set up cube to write, read and check """
@@ -78,18 +73,18 @@ class Test_save_netcdf(IrisTest):
         call(['rm', '-f', self.filepath])
         call(['rmdir', self.directory])
 
-    def test_basic(self):
+    def test_basic_cube(self):
         """ Test saves file in required location """
         self.assertFalse(os.path.exists(self.filepath))
         save_netcdf(self.cube, self.filepath)
         self.assertTrue(os.path.exists(self.filepath))
 
-    def test_cube_list(self):
+    def test_basic_cube_list(self):
         """ Test functionality for saving iris.cube.CubeList """
         cube_list = ([self.cube, self.cube])
         save_netcdf(cube_list, self.filepath)
         read_cubes = iris.load(self.filepath)
-        self.assertTrue(isinstance(read_cubes, iris.cube.CubeList))
+        self.assertIsInstance(read_cubes, iris.cube.CubeList)
         self.assertEqual(len(read_cubes), 2)
 
     def test_cube_data(self):
@@ -109,18 +104,27 @@ class Test_save_netcdf(IrisTest):
         self.assertItemsEqual(coord_names, reference_names)
 
     def test_cf_global_attributes(self):
-        """
-        Test that the saved NetCDF file only contains the expected global
-        attributes.
+        """ Test that a NetCDF file saved from one cube only contains the
+        expected global attributes.
 
         NOTE Loading the file as an iris.cube.Cube does not distinguish global
         from local attributes, and therefore cannot test for the correct
         behaviour here.
         """
         save_netcdf(self.cube, self.filepath)
-        cube_keys = CFReader(self.filepath).cf_group.global_attributes.keys()
+        global_keys = CFReader(self.filepath).cf_group.global_attributes.keys()
         self.assertTrue(all(key in self.global_keys_ref
-                            for key in cube_keys))
+                            for key in global_keys))
+
+    def test_cf_shared_attributes_list(self):
+        """ Test that a NetCDF file saved from a list of cubes that share
+        non-global attributes does not promote these attributes to global.
+        """
+        cube_list = ([self.cube, self.cube])
+        save_netcdf(cube_list, self.filepath)
+        global_keys = CFReader(self.filepath).cf_group.global_attributes.keys()
+        self.assertTrue(all(key in self.global_keys_ref
+                            for key in global_keys))
 
 
 if __name__ == '__main__':
