Moved gradient calculation out into DifferenceBetweenGridSquares plugin.  No unit tests as yet.  Need to correct metadata on gradient cube.  Fixed PEP8 compliance.

lib/improver/utilities/ancillary_creation.py
@@ -31,12 +31,17 @@
 
 import iris
 import numpy as np
-from improver.psychrometric_calculations.psychrometric_calculations import (
-    Utilities)
-from improver.utilities.spatial import (DifferenceBetweenAdjacentGridSquares)
+from improver.psychrometric_calculations.psychrometric_calculations import \
+    Utilities
+from improver.utilities.spatial import DifferenceBetweenAdjacentGridSquares
 
 
 class OrographicAlphas(object):
+
+    """
+    Class to generate alpha smoothing parameters for recursive filtering
+    based on orography gradients.
+    """
 
     def __init__(self, min_alpha=0., max_alpha=1., coefficient=1, power=1,
                  intercept=0, invert_alphas=True):
@@ -73,34 +78,6 @@ class OrographicAlphas(object):
                                 self.invert_alphas))
 
         return result
-
-    def difference_to_gradient(self, cube_x, cube_y):
-        """
-        This uses the grid spacing with difference in height fields
-        to calculate the gradient between grid spaces.
-
-        Args:
-            cube_x : iris.cube.Cube
-                The difference in height between adjacent grid squares
-                in the x direction.
-            cube_y: iris.cube.Cube
-                The difference in height between adjacent grid squares
-                in the y direction.
-
-         Returns:
-            gradient_x : iris.cube.Cube
-               A cube of the gradients based on orography in the x
-               direction.
-            gradient_y : iris.cube.Cube
-               A cube of the gradients based on orography in the y
-               direction.
-        """
-        grid_space_x = np.diff(cube_x.coord(axis='x').points)[0]
-        grid_space_y = np.diff(cube_y.coord(axis='y').points)[0]
-        gradient_x = cube_x.copy(data=abs(cube_x.data / grid_space_x))
-        gradient_y = cube_y.copy(data=abs(cube_y.data / grid_space_y))
-
-        return gradient_x, gradient_y
 
     def normalise_cube(self, cubes, min_output_value=0, max_output_value=1):
         """
@@ -173,11 +150,8 @@ class OrographicAlphas(object):
                A cube of orographic dependent alphas calculated in the y
                direction.
         """
-        ukvx_x, ukvx_y = DifferenceBetweenAdjacentGridSquares().process(cube)
-        gradient_x, gradient_y = self.difference_to_gradient(ukvx_x, ukvx_y)
-        gradient_x = gradient_x.regrid(cube, iris.analysis.Linear())
-        gradient_y = gradient_y.regrid(cube, iris.analysis.Linear())
-        gradient_x, gradient_y = self.normalise_cube([gradient_x, gradient_y])
+        gradient_x, gradient_y = \
+            DifferenceBetweenAdjacentGridSquares().process(cube, gradient=True)
         alpha_x = self.scale_alpha_values(gradient_x)
         alpha_y = self.scale_alpha_values(gradient_y)
 

lib/improver/utilities/spatial.py
@@ -31,6 +31,7 @@
 """ Provides support utilities."""
 
 import copy
+import iris
 from iris.coords import CellMethod
 from iris.cube import Cube, CubeList
 from iris.exceptions import CoordinateNotFoundError
@@ -230,7 +231,34 @@ class DifferenceBetweenAdjacentGridSquares(object):
             cube, coord_name, diff_along_axis)
         return diff_cube
 
-    def process(self, cube):
+    @staticmethod
+    def calculate_gradient(diff_cube, ref_cube, coord_axis):
+        """
+        Calculate the gradient along the x or y axis from differences between
+        adjacent grid squares.
+
+        Args:
+            diff_cube (Iris.cube.Cube):
+                Cube containing differences along the x or y axis
+            ref_cube (Iris.cube.Cube):
+                Cube with correct output dimensions
+            coord_axis (String):
+                Short-hand reference for the x or y coordinate, as allowed by
+                iris.util.guess_coord_axis.
+
+
+        Returns:
+            gradient (Iris.cube.Cube):
+                A cube of the gradients in the coordinate direction specified.
+        """
+        grid_spacing = np.diff(diff_cube.coord(axis=coord_axis).points)[0]
+        gradient = diff_cube.copy(data=abs(diff_cube.data / grid_spacing))
+        gradient = gradient.regrid(ref_cube, iris.analysis.Linear())
+        # TODO make cube units per m / km, not original cube units
+        # gradient.units =
+        return gradient
+
+    def process(self, cube, gradient=False):
         """
         Calculate the difference along the x and y axes and return
         the result in separate cubes. The difference along each axis is
@@ -239,6 +267,11 @@ class DifferenceBetweenAdjacentGridSquares(object):
         Args:
             cube (Iris.cube.Cube):
                 Cube from which the differences will be calculated.
+
+        Kwargs:
+            gradient (boolean):
+                Optionally return gradient rather than difference.  This has
+                dimensions of original grid, rather than losing 1 row & col.
 
         Returns:
             (tuple) : tuple containing:
@@ -252,6 +285,11 @@ class DifferenceBetweenAdjacentGridSquares(object):
         """
         diff_along_y_cube = self.calculate_difference(cube, "y")
         diff_along_x_cube = self.calculate_difference(cube, "x")
+        if gradient:
+            diff_along_y_cube = self.calculate_gradient(diff_along_y_cube,
+                                                        cube, "y")
+            diff_along_x_cube = self.calculate_gradient(diff_along_x_cube,
+                                                        cube, "x")
         return diff_along_x_cube, diff_along_y_cube
 
 