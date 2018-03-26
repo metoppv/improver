6618da4bb0d5066a596c13f349acd310f771026f
Caroline Jones <caroline.jones@metoffice.gov.uk>
2018-03-26 14:22:58 +0100

    Removing unit test warnings - final batch

--- lib/improver/tests/ensemble_calibration/ensemble_calibration/test_ContinuousRankedProbabilityScoreMinimisers.py
+++ lib/improver/tests/ensemble_calibration/ensemble_calibration/test_ContinuousRankedProbabilityScoreMinimisers.py
@@ -39,7 +39,6 @@ import unittest
 import iris
 from iris.tests import IrisTest
 import numpy as np
-import warnings
 
 from improver.ensemble_calibration.ensemble_calibration import (
     ContinuousRankedProbabilityScoreMinimisers as Plugin)
@@ -47,6 +46,7 @@ from improver.ensemble_calibration.ensemble_calibration_utilities import (
     convert_cube_data_to_2d)
 from improver.tests.ensemble_calibration.ensemble_calibration.\
     helper_functions import set_up_temperature_cube, set_up_wind_speed_cube
+from improver.utilities.warnings_handler import ManageWarnings
 
 
 class Test_normal_crps_minimiser(IrisTest):
@@ -274,12 +274,14 @@ class Test_crps_minimiser_wrapper(IrisTest):
     Either the ensemble mean or the individual ensemble members are used as
     the predictors.
     """
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_basic_normal_mean_predictor(self):
         """
         Test that the plugin returns a numpy float value.
         The ensemble mean is the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -300,12 +302,14 @@ class Test_crps_minimiser_wrapper(IrisTest):
         self.assertArrayAlmostEqual(
             result, [-0.08169791, -0.09784413, 0.00822535, 1.00956199])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_basic_normal_members_predictor(self):
         """
         Test that the plugin returns a numpy array.
         The ensemble members are the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1, 1, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -327,13 +331,14 @@ class Test_crps_minimiser_wrapper(IrisTest):
             result, [6.24021609e+00, 1.35694934e+00, 1.84642787e-03,
                      5.55444682e-01, 5.04367388e-01, 6.68575194e-01])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
     def test_normal_mean_predictor_keyerror(self):
         """
         Test that the minimisation has resulted in a KeyError, if the
         distribution that has been requested was not within the dictionary
         containing the minimisation functions.
         """
-        warnings.simplefilter("always")
         initial_guess = [
             -8.70808509e-06, 7.23255721e-06, 2.66662740e+00, 1.00000012e+00]
         initial_guess = np.array(initial_guess, dtype=np.float32)
@@ -354,6 +359,9 @@ class Test_crps_minimiser_wrapper(IrisTest):
                 initial_guess, forecast_predictor, truth, forecast_variance,
                 predictor_of_mean_flag, distribution)
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_normal_mean_predictor_max_iterations(self):
         """
         Test that the plugin returns a list of coefficients
@@ -363,7 +371,6 @@ class Test_crps_minimiser_wrapper(IrisTest):
         minimising the CRPS and using a set default value for the
         initial guess.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -384,6 +391,9 @@ class Test_crps_minimiser_wrapper(IrisTest):
         self.assertArrayAlmostEqual(
             result, [-0.303343, -0.022553, 0.008502, 1.009565])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_normal_members_predictor_max_iterations(self):
         """
         Test that the plugin returns a list of coefficients
@@ -393,7 +403,6 @@ class Test_crps_minimiser_wrapper(IrisTest):
         calculated by minimising the CRPS and using a set default value for
         the initial guess.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1, 1, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -415,13 +424,15 @@ class Test_crps_minimiser_wrapper(IrisTest):
             result, [5.375955e+00, 1.457850e+00, 2.566869e-03,
                      1.934232e-01, 5.540603e-01, 8.115994e-01])
 
-    def test_normal_catch_warnings(self):
+    @ManageWarnings(
+        record=True,
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
+    def test_normal_catch_warnings(self, warning_list=None):
         """
         Test that a warning is generated if the minimisation
         does not result in a convergence.
         The ensemble mean is the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -435,18 +446,20 @@ class Test_crps_minimiser_wrapper(IrisTest):
 
         plugin = Plugin()
         distribution = "gaussian"
-        with warnings.catch_warnings(record=True) as warning_list:
-            warnings.simplefilter("always")
-            result = plugin.crps_minimiser_wrapper(
-                initial_guess, forecast_predictor, truth, forecast_variance,
-                predictor_of_mean_flag, distribution)
-            self.assertTrue(len(warning_list) == 1)
-            self.assertTrue(any(item.category == UserWarning
-                                for item in warning_list))
-            self.assertTrue("Minimisation did not result in convergence after"
-                            in str(warning_list[0]))
 
-    def test_normal_catch_warnings_percentage_change(self):
+        result = plugin.crps_minimiser_wrapper(
+            initial_guess, forecast_predictor, truth, forecast_variance,
+            predictor_of_mean_flag, distribution)
+        self.assertTrue(len(warning_list) == 1)
+        self.assertTrue(any(item.category == UserWarning
+                            for item in warning_list))
+        self.assertTrue("Minimisation did not result in convergence after"
+                        in str(warning_list[0]))
+
+    @ManageWarnings(
+        record=True,
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
+    def test_normal_catch_warnings_percentage_change(self, warning_list=None):
         """
         Test that two warnings are generated if the minimisation
         does not result in a convergence. The first warning reports a that
@@ -455,7 +468,6 @@ class Test_crps_minimiser_wrapper(IrisTest):
         greater than the tolerated value.
         The ensemble mean is the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [500, 100, 0, 100]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -469,26 +481,26 @@ class Test_crps_minimiser_wrapper(IrisTest):
 
         plugin = Plugin()
         distribution = "gaussian"
-        with warnings.catch_warnings(record=True) as warning_list:
-            warnings.simplefilter("always")
-            result = plugin.crps_minimiser_wrapper(
-                initial_guess, forecast_predictor, truth, forecast_variance,
-                predictor_of_mean_flag, distribution)
-            self.assertTrue(len(warning_list) == 2)
-            self.assertTrue(any(item.category == UserWarning
-                                for item in warning_list))
-            self.assertTrue("Minimisation did not result in convergence after"
-                            in str(warning_list[0]))
-            self.assertTrue("The final iteration resulted in a percentage "
-                            "change" in str(warning_list[1]))
+        result = plugin.crps_minimiser_wrapper(
+            initial_guess, forecast_predictor, truth, forecast_variance,
+            predictor_of_mean_flag, distribution)
+        self.assertTrue(len(warning_list) == 2)
+        self.assertTrue(any(item.category == UserWarning
+                            for item in warning_list))
+        self.assertTrue("Minimisation did not result in convergence after"
+                        in str(warning_list[0]))
+        self.assertTrue("The final iteration resulted in a percentage "
+                        "change" in str(warning_list[1]))
 
     """Test minimising the CRPS for a truncated_normal distribution."""
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_basic_truncated_normal_mean_predictor(self):
         """
         Test that the plugin returns a numpy float value.
         The ensemble mean is the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -509,9 +521,11 @@ class Test_crps_minimiser_wrapper(IrisTest):
         self.assertArrayAlmostEqual(
             result, [-0.08169791, -0.09784413, 0.00822535, 1.00956199])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_basic_truncated_normal_members_predictor(self):
         """Test that the plugin returns a numpy array."""
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1, 1, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -533,13 +547,14 @@ class Test_crps_minimiser_wrapper(IrisTest):
             result, [6.24021609e+00, 1.35694934e+00, 1.84642787e-03,
                      5.55444682e-01, 5.04367388e-01, 6.68575194e-01])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
     def test_truncated_normal_mean_predictor_keyerror(self):
         """
         Test that the minimisation has resulted in a successful convergence,
         and that the object returned is an OptimizeResult object, when the
         ensemble mean is the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [
             -8.70808509e-06, 7.23255721e-06, 2.66662740e+00, 1.00000012e+00]
         initial_guess = np.array(initial_guess, dtype=np.float32)
@@ -560,13 +575,14 @@ class Test_crps_minimiser_wrapper(IrisTest):
                 initial_guess, forecast_predictor, truth, forecast_variance,
                 predictor_of_mean_flag, distribution)
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
     def test_truncated_normal_members_predictor_keyerror(self):
         """
         Test that the minimisation has resulted in a successful convergence,
         and that the object returned is an OptimizeResult object, when the
         ensemble members are the predictor.
         """
-        warnings.simplefilter("always")
         initial_guess = [
             -8.70808509e-06, 7.23255721e-06, 2.66662740e+00, 1.00000012e+00]
         initial_guess = np.array(initial_guess, dtype=np.float32)
@@ -587,6 +603,9 @@ class Test_crps_minimiser_wrapper(IrisTest):
                 initial_guess, forecast_predictor, truth, forecast_variance,
                 predictor_of_mean_flag, distribution)
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_truncated_normal_mean_predictor_max_iterations(self):
         """
         Test that the plugin returns a list of coefficients
@@ -596,7 +615,6 @@ class Test_crps_minimiser_wrapper(IrisTest):
         calculated by minimising the CRPS and using a set default value for
         the initial guess.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -617,6 +635,9 @@ class Test_crps_minimiser_wrapper(IrisTest):
         self.assertArrayAlmostEqual(
             result, [-0.303343, -0.022553, 0.008502, 1.009565])
 
+    @ManageWarnings(
+        ignored_messages=["Collapsing a non-contiguous coordinate.",
+                          "Minimisation did not result in convergence"])
     def test_truncated_normal_members_predictor_max_iterations(self):
         """
         Test that the plugin returns a list of coefficients
@@ -626,7 +647,6 @@ class Test_crps_minimiser_wrapper(IrisTest):
         calculated by minimising the CRPS and using a set default value for
         the initial guess.
         """
-        warnings.simplefilter("always")
         initial_guess = [5, 1, 0, 1, 1, 1]
         initial_guess = np.array(initial_guess, dtype=np.float32)
         cube = set_up_temperature_cube()
@@ -648,7 +668,10 @@ class Test_crps_minimiser_wrapper(IrisTest):
             result, [5.375955, 1.45785, 0.002567,
                      0.193423, 0.55406, 0.811599])
 
-    def test_truncated_normal_catch_warnings(self):
+    @ManageWarnings(
+        record=True,
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
+    def test_truncated_normal_catch_warnings(self, warning_list=None):
         """
         Test that a warning is generated if the minimisation
         does not result in a convergence.
@@ -667,18 +690,21 @@ class Test_crps_minimiser_wrapper(IrisTest):
 
         plugin = Plugin()
         distribution = "truncated gaussian"
-        with warnings.catch_warnings(record=True) as warning_list:
-            warnings.simplefilter("always")
-            result = plugin.crps_minimiser_wrapper(
-                initial_guess, forecast_predictor, truth, forecast_variance,
-                predictor_of_mean_flag, distribution)
-            self.assertTrue(len(warning_list) == 1)
-            self.assertTrue(any(item.category == UserWarning
-                                for item in warning_list))
-            self.assertTrue("Minimisation did not result in convergence after"
-                            in str(warning_list[0]))
+        result = plugin.crps_minimiser_wrapper(
+            initial_guess, forecast_predictor, truth, forecast_variance,
+            predictor_of_mean_flag, distribution)
+        self.assertTrue(len(warning_list) == 1)
+        self.assertTrue(any(item.category == UserWarning
+                            for item in warning_list))
+        self.assertTrue("Minimisation did not result in convergence after"
+                        in str(warning_list[0]))
 
-    def test_truncated_normal_catch_warnings_percentage_change(self):
+    @ManageWarnings(
+        record=True,
+        ignored_messages=["Collapsing a non-contiguous coordinate."])
+    def test_truncated_normal_catch_warnings_percentage_change(
+            self,
+            warning_list=None):
         """
         Test that two warnings are generated if the minimisation
         does not result in a convergence. The first warning reports a that
@@ -700,18 +726,16 @@ class Test_crps_minimiser_wrapper(IrisTest):
 
         plugin = Plugin()
         distribution = "truncated gaussian"
-        with warnings.catch_warnings(record=True) as warning_list:
-            warnings.simplefilter("always")
-            result = plugin.crps_minimiser_wrapper(
-                initial_guess, forecast_predictor, truth, forecast_variance,
-                predictor_of_mean_flag, distribution)
-            self.assertTrue(len(warning_list) == 2)
-            self.assertTrue(any(item.category == UserWarning
-                                for item in warning_list))
-            self.assertTrue("Minimisation did not result in convergence after"
-                            in str(warning_list[0]))
-            self.assertTrue("The final iteration resulted in a percentage "
-                            "change" in str(warning_list[1]))
+        result = plugin.crps_minimiser_wrapper(
+            initial_guess, forecast_predictor, truth, forecast_variance,
+            predictor_of_mean_flag, distribution)
+        self.assertTrue(len(warning_list) == 2)
+        self.assertTrue(any(item.category == UserWarning
+                            for item in warning_list))
+        self.assertTrue("Minimisation did not result in convergence after"
+                        in str(warning_list[0]))
+        self.assertTrue("The final iteration resulted in a percentage "
+                        "change" in str(warning_list[1]))
 
 
 if __name__ == '__main__':
