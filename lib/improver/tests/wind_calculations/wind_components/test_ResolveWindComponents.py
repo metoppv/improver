57a051d9d26dd4a65c6a91f17d4e62bb102ce7d1
cgsandford <35029690+cgsandford@users.noreply.github.com>
2018-08-09 13:28:59 +0100

    Plugin to calculate wind components from direction and speed (#627)
    * Started plugin to convert wind speed and direction into grid u and v components.
    
    * Added unit test framework.
    
    * Added some unit tests for the "process" function.  Found NetCDF compliant variable names to use for directional wind components.
    
    * Added unit tests for basic resolving  u/v components.
    
    * Got plugin working - now need to establish appropriate coordinate system.
    
    * Set up appropriate treatment of coordinate systems.
    
    * Set up plugin to read required projection from input cube.  Discovered that rotate_winds doesn't do what I need it to do.
    
    * Restructured code around dummy function to calculate angle differences between grid north and true north.  Unit tests now pass (but codacy is likely to object to some currently unused imports).
    
    * Hacked together a function which (I believe) calculates the correction in degrees for wind components aligned to grid North...
    
    * Removed unnecessary whitespace.
    
    * Minor correction to unit test.
    
    * Corrected maths in calc_true_north_offset - changes to unit test output at 4th decimal place.  Put all the angle stuff into radians to reduce conversions.  Reversed sign of rotation to add to wind direction.
    
    * Changed the method of calculating angle offsets to use standard iris and cartopy functionality.
    
    * Removed unused import.  Converted realizations test to use height dimension, since this is more likely as a plugin input.
    
    * Response to review comments.
    
    * Removed unused constants.
    
    * Removed unused import.
    
    * Unmasked and regridded rotated winds to match input cube coordinates.
    
    * Removed blank line (relic of earlier reverted commits).
    
    * Moved new module and tests into wind calculations subdirectory.
    
    * Removed slicing in "process" and confirmed sensible operation on 3D arrays (including angle adjustment broadcasting).  Added unit test for "wind_from_direction".
    
    * Fixed pycodestyle issues.
    
    * Update to docstring describing rotate_winds imprecision.

---
+++ lib/improver/tests/wind_calculations/wind_components/test_ResolveWindComponents.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2018 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Unit tests for the wind_components.ResolveWindComponents plugin."""
+
+import unittest
+import numpy as np
+from cf_units import Unit
+
+import iris
+from iris.tests import IrisTest
+from iris.coords import DimCoord
+from iris.coord_systems import OSGB
+
+from improver.wind_calculations.wind_components import ResolveWindComponents
+
+RAD_TO_DEG = 180./np.pi
+
+
+def set_up_cube(data_2d, name, unit):
+    """Set up a 2D test cube of wind direction or speed"""
+
+    x_coord = DimCoord(np.linspace(150000, 250000, data_2d.shape[1]),
+                       "projection_x_coordinate", units="metres",
+                       coord_system=OSGB())
+    y_coord = DimCoord(np.linspace(0, 600000, data_2d.shape[0]),
+                       "projection_y_coordinate", units="metres",
+                       coord_system=OSGB())
+    cube = iris.cube.Cube(data_2d, standard_name=name, units=unit,
+                          dim_coords_and_dims=[(y_coord, 0), (x_coord, 1)])
+    time_unit = Unit("hours since 1970-01-01 00:00:00", "gregorian")
+    t_coord = DimCoord(402292.5, "time", units=time_unit)
+    cube.add_aux_coord(t_coord)
+    return cube
+
+
+def add_new_dimension(cube, npoints, name, unit):
+    """Add a new dimension with npoints by copying cube data"""
+    cubelist = iris.cube.CubeList([])
+    for i in range(npoints):
+        newcube = cube.copy(cube.data)
+        newcube.add_aux_coord(DimCoord(i, name, unit))
+        cubelist.append(newcube)
+    merged_cube = cubelist.merge_cube()
+    return merged_cube
+
+
+class Test__repr__(IrisTest):
+    """Tests the __repr__ method"""
+
+    def test_basic(self):
+        """Tests the output string is as expected"""
+        result = str(ResolveWindComponents())
+        self.assertEqual(result, '<ResolveWindComponents>')
+
+
+class Test_calc_true_north_offset(IrisTest):
+    """Tests the calc_true_north_offset function"""
+
+    def setUp(self):
+        """Set up a target cube with OSGB projection"""
+        wind_angle = np.zeros((3, 5), dtype=np.float32)
+        self.directions = set_up_cube(
+            wind_angle, "wind_to_direction", "degrees")
+        self.plugin = ResolveWindComponents()
+
+    def test_basic(self):
+        """Test function returns correct type"""
+        result = self.plugin.calc_true_north_offset(self.directions)
+        self.assertIsInstance(result, np.ndarray)
+
+    def test_values(self):
+        """Test that for UK National Grid coordinates the angle adjustments
+        are sensible"""
+        expected_result = np.array([
+            [3.012338, 2.635347, 2.258535, 1.881877, 1.505348],
+            [3.167531, 2.771182, 2.374997, 1.978951, 1.583021],
+            [3.310965, 2.896735, 2.482648, 2.068685, 1.654825]],
+            dtype=np.float32)
+        result = self.plugin.calc_true_north_offset(self.directions)
+        self.assertArrayAlmostEqual(RAD_TO_DEG*result, expected_result)
+
+
+class Test_resolve_wind_components(IrisTest):
+    """Tests the resolve_wind_components method"""
+
+    def setUp(self):
+        """Set up some arrays to convert"""
+        self.plugin = ResolveWindComponents()
+        wind_speed = 10.*np.ones((4, 4), dtype=np.float32)
+        wind_angle = np.array([[0., 30., 45., 60.],
+                               [90., 120., 135., 150.],
+                               [180., 210., 225., 240.],
+                               [270., 300., 315., 330.]])
+        self.wind_cube = set_up_cube(wind_speed, "wind_speed", "knots")
+        self.directions = set_up_cube(
+            wind_angle, "wind_to_direction", "degrees")
+        self.adjustments = np.zeros((4, 4), dtype=np.float32)
+
+    def test_basic(self):
+        """Test function returns correct type"""
+        uspeed, vspeed = self.plugin.resolve_wind_components(
+            self.wind_cube, self.directions, self.adjustments)
+        self.assertIsInstance(uspeed, iris.cube.Cube)
+        self.assertIsInstance(vspeed, iris.cube.Cube)
+
+    def test_values(self):
+        """Test correct values are returned for well-behaved angles"""
+        expected_uspeed = 5.*np.array(
+            [[0., 1., np.sqrt(2.), np.sqrt(3.)],
+             [2., np.sqrt(3.), np.sqrt(2.), 1.],
+             [0., -1., -np.sqrt(2.), -np.sqrt(3.)],
+             [-2., -np.sqrt(3.), -np.sqrt(2.), -1.]])
+
+        expected_vspeed = 5*np.array(
+            [[2., np.sqrt(3.), np.sqrt(2.), 1.],
+             [0., -1., -np.sqrt(2.), -np.sqrt(3.)],
+             [-2., -np.sqrt(3.), -np.sqrt(2.), -1.],
+             [0., 1., np.sqrt(2.), np.sqrt(3.)]])
+
+        uspeed, vspeed = self.plugin.resolve_wind_components(
+            self.wind_cube, self.directions, self.adjustments)
+        self.assertArrayAlmostEqual(uspeed.data, expected_uspeed)
+        self.assertArrayAlmostEqual(vspeed.data, expected_vspeed)
+
+
+class Test_process(IrisTest):
+    """Tests the process method"""
+
+    def setUp(self):
+        """Create dummy cubes for tests"""
+        self.plugin = ResolveWindComponents()
+        wind_speed_data = np.array(
+            [[6, 5, 4, 3], [8, 6, 4, 4], [12, 8, 6, 5]], dtype=np.float32)
+        self.wind_speed_cube = set_up_cube(
+            wind_speed_data, "wind_speed", "knots")
+
+        wind_direction_data = np.array(
+            [[138, 142, 141, 141], [141, 143, 140, 142],
+             [142, 146, 141, 142]], dtype=np.float32)
+        self.wind_direction_cube = set_up_cube(
+            wind_direction_data, "wind_to_direction", "degrees")
+
+        self.expected_u = np.array([
+            [3.774919, 2.902824, 2.406847, 1.826062],
+            [4.683339, 3.386423, 2.456537, 2.374629],
+            [6.829466, 4.149148, 3.593584, 2.963243]], dtype=np.float32)
+
+        self.expected_v = np.array([
+            [-4.663688, -4.071071, -3.194854, -2.380230],
+            [-6.485857, -4.952993, -3.156806, -3.218872],
+            [-9.867035, -6.839924, -4.804805, -4.027306]], dtype=np.float32)
+
+    def test_basic(self):
+        """Test plugin creates two output cubes with the correct metadata"""
+        ucube, vcube = self.plugin.process(
+            self.wind_speed_cube, self.wind_direction_cube)
+        for cube in ucube, vcube:
+            self.assertIsInstance(cube, iris.cube.Cube)
+            self.assertEqual(cube.units, self.wind_speed_cube.units)
+        self.assertEqual(ucube.name(), "grid_eastward_wind")
+        self.assertEqual(vcube.name(), "grid_northward_wind")
+
+    def test_values(self):
+        """Test plugin generates expected wind values"""
+        ucube, vcube = self.plugin.process(
+            self.wind_speed_cube, self.wind_direction_cube)
+        self.assertArrayAlmostEqual(ucube.data, self.expected_u)
+        self.assertArrayAlmostEqual(vcube.data, self.expected_v)
+
+    def test_coordinate_value_mismatch(self):
+        """Test an error is raised if coordinate values are different for wind
+        speed and direction cubes"""
+        self.wind_direction_cube.coord(axis='y').convert_units("km")
+        msg = 'Wind speed and direction cubes have unmatched coordinates'
+        with self.assertRaisesRegex(ValueError, msg):
+            _, _ = self.plugin.process(
+                self.wind_speed_cube, self.wind_direction_cube)
+
+    def test_projection_mismatch(self):
+        """Test an error is raised if coordinate names are different for wind
+        speed and direction cubes"""
+        self.wind_speed_cube.coord(axis='x').rename('longitude')
+        self.wind_speed_cube.coord(axis='y').rename('latitude')
+        msg = 'Wind speed and direction cubes have unmatched coordinates'
+        with self.assertRaisesRegex(ValueError, msg):
+            _, _ = self.plugin.process(
+                self.wind_speed_cube, self.wind_direction_cube)
+
+    def test_height_levels(self):
+        """Test a cube on more than one height level is correctly processed"""
+        wind_speed_3d = add_new_dimension(
+            self.wind_speed_cube, 3, "height", "km")
+        wind_direction_3d = add_new_dimension(
+            self.wind_direction_cube, 3, "height", "km")
+        ucube, vcube = self.plugin.process(wind_speed_3d, wind_direction_3d)
+        self.assertSequenceEqual(ucube.shape, (3, 3, 4))
+        self.assertArrayAlmostEqual(ucube[1].data, self.expected_u)
+        self.assertArrayAlmostEqual(vcube[2].data, self.expected_v)
+
+    def test_wind_from_direction(self):
+        """Test correct behaviour when wind direction is 'from' not 'to'.
+        We do not get perfect direction inversion to the 7th decimal place here
+        because we ignore imprecision in the iris rotate_winds calcuation near
+        the corners of the domain, and regrid the available data linearly to
+        fill the gap.  The output wind speeds (in m s-1) compare equal to the
+        5th decimal place.
+        """
+        expected_u = -1.*self.expected_u
+        expected_v = -1.*self.expected_v
+        self.wind_direction_cube.rename("wind_from_direction")
+        ucube, vcube = self.plugin.process(
+            self.wind_speed_cube, self.wind_direction_cube)
+        self.assertTrue(np.allclose(ucube.data, expected_u, atol=1e-6))
+        self.assertTrue(np.allclose(vcube.data, expected_v, atol=1e-6))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ lib/improver/wind_calculations/wind_components.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2018 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Module containing plugin to resolve wind components."""
+
+import numpy as np
+
+from iris.analysis import Linear
+from iris.analysis.cartography import rotate_winds
+from iris.coord_systems import GeogCS
+from iris.coords import DimCoord
+from iris.cube import Cube
+
+from improver.utilities.cube_manipulation import compare_coords
+
+# Global coordinate reference system used in StaGE (GRS80)
+GLOBAL_CRS = GeogCS(semi_major_axis=6378137.0,
+                    inverse_flattening=298.257222101)
+
+
+class ResolveWindComponents(object):
+    """
+    Plugin to resolve wind components along an input cube's projection axes,
+    given directions with respect to true North
+    """
+
+    def __repr__(self):
+        """Represent the plugin instance as a string"""
+        return '<ResolveWindComponents>'
+
+    @staticmethod
+    def calc_true_north_offset(reference_cube):
+        """
+        Calculate the angles between grid North and true North, as a
+        matrix of values on the grid of the input reference cube.
+
+        Args:
+            reference_cube (iris.cube.Cube):
+                2D cube on grid for which "north" is required.  Provides both
+                coordinate system (reference_cube.coord_system()) and template
+                spatial grid on which the angle adjustments should be provided.
+
+        Returns:
+            angle_adjustment (numpy.ndarray):
+                Angle in radians by which wind direction wrt true North at
+                each point must be rotated to be relative to grid North.
+        """
+        reference_x_coord = reference_cube.coord(axis='x')
+        reference_y_coord = reference_cube.coord(axis='y')
+
+        # find corners of reference_cube grid in lat / lon coordinates
+        latlon = [GLOBAL_CRS.as_cartopy_crs().transform_point(
+            reference_x_coord.points[i], reference_y_coord.points[j],
+            reference_cube.coord_system().as_cartopy_crs()) for i in [0, -1]
+                                                            for j in [0, -1]]
+        latlon = np.array(latlon).T.tolist()
+
+        # define lat / lon coordinates to cover the reference_cube grid at an
+        # equivalent resolution
+        lat_points = np.linspace(np.floor(min(latlon[1])),
+                                 np.ceil(max(latlon[1])),
+                                 len(reference_y_coord.points))
+        lon_points = np.linspace(np.floor(min(latlon[0])),
+                                 np.ceil(max(latlon[0])),
+                                 len(reference_x_coord.points))
+
+        lat_coord = DimCoord(lat_points, 'latitude', units='degrees',
+                             coord_system=GLOBAL_CRS)
+        lon_coord = DimCoord(lon_points, 'longitude', units='degrees',
+                             coord_system=GLOBAL_CRS)
+
+        # define a unit vector wind towards true North over the lat / lon grid
+        udata = np.zeros(reference_cube.shape, dtype=np.float32)
+        vdata = np.ones(reference_cube.shape, dtype=np.float32)
+
+        ucube_truenorth = Cube(udata, "grid_eastward_wind",
+                               dim_coords_and_dims=[(lat_coord, 0),
+                                                    (lon_coord, 1)])
+        vcube_truenorth = Cube(vdata, "grid_northward_wind",
+                               dim_coords_and_dims=[(lat_coord, 0),
+                                                    (lon_coord, 1)])
+
+        # rotate unit vector onto reference_cube coordinate system
+        ucube, vcube = rotate_winds(
+            ucube_truenorth, vcube_truenorth, reference_cube.coord_system())
+
+        # unmask and regrid rotated winds onto reference_cube grid
+        for cube in [ucube, vcube]:
+            cube.data = cube.data.data
+            cube = cube.regrid(reference_cube, Linear())
+
+        # ratio of u to v winds is the tangent of the angle which is the
+        # true North to grid North rotation
+        angle_adjustment = np.arctan2(ucube.data, vcube.data)
+
+        return angle_adjustment
+
+    @staticmethod
+    def resolve_wind_components(speed, angle, adj):
+        """
+        Perform trigonometric reprojection onto x and y axes
+
+        Args:
+            speed (iris.cube.Cube):
+                Cube containing wind speed data
+            angle (iris.cube.Cube):
+                Cube containing wind directions as angles from true North
+            adj (numpy.ndarray):
+                2D array of wind direction angle adjustments in radians, to
+                convert zero reference from true North to grid North.
+                Broadcast automatically if speed and angle cubes have extra
+                dimensions.
+
+        Returns:
+            (tuple): tuple containing
+
+                (iris.cube.Cube):
+                    Cube containing wind vector component in the positive
+                    x-direction
+
+                (iris.cube.Cube):
+                    Cube containing wind vector component in the positive
+                    y-direction
+        """
+        angle.convert_units('radians')
+        angle.data += adj
+
+        # output vectors should be pointing "to" not "from"
+        if "wind_from_direction" in angle.name():
+            angle.data += np.pi
+        sin_angle = np.sin(angle.data)
+        cos_angle = np.cos(angle.data)
+        uspeed = np.multiply(speed.data, sin_angle)
+        vspeed = np.multiply(speed.data, cos_angle)
+        return [speed.copy(data=uspeed), speed.copy(data=vspeed)]
+
+    def process(self, wind_speed, wind_dir):
+
+        """
+        Convert wind speed and direction into u,v components along input cube
+        projection axes.
+
+        Args:
+            wind_speed (iris.cube.Cube):
+                Cube containing wind speed values
+            wind_dir (iris.cube.Cube):
+                Cube containing wind direction values relative to true North
+
+        Returns:
+            (tuple): tuple containing
+
+                **ucube** (iris.cube.Cube):
+                    Cube containing wind speeds in the positive projection
+                    x-axis direction, with units and projection matching
+                    wind_speed cube.
+
+                **vcube** (iris.cube.Cube):
+                    Cube containing wind speeds in the positive projection
+                    y-axis direction, with units and projection matching
+                    wind_speed cube.
+        """
+        # check cubes contain the correct data (assuming CF standard names)
+        if "wind_speed" not in wind_speed.name():
+            msg = '{} cube does not contain wind speeds'
+            raise ValueError('{} {}'.format(wind_speed.name(), msg))
+
+        if "wind" not in wind_dir.name() or "direction" not in wind_dir.name():
+            msg = '{} cube does not contain wind directions'
+            raise ValueError('{} {}'.format(wind_dir.name(), msg))
+
+        # check input cube coordinates match
+        unmatched_coords = compare_coords([wind_speed, wind_dir])
+        if unmatched_coords != [{}, {}]:
+            msg = 'Wind speed and direction cubes have unmatched coordinates'
+            raise ValueError('{} {}'.format(msg, unmatched_coords))
+
+        # calculate angle adjustments for wind direction
+        wind_dir_slice = next(
+            wind_dir.slices([wind_dir.coord(axis='y').name(),
+                             wind_dir.coord(axis='x').name()]))
+        adj = self.calc_true_north_offset(wind_dir_slice)
+
+        # calculate grid eastward and northward speeds
+        ucube, vcube = self.resolve_wind_components(wind_speed, wind_dir, adj)
+
+        # relabel final cubes with CF compliant data names corresponding to
+        # positive wind speeds along the x and y axes
+        ucube.rename("grid_eastward_wind")
+        vcube.rename("grid_northward_wind")
+
+        return ucube, vcube
