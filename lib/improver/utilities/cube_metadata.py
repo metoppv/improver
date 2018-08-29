241d4c8970992b0c610b56052427ef1b077c6864
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2018-08-29 08:33:34 +0100

    Impro 331 Improve cube_manipulation (#653)
    * Adds utility to demote any float64 data to float32 when merging cubes.
    
    * Added check in cube_manipulation to discard mismatched bounds on coordinates.
    
    * Adds iris.util.squeeze before calling iris.merge_cubes()
    
    * Uses StaGE v1.2.0 meta-data keys for model configuration instead of "grid_id" and "title".
    Applies a fixed "model_id" for each known model configuration.
    Updates unit-tests and deletes ones that are no longer relevant.
    
    * Changes coordinate name from "model" to "model_configuration" to match the attribute it came from.
    
    * Adds function to update cube meta-data after blending.
    
    * Adds two more attibutes to be deleted without warning if they are unmatched.
    
    * Changes blending coordinate to match new standard.
    
    * Adds handling of a new forecast reference time to the update_cube_blended_metadata function.

--- lib/improver/utilities/cube_metadata.py
+++ lib/improver/utilities/cube_metadata.py
@@ -392,7 +392,6 @@ def update_cube_blended_metadata(cube, coord, forecast_reference_time):
 
     # Sort out time coordinates
     cube, = unify_forecast_reference_time(cube, forecast_reference_time)
-
     if cube_coord.name() in "model_configuration":
         cube.remove_coord("model_id")
         cube.attributes["mosg__model_configuration"] = "blend"
