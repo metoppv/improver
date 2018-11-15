5844d8d13c4cb86ff31b44edcea57cdf12bf8a66
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2018-11-15 16:11:13 +0000

    Removed add_renamed_cell_method function and associated tests.

--- lib/improver/utilities/cube_manipulation.py
+++ lib/improver/utilities/cube_manipulation.py
@@ -676,37 +676,6 @@ def build_coordinate(data, long_name=None,
     return crd_out
 
 
-def add_renamed_cell_method(cube, orig_cell_method, new_cell_method_name):
-    """A function that modifies the input cube by adding a new cell method,
-       which is a renamed version of the input cell_method.
-
-        Args:
-            cube (iris.cube.Cube):
-                The cube which we need to add the cell_method to.
-            orig_cell_method(iris.coord.CellMethod):
-                The original cell method we want to rename and add to the
-                cube.
-            new_cell_method_name (string):
-                The name of the new cell_method we want to rename the
-                original cell_method to.
-        Raises:
-            TypeError: If Input Cell_method is not an instance of
-                iris.coord.CellMethod.
-        """
-    if not isinstance(orig_cell_method, iris.coords.CellMethod):
-        message = ('Input Cell_method is not an instance of '
-                   'iris.coord.CellMethod')
-        raise TypeError(message)
-    renamed_cell_method = iris.coords.CellMethod(
-        method=new_cell_method_name,
-        coords=orig_cell_method.coord_names,
-        intervals=orig_cell_method.intervals,
-        comments=orig_cell_method.comments)
-    final_cms = [cm for cm in cube.cell_methods if cm != orig_cell_method]
-    final_cms = tuple(final_cms + [renamed_cell_method])
-    cube.cell_methods = final_cms
-
-
 def sort_coord_in_cube(cube, coord, order="ascending"):
     """Sort a cube based on the ordering within the chosen coordinate.
     Sorting can either be in ascending or descending order.
