updates based on removing the met office models dictionary in constants.py and fixing units tests to work with the auto generated numbers from _equalise_cube_attributes in cube_manipulation.py

lib/improver/tests/utilities/cube_manipulation/test__equalise_cube_attributes.py
@@ -124,11 +124,11 @@ class Test__equalise_cube_attributes(IrisTest):
         result = _equalise_cube_attributes(cubelist)
 
         self.assertArrayAlmostEqual(result[0].coord("model_id").points,
-                                    np.array([3000]))
+                                    np.array([0]))
         self.assertEqual(result[0].coord("model_configuration").points[0],
                          'uk_det')
         self.assertArrayAlmostEqual(result[1].coord("model_id").points,
-                                    np.array([4000]))
+                                    np.array([1000]))
         self.assertEqual(result[1].coord("model_configuration").points[0],
                          'uk_ens')
         self.assertNotIn("mosg__model_configuration",

lib/improver/tests/utilities/cube_manipulation/test_equalise_cubes.py
@@ -93,11 +93,11 @@ class Test_equalise_cubes(IrisTest):
         cubelist = iris.cube.CubeList([self.cube_ukv, self.cube])
         result = equalise_cubes(cubelist)
         self.assertArrayAlmostEqual(result[0].coord("model_id").points,
-                                    np.array([3000]))
+                                    np.array([0]))
         self.assertEqual(result[0].coord("model_configuration").points[0],
                          'uk_det')
         self.assertArrayAlmostEqual(result[1].coord("model_id").points,
-                                    np.array([4000]))
+                                    np.array([1000]))
         self.assertEqual(result[1].coord("model_configuration").points[0],
                          'uk_ens')
         self.assertNotIn("mosg__model_configuration", result[0].attributes)
@@ -133,7 +133,7 @@ class Test_equalise_cubes(IrisTest):
         result = equalise_cubes(cubelist)
         self.assertEqual(len(result), 4)
         self.assertAlmostEqual(result[3].coord('model_realization').points,
-                               4002.0)
+                               1002.0)
 
 
 if __name__ == '__main__':

lib/improver/utilities/cube_manipulation.py
@@ -19,7 +19,7 @@
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# IMPLIED  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 # ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
@@ -38,7 +38,6 @@ import numpy as np
 import iris
 from iris.coords import AuxCoord, DimCoord
 from iris.exceptions import CoordinateNotFoundError
-from improver.constants import MODEL_ID_DICT
 from improver.utilities.cube_checker import check_cube_coordinates
 
 
@@ -308,19 +307,30 @@ def _equalise_cube_attributes(
                     cube.attributes.pop(attr)
                     unmatching_attributes[i].pop(attr)
             # Add associated model_id if model_configurations do not match.
+            # if model_id_attr in unmatching_attributes[i]:
+            #     model_title = cube.attributes.pop(model_id_attr)
+            #     for key, val in MODEL_ID_DICT.items():
+            #         if val == model_title:
+            #             break
+            #     new_model_id_coord = build_coordinate([key],
+            #                                           long_name='model_id',
+            #                                           data_type=np.int)
+            #     new_model_coord = (
+            #         build_coordinate([model_title],
+            #                          long_name='model_configuration',
+            #                          coord_type=AuxCoord,
+            #                          data_type=np.str))
+
             if model_id_attr in unmatching_attributes[i]:
                 model_title = cube.attributes.pop(model_id_attr)
-                for key, val in MODEL_ID_DICT.items():
-                    if val == model_title:
-                        break
-                new_model_id_coord = build_coordinate([key],
+                new_model_id_coord = build_coordinate([1000 * i],
                                                       long_name='model_id',
                                                       data_type=np.int)
-                new_model_coord = (
-                    build_coordinate([model_title],
-                                     long_name='model_configuration',
-                                     coord_type=AuxCoord,
-                                     data_type=np.str))
+
+                new_model_coord = (build_coordinate([model_title],
+                                                    long_name='model_configuration',
+                                                    coord_type=AuxCoord,
+                                                    data_type=np.str))
                 cube.add_aux_coord(new_model_id_coord)
                 cube.add_aux_coord(new_model_coord)
                 unmatching_attributes[i].pop(model_id_attr)