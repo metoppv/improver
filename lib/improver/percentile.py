04526a0945cf542d0dc80a6941b191af6df7b38a
Gavin Evans <gavin.evans@metoffice.gov.uk>
2019-01-11 13:36:59 +0000

    Correction to allow percentiles to be calculated over the time coordinate, if there is also a forecast_reference_time coordinate on the cube. Unit tests have been amended to use the set_up_variable_cube helper function.

--- lib/improver/percentile.py
+++ lib/improver/percentile.py
@@ -116,7 +116,7 @@ class PercentileConverter(object):
         data_type = cube.dtype
         # Test that collapse coords are present in cube before proceding.
         n_collapse_coords = len(self.collapse_coord)
-        n_valid_coords = sum([test_coord in coord.name()
+        n_valid_coords = sum([test_coord == coord.name()
                               for coord in cube.coords()
                               for test_coord in self.collapse_coord])
 
