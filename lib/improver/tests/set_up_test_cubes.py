ea3270afd08afb7423f3a2df046748655a9aadf5
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2019-01-17 12:09:27 +0000

    Updated unit tests for cube setup with forecast period as int32.

--- lib/improver/tests/set_up_test_cubes.py
+++ lib/improver/tests/set_up_test_cubes.py
@@ -115,7 +115,8 @@ def construct_scalar_time_coords(time, time_bounds, frt):
 
     if time_point_seconds < frt_point_seconds:
         raise ValueError('Cannot set up cube with negative forecast period')
-    fp_point_seconds = np.int32(time_point_seconds - frt_point_seconds)
+    fp_point_seconds = (
+        time_point_seconds - frt_point_seconds).astype(np.int32)
 
     # parse bounds if required
     if time_bounds is not None:
@@ -129,8 +130,9 @@ def construct_scalar_time_coords(time, time_bounds, frt):
             raise ValueError(
                 'Time point {} not within bounds {}-{}'.format(
                     time, time_bounds[0], time_bounds[1]))
-        fp_bounds = (bounds[0] - frt_point_seconds,
-                     bounds[1] - frt_point_seconds)
+        fp_bounds = np.array([
+            [bounds[0] - frt_point_seconds,
+             bounds[1] - frt_point_seconds]]).astype(np.int32)
     else:
         bounds = None
         fp_bounds = None
