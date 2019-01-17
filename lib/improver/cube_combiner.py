Refined data type conversions and added unit test to cover non-escalation of int32 precision.  CLI test outputs now have the correct values and units.

lib/improver/cube_combiner.py
@@ -105,7 +105,8 @@ class CubeCombiner(object):
                 Cube with coord expanded.
 
                 n.b. If argument point == 'mid' then python will convert
-                result.coord('coord').points[0] to a float.
+                result.coord('coord').points[0] to a float UNLESS the coord
+                units contain 'seconds'.
 
                 This is to ensure that midpoints are not accidentally
                 rounded down by Python's default integer divide behavour:
@@ -136,10 +137,13 @@ class CubeCombiner(object):
             result_coord.bounds = result_coord.bounds.astype(np.float32)
 
         if point == 'mid':
-            if 'seconds since' in str(result_coord.units):
+            if 'seconds' in str(result_coord.units):
                 # integer division of seconds required to retain precision
+                dtype_orig = result_coord.dtype
                 result_coord.points = [
                     (new_top_bound - new_low_bound) // 2 + new_low_bound]
+                # re-cast to original precision to avoid escalating int32s
+                result_coord.points = result_coord.points.astype(dtype_orig)
             else:
                 # float division of hours required for accuracy
                 result_coord.points = [

lib/improver/tests/set_up_test_cubes.py
@@ -115,7 +115,7 @@ def construct_scalar_time_coords(time, time_bounds, frt):
 
     if time_point_seconds < frt_point_seconds:
         raise ValueError('Cannot set up cube with negative forecast period')
-    fp_point_seconds = time_point_seconds - frt_point_seconds
+    fp_point_seconds = np.int32(time_point_seconds - frt_point_seconds)
 
     # parse bounds if required
     if time_bounds is not None: