73390df4107fa4c7206657d164e48bd0cab2c409
bayliffe <benjamin.ayliffe@metoffice.gov.uk>
2019-02-06 14:51:53 +0000

    IMPRO-1082: Global domain checking to allow circular coordinates (#803)
    * Remove domain checking for spot-data on a global grid.
    
    * Changed identifier of a global grid to be a circular x-coordinate as this is changed to false when a cut-out of a global grid is used. Improved the domain checker that excludes points outside the cube domain to allow for the circular x-coordinate of a global grid whilst still checking on the latitude coordinate.
    
    * Additional bats tests to capture the behaviour that was previously missed and to check the new functionality.
    
    * Modified domain check to use global variable rather than pass in an additional keyword.
    
    * Tiny pylint changes.

--- lib/improver/spotdata/neighbour_finding.py
+++ lib/improver/spotdata/neighbour_finding.py
@@ -41,7 +41,7 @@ from improver.utilities.cube_manipulation import enforce_coordinate_ordering
 from improver.spotdata.build_spotdata_cube import build_spotdata_cube
 
 
-class NeighbourSelection(object):
+class NeighbourSelection:
     """
     For the selection of a grid point near an arbitrary coordinate, where the
     selection may be the nearest point, or a point that fulfils other
@@ -107,9 +107,7 @@ class NeighbourSelection(object):
         self.site_altitude = 'altitude'
         self.grid_metadata_identifier = grid_metadata_identifier
         self.node_limit = node_limit
-        # This is a global flag that will be set to True if the input cubes are
-        # geodetic, which here means they are global grids.
-        self.geodetic_coordinate_system = False
+        self.global_coordinate_system = False
 
     def __repr__(self):
         """Represent the configured plugin instance as a string."""
@@ -166,8 +164,7 @@ class NeighbourSelection(object):
         return target_coordinate_system.transform_points(
             self.site_coordinate_system, x_points, y_points)[:, 0:2]
 
-    @staticmethod
-    def check_sites_are_within_domain(sites, site_coords, site_x_coords,
+    def check_sites_are_within_domain(self, sites, site_coords, site_x_coords,
                                       site_y_coords, cube):
         """
         A function to remove sites from consideration if they fall outside the
@@ -205,13 +202,20 @@ class NeighbourSelection(object):
         y_min = cube.coord(axis='y').bounds.min()
         y_max = cube.coord(axis='y').bounds.max()
 
-        domain_valid = np.where(
-            (site_coords[:, 0] >= x_min) & (site_coords[:, 0] <= x_max) &
-            (site_coords[:, 1] >= y_min) & (site_coords[:, 1] <= y_max))
+        if self.global_coordinate_system:
+            domain_valid = np.where(
+                (site_coords[:, 1] >= y_min) & (site_coords[:, 1] <= y_max))
 
-        domain_invalid = np.where(
-            (site_coords[:, 0] < x_min) | (site_coords[:, 0] > x_max) |
-            (site_coords[:, 1] < y_min) | (site_coords[:, 1] > y_max))
+            domain_invalid = np.where(
+                (site_coords[:, 1] < y_min) | (site_coords[:, 1] > y_max))
+        else:
+            domain_valid = np.where(
+                (site_coords[:, 0] >= x_min) & (site_coords[:, 0] <= x_max) &
+                (site_coords[:, 1] >= y_min) & (site_coords[:, 1] <= y_max))
+
+            domain_invalid = np.where(
+                (site_coords[:, 0] < x_min) | (site_coords[:, 0] > x_max) |
+                (site_coords[:, 1] < y_min) | (site_coords[:, 1] > y_max))
 
         num_invalid = len(domain_invalid[0])
         if num_invalid > 0:
@@ -256,7 +260,7 @@ class NeighbourSelection(object):
     @staticmethod
     def geocentric_cartesian(cube, x_coords, y_coords):
         """
-        A function to convert a geodetic (lat/lon) coordinate system into a
+        A function to convert a global (lat/lon) coordinate system into a
         geocentric (3D trignonometric) system. This function ignores orographic
         height differences between coordinates, giving a 2D projected
         neighbourhood akin to selecting a neighbourhood of grid points about a
@@ -316,7 +320,7 @@ class NeighbourSelection(object):
         x_coords = land_mask.coord(axis='x').points[x_indices]
         y_coords = land_mask.coord(axis='y').points[y_indices]
 
-        if self.geodetic_coordinate_system:
+        if self.global_coordinate_system:
             nodes = self.geocentric_cartesian(land_mask, x_coords, y_coords)
         else:
             nodes = list(zip(x_coords, y_coords))
@@ -422,11 +426,10 @@ class NeighbourSelection(object):
         """
         index_nodes = []
         # Check if we are dealing with a global grid.
-        self.geodetic_coordinate_system = (
-            orography.coord_system().as_cartopy_crs().is_geodetic())
+        self.global_coordinate_system = orography.coord(axis='x').circular
 
         # Exclude regional grids with spatial dimensions other than metres.
-        if not self.geodetic_coordinate_system:
+        if not self.global_coordinate_system:
             if not orography.coord(axis='x').units == 'metres':
                 msg = ('Cube spatial coordinates for regional grids must be'
                        'in metres to match the defined search_radius.')
@@ -458,7 +461,8 @@ class NeighbourSelection(object):
         # notify the user.
         sites, site_coords, site_x_coords, site_y_coords = (
             self.check_sites_are_within_domain(
-                sites, site_coords, site_x_coords, site_y_coords, orography))
+                sites, site_coords, site_x_coords, site_y_coords,
+                orography))
 
         # Find nearest neighbour point using quick iris method.
         nearest_indices = self.get_nearest_indices(site_coords, orography)
@@ -479,7 +483,7 @@ class NeighbourSelection(object):
             tree, index_nodes = self.build_KDTree(land_mask)
 
             # Site coordinates made cartesian for global coordinate system
-            if self.geodetic_coordinate_system:
+            if self.global_coordinate_system:
                 site_coords = self.geocentric_cartesian(
                     orography, site_coords[:, 0], site_coords[:, 1])
 
