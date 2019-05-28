466cfa37ee63b5e0852f86c487b0709c76eb0b0f
Gavin Evans <gavin.evans@metoffice.gov.uk>
2019-05-28 14:49:42 +0100

    Modify docstring for max_iterations in estimate_emos_coefficients CLI.

--- lib/improver/cli/estimate_emos_coefficients.py
+++ lib/improver/cli/estimate_emos_coefficients.py
@@ -97,7 +97,11 @@ def main(argv=None):
                              'of iterations is reached, but the '
                              'minimisation has not yet converged to a '
                              'stable solution, then the available solution '
-                             'is used anyway, and a warning is raised.')
+                             'is used anyway, and a warning is raised. '
+                             'If the predictor_of_mean is "realizations", '
+                             'then the number of iterations may require '
+                             'increasing, as there will be more coefficients '
+                             'to solve for.')
 
     args = parser.parse_args(args=argv)
 
