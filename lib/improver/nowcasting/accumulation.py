a29146f77e20c4a8b720a0ec744179741df2cd46
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2019-06-13 13:18:04 +0100

    Simplified approach, making use of new standardisation functions that seem to have potential general uses.

--- lib/improver/nowcasting/accumulation.py
+++ lib/improver/nowcasting/accumulation.py
@@ -32,15 +32,12 @@
 This module defines the accumulation class for calculating precipitation
 accumulations from advected radar fields.
 """
-import datetime
-import warnings
 import numpy as np
-from datetime import timezone
 
 import iris
 from iris.exceptions import CoordinateNotFoundError, InvalidCubeError
-from improver.utilities.temporal import (iris_time_to_datetime,
-                                         datetime_to_iris_time)
+from improver.utilities.cube_units import (enforce_coordinate_units_and_dtypes,
+                                           enforce_diagnostic_units_and_dtypes)
 
 
 class Accumulation:
@@ -48,20 +45,19 @@ class Accumulation:
     Class to calculate precipitation accumulations from radar rates fields
     provided at discrete time intervals. The plugin will return one fewer
     cubes of accumulation than the number of input precipitation rate cubes.
+    The time intervals between the precipitation rate cubes determine the
+    accumulation periods.
     """
-    def __init__(self):
+    def __init__(self, accumulation_units='m'):
         """
-        Init
+        Initialise the plugin.
         """
+        self.accumulation_units = accumulation_units
 
     def __repr__(self):
         """Represent the plugin instance as a string."""
-        result = ('<OpticalFlow: data_smoothing_radius_km: {}, '
-                  'data_smoothing_method: {}, iterations: {}, '
-                  'point_weight: {}, metadata_dict: {}>')
-        return result.format(
-            self.data_smoothing_radius_km, self.data_smoothing_method,
-            self.iterations, self.point_weight, self.metadata_dict)
+        result = '<Accumulation>'
+        return result
 
     @staticmethod
     def sort_cubes_by_time(cubes):
@@ -74,106 +70,84 @@ class Accumulation:
         Returns:
             cubes (iris.cube.CubeList):
                 The cubelist in ascending time order.
-            times (list of datetime objects):
-                A list of the validity times of the precipitation rate cubes as
-                datetime objects in matching order to the returned cubelist.
+            times (list):
+                A list of the validity times of the precipitation rate cubes in
+                integer seconds since 1970-01-01 00:00:00.
         """
-        times = np.array([
-            iris_time_to_datetime(cube.coord('time'))[0] for cube in cubes])
+        times = np.array([cube.coord('time').points[0] for cube in cubes])
         time_sorted = np.argsort(times)
         times = times[time_sorted]
         cubes = list(np.array(cubes)[time_sorted])
         return cubes, times
 
     @staticmethod
-    def calculate_intervals(times):
-        """
-        Calculate the time intervals between the precipitation rate cubes and
-        return this as a list.
-
-        Args:
-            times (list of datetime objects):
-                A list of the validity times of the precipitation rate cubes as
-                datetime objects in ascending order.
-        Returns:
-            time_intervals (list of datetime objects):
-                A list of time intervals between precipitation rate cubes.
-        """
-        time_intervals = np.diff(times, axis=0)
-        return time_intervals
-
-    @staticmethod
-    def calculate_accumulation_times(times, time_intervals):
-        """
-        Calculate the validity times for the precipitation accumulation cubes
-        where these fall at the end of the accumulation period. Return these
-        in integer seconds since 1970-01-01 00:00:00.
-
-        Args:
-            times (list of datetime objects):
-                A list of the validity times of the precipitation rate cubes as
-                datetime objects in ascending order.
-            time_intervals (list of datetime objects):
-                A list of time intervals between precipitation rate cubes.
-        Returns:
-            accumulation_times (list):
-                A list of validity times for the accumulation cubes in integer
-                seconds since 1970-01-01 00:00:00.
-        """
-        accumulation_times = times + time_intervals
-        accumulation_times = np.array(
-            [time.replace(tzinfo=timezone.utc).timestamp()
-             for time in accumulation_times], dtype=np.int64)
-
-        return accumulation_times
-
-    @staticmethod
-    def create_accumulation_cube(cube, time_interval,
-                                 accumulation_time):
+    def create_accumulation_cube(cube, time_interval):
         """
         Create a new cube to contain the calculated accumulation data.
 
         Args:
             cube (iris.cube.Cube):
-                The precipitation rate cube that is valid at the beginning of
-                the accumulation period.
+                The precipitation rate cube that is valid at the end of the
+                accumulation period.
+            time_interval (int):
+                The number of seconds between the validity times of the
+                precipitation rate cubes that bookend the accumulation period.
+        Returns:
+            accumulation_cube (iris.cube.Cube):
+                A new cube based upon the precipitation rate cubes. The name
+                and time coordinates are modified to reflect the accumulation
+                data that will be contained within the cube.
         """
+        validity_time, = cube.coord('time').points
         time_coordinate = cube.coord('time').copy(
-            points=accumulation_time,
-            bounds=(accumulation_time - time_interval.seconds, accumulation_time))
+            points=validity_time,
+            bounds=(validity_time - time_interval, validity_time))
         fp, = cube.coord('forecast_period').points
         fp_coordinate = cube.coord('forecast_period').copy(
-            points=fp + time_interval.seconds,
-            bounds=(fp, fp + time_interval.seconds))
+            points=validity_time,
+            bounds=(fp - time_interval, fp))
 
         accumulation_cube = cube.copy()
         accumulation_cube.rename('lwe_thickness_of_precipitation_amount')
         accumulation_cube.replace_coord(time_coordinate)
         accumulation_cube.replace_coord(fp_coordinate)
+        accumulation_cube.units = 'm'
         return accumulation_cube
-
-    def integrate_rates(self, cubes, time_intervals,
-                        accumulation_times):
-        accumulation_cubes = []
-        iterator = zip(cubes, time_intervals, accumulation_times)
-        for cube, time_interval, accumulation_time in iterator:
-            accumulation_cube = self.create_accumulation_cube(
-                cube, time_interval, accumulation_time)
-            accumulation_data = cube.data * time_interval.seconds
-            accumulation_cube.data = accumulation_data
-            accumulation_cubes.append(accumulation_cube)
-
-        return accumulation_cubes
 
     def process(self, cubes):
         """
+        Calculate period precipitation accumulations based upon precipitation
+        rate fields.
+
+        Args:
+            cubes (iris.cube.CubeList):
+                A cubelist containing input precipitation rate cubes.
+        Returns:
+            accumulation_cubes (iris.cube.CubeList):
+                A cubelist containing precipitation accumulation cubes where
+                the accumulation periods are determined by the intervals
+                between the precipitation rate cubes.
         """
-        # TODO Check units are in seconds, or be adaptable to not seconds.
+        # Standardise inputs to expected units.
+        cubes = enforce_coordinate_units_and_dtypes(
+            cubes, ['time', 'forecast_reference_time', 'forecast_period'],
+            inplace=False)
+        enforce_diagnostic_units_and_dtypes(cubes)
+
+        # Sort cubes into time order and calculate intervals.
         cubes, times = self.sort_cubes_by_time(cubes)
-        time_intervals = self.calculate_intervals(times)
-        accumulation_times = self.calculate_accumulation_times(
-            times[:-1], time_intervals)
-        accumulation_cubes = self.integrate_rates(
-            cubes[:-1], time_intervals, accumulation_times)
+        time_intervals = np.diff(times, axis=0)
+        accumulation_cubes = iris.cube.CubeList()
+
+        iterator = zip(cubes[:-1], cubes[1:], time_intervals)
+
+        # Calculate accumulations and convert to desired units.
+        for start_cube, end_cube, time_interval in iterator:
+            accumulation_cube = self.create_accumulation_cube(
+                end_cube, time_interval)
+            accumulation_data = start_cube.data * time_interval
+            accumulation_cube.data = accumulation_data
+            accumulation_cube.convert_units(self.accumulation_units)
+            accumulation_cubes.append(accumulation_cube)
 
         return accumulation_cubes
