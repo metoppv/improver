5899298a51d07778a20a7aca102fc1b62f1d17ef
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-06-21 09:53:54 +0100

    Fix typos (#884)

--- lib/improver/cli/nbhood_iterate_with_mask.py
+++ lib/improver/cli/nbhood_iterate_with_mask.py
@@ -51,7 +51,7 @@ def main(argv=None):
                     'an extra dimension compared to the input diagnostic. '
                     'In this case, the user specifies the name of '
                     'the extra coordinate and this coordinate is iterated '
-                    'over so each mask is applied to seperate slices over the'
+                    'over so each mask is applied to separate slices over the'
                     ' input data. These intermediate masked datasets are then'
                     ' concatenated, resulting in a dataset that has been '
                     ' processed using multiple masks and has gained an extra '
