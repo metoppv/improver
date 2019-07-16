2cb1d0875e6e9dc99b539243532b277e22e58910
TomekTrzeciak <tomasz.trzeciak@metoffice.gov.uk>
2019-07-16 19:20:22 +0100

    transpose only if dim order changed

--- lib/improver/utilities/cube_manipulation.py
+++ lib/improver/utilities/cube_manipulation.py
@@ -861,9 +861,11 @@ def enforce_coordinate_ordering(
     # Transpose by inserting the requested coordinates at either the start
     # or the end.
     if anchor == "start":
-        cube.transpose(coord_dims + remaining_coords)
+        dim_order = coord_dims + remaining_coords
     elif anchor == "end":
-        cube.transpose(remaining_coords + coord_dims)
+        dim_order = remaining_coords + coord_dims
+    if dim_order != sorted(dim_order):
+        cube.transpose(dim_order)
     return cube
 
 
