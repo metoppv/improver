a8a07d002fcc535e5bf4e8da7624a5650fcedd65
cgsandford <35029690+cgsandford@users.noreply.github.com>
2019-09-17 16:24:36 +0100

    Between thresholds plugin (#951)
    * Added plugin to calculate probability of difference between thresholds.
    
    * Found the extra brackets!  Fixed pycodestyle & syntax errors.
    
    * Added initial unit test.  Need to rebase and pick up bug fix from master.
    
    * Completed process unit tests.
    
    * Added flexibility to specify required thresholds in different units.
    
    * Tidied up a bit.  No impact on outputs.
    
    * Updated thresh_coord to be a class member and tidied up some coordinate-finding.
    
    * Added unit test to cover zero comparisons.
    
    * Added absolute tolerance for zero-value comparisons.
    
    * Added spaces in calculation.
    
    * Updated unit tests in response to review.
    
    * Made "cube" a class member and factored out metadata changes into lower-level function.
    
    * Added handling of case where threshold ranges are indistinguishable in the given units.
    
    * Fixed pycodestyle issue.

---
+++ lib/improver/between_thresholds.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2019 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Plugin to calculate probabilities of occurrence between specified thresholds
+"""
+
+import iris
+import numpy as np
+
+from iris.exceptions import CoordinateNotFoundError
+
+from improver.utilities.cube_checker import find_threshold_coordinate
+from improver.utilities.cube_metadata import extract_diagnostic_name
+
+
+class OccurrenceBetweenThresholds(object):
+    """Calculate the probability of occurrence between thresholds"""
+
+    def __init__(self, threshold_ranges, threshold_units):
+        """
+        Initialise the class.  Threshold ranges must be specified in a unit
+        that is NOT sensitive to differences at the 1e-5 (float32) precision
+        level.
+
+        Args:
+            threshold_ranges (list):
+                List of 2-item iterables specifying thresholds between which
+                probabilities should be calculated
+            threshold_units (str):
+                Units in which the thresholds are specified
+
+        Raises:
+            ValueError:
+                If any of the specified thresholds are indistinguishable at the
+                1e-5 (float32) precision level
+        """
+        threshold_diffs = np.diff(threshold_ranges)
+        if any([diff < 1e-5 for diff in threshold_diffs]):
+            raise ValueError('Plugin cannot distinguish between thresholds at '
+                             '{} {}'.format(threshold_ranges, threshold_units))
+        self.threshold_ranges = threshold_ranges
+        self.threshold_units = threshold_units
+
+    def _slice_cube(self):
+        """
+        Extract required slices from input cube
+
+        Returns:
+            cubes (list):
+                List of 2-item lists containing lower and upper
+                threshold cubes
+
+        Raises:
+            ValueError:
+                If any of the required constraints returns None
+        """
+        thresh_coord = self.cube.coord(self.thresh_coord.name())
+        error_string = (thresh_coord.name() + ' threshold {} ' +
+                        self.threshold_units + ' is not available\n')
+        error_msg = ''
+
+        cubes = []
+        for t_range in self.threshold_ranges:
+            t_range.sort()
+            lower_constraint = iris.Constraint(coord_values={
+                thresh_coord: lambda t: np.isclose(
+                    t.point, t_range[0], atol=1e-5)})
+            lower_cube = self.cube.extract(lower_constraint)
+            if lower_cube is None:
+                error_msg += error_string.format(t_range[0])
+            upper_constraint = iris.Constraint(coord_values={
+                thresh_coord: lambda t: np.isclose(
+                    t.point, t_range[1], atol=1e-5)})
+            upper_cube = self.cube.extract(upper_constraint)
+            if upper_cube is None:
+                error_msg += error_string.format(t_range[1])
+            cubes.append([lower_cube, upper_cube])
+
+        if error_msg:
+            # if any thresholds were unavailable, raise errors together here
+            raise ValueError(error_msg)
+
+        return cubes
+
+    def _get_multiplier(self):
+        """
+        Check whether the cube contains "above" or "below" threshold
+        probabilities.  For "above", the probability of occurrence between
+        thresholds is the difference between probabilities at the lower
+        and higher thresholds: P(lower) - P(higher).  For "below" it is the
+        inverse of this: P(higher) - P(lower), which is implemented by
+        multiplying the difference by -1.
+
+        Returns:
+            multiplier (float):
+                1. or -1.
+
+        Raises:
+            ValueError: If the spp__relative_to_threshold attribute is
+                not recognised
+        """
+        relative_to_threshold = (
+            self.thresh_coord.attributes['spp__relative_to_threshold'])
+        if relative_to_threshold == 'above':
+            multiplier = 1.
+        elif relative_to_threshold == 'below':
+            multiplier = -1.
+        else:
+            raise ValueError('Input cube must contain probabilities of '
+                             'occurrence above or below threshold')
+        return multiplier
+
+    def _calculate_probabilities(self):
+        """
+        Calculate between_threshold probabilities cube
+
+        Returns:
+            (iris.cube.Cube):
+                Merged cube containing recalculated probabilities
+        """
+        multiplier = self._get_multiplier()
+        thresh_name = self.thresh_coord.name()
+
+        cubelist = iris.cube.CubeList([])
+        for (lower_cube, upper_cube) in self.cube_slices:
+            # construct difference cube
+            between_thresholds_data = (
+                lower_cube.data - upper_cube.data) * multiplier
+            between_thresholds_cube = upper_cube.copy(between_thresholds_data)
+
+            # add threshold coordinate bounds
+            lower_threshold = lower_cube.coord(thresh_name).points[0]
+            upper_threshold = upper_cube.coord(thresh_name).points[0]
+            between_thresholds_cube.coord(thresh_name).bounds = (
+                [lower_threshold, upper_threshold])
+
+            cubelist.append(between_thresholds_cube)
+
+        return cubelist.merge_cube()
+
+    def _update_metadata(self, output_cube, original_units):
+        """
+        Update output cube name and threshold coordinate
+
+        Args:
+            output_cube (iris.cube.Cube):
+                Cube containing new "between_thresholds" probabilities
+            original_units (str):
+                Required threshold-type coordinate units
+        """
+        output_cube.rename(
+            'probability_of_{}_between_thresholds'.format(
+                extract_diagnostic_name(self.cube.name())))
+        new_thresh_coord = output_cube.coord(self.thresh_coord.name())
+        new_thresh_coord.convert_units(original_units)
+        new_thresh_coord.attributes['spp__relative_to_threshold'] = (
+            'between_thresholds')
+
+    def process(self, cube):
+        """
+        Calculate probabilities between thresholds for the input cube
+
+        Args:
+            cube (iris.cube.Cube):
+                Probability cube containing thresholded data (above or below)
+
+        Returns:
+            output_cube (iris.cube.Cube):
+                Cube containing probability of occurrence between thresholds
+        """
+        # if cube has no threshold-type coordinate, raise an error
+        try:
+            self.thresh_coord = find_threshold_coordinate(cube)
+        except CoordinateNotFoundError:
+            raise ValueError('Input is not a probability cube '
+                             '(has no threshold-type coordinate)')
+        self.cube = cube.copy()
+
+        # check input cube units and convert if needed
+        original_units = self.thresh_coord.units
+        if original_units != self.threshold_units:
+            self.cube.coord(self.thresh_coord).convert_units(
+                self.threshold_units)
+
+        # extract suitable cube slices
+        self.cube_slices = self._slice_cube()
+
+        # generate "between thresholds" probabilities
+        output_cube = self._calculate_probabilities()
+        self._update_metadata(output_cube, original_units)
+
+        return output_cube
---
+++ lib/improver/tests/between_thresholds/test_between_thresholds.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2019 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Tests for the OccurrenceBetweenThresholds plugin"""
+
+import unittest
+import numpy as np
+
+import iris
+from iris.tests import IrisTest
+
+from improver.tests.set_up_test_cubes import (
+    set_up_probability_cube, set_up_percentile_cube)
+from improver.between_thresholds import OccurrenceBetweenThresholds
+
+
+class Test_process(IrisTest):
+    """Test the process method"""
+
+    def setUp(self):
+        """Set up a test cube with probability data"""
+        data = np.array([
+            [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]],
+            [[0.9, 0.9, 0.9], [0.8, 0.8, 0.8], [0.7, 0.7, 0.7]],
+            [[0.1, 0.2, 0.3], [0.1, 0.2, 0.3], [0.1, 0.2, 0.3]],
+            [[0.0, 0.0, 0.0], [0.1, 0.1, 0.1], [0.1, 0.2, 0.2]]],
+            dtype=np.float32)
+        temp_thresholds = np.array([279, 280, 281, 282], dtype=np.float32)
+        vis_thresholds = np.array([100, 1000, 5000, 10000], dtype=np.float32)
+
+        self.temp_cube = set_up_probability_cube(data, temp_thresholds)
+        self.vis_cube = set_up_probability_cube(
+            np.flip(data, axis=0), vis_thresholds, variable_name='visibility',
+            threshold_units='m', spp__relative_to_threshold='below')
+
+        # set up a cube of rainfall rates in m s-1 (~1e-8 values)
+        self.precip_cube = self.temp_cube.copy()
+        self.precip_cube.coord('air_temperature').rename('rainfall_rate')
+        self.precip_cube.coord('rainfall_rate').var_name = 'threshold'
+        self.precip_cube.coord('rainfall_rate').points = np.array(
+            [0, 0.25, 0.5, 1], dtype=np.float32)
+        self.precip_cube.coord('rainfall_rate').units = 'mm h-1'
+        self.precip_cube.coord('rainfall_rate').convert_units('m s-1')
+
+    def test_above_threshold(self):
+        """Test values from an "above threshold" cube"""
+        threshold_ranges = [[280, 281], [281, 282]]
+        expected_data = np.array([
+            [[0.8, 0.7, 0.6], [0.7, 0.6, 0.5], [0.6, 0.5, 0.4]],
+            [[0.1, 0.2, 0.3], [0.0, 0.1, 0.2], [0.0, 0.0, 0.1]]],
+            dtype=np.float32)
+        plugin = OccurrenceBetweenThresholds(threshold_ranges.copy(), 'K')
+        result = plugin.process(self.temp_cube)
+        self.assertIsInstance(result, iris.cube.Cube)
+        self.assertEqual(
+            result.name(), 'probability_of_air_temperature_between_thresholds')
+        self.assertArrayAlmostEqual(result.data, expected_data)
+        thresh_coord = result.coord('air_temperature')
+        self.assertArrayAlmostEqual(thresh_coord.points, [281., 282.])
+        self.assertArrayAlmostEqual(thresh_coord.bounds, threshold_ranges)
+        self.assertEqual(thresh_coord.attributes['spp__relative_to_threshold'],
+                         'between_thresholds')
+
+    def test_below_threshold(self):
+        """Test values from a "below threshold" cube"""
+        threshold_ranges = [[1000, 5000]]
+        expected_data = np.array(
+            [[0.8, 0.7, 0.6], [0.7, 0.6, 0.5], [0.6, 0.5, 0.4]],
+            dtype=np.float32)
+        plugin = OccurrenceBetweenThresholds(threshold_ranges.copy(), 'm')
+        result = plugin.process(self.vis_cube)
+        self.assertArrayAlmostEqual(result.data, expected_data)
+        self.assertArrayAlmostEqual(
+            result.coord('visibility').points, [5000.])
+        self.assertArrayAlmostEqual(
+            result.coord('visibility').bounds, threshold_ranges)
+
+    def test_skip_threshold(self):
+        """Test calculation works for non-adjacent thresholds"""
+        threshold_ranges = [[100, 1000], [1000, 10000]]
+        expected_data = np.array([
+            [[0.1, 0.2, 0.3], [0.0, 0.1, 0.2], [0.0, 0.0, 0.1]],
+            [[0.9, 0.8, 0.7], [0.9, 0.8, 0.7], [0.9, 0.8, 0.7]]],
+            dtype=np.float32)
+        plugin = OccurrenceBetweenThresholds(threshold_ranges, 'm')
+        result = plugin.process(self.vis_cube)
+        self.assertArrayAlmostEqual(result.data, expected_data)
+
+    def test_threshold_units(self):
+        """Test calculation works for thresholds specified in different units
+        from the cube data"""
+        threshold_ranges = [[0.1, 1], [1, 10]]
+        expected_data = np.array([
+            [[0.1, 0.2, 0.3], [0.0, 0.1, 0.2], [0.0, 0.0, 0.1]],
+            [[0.9, 0.8, 0.7], [0.9, 0.8, 0.7], [0.9, 0.8, 0.7]]],
+            dtype=np.float32)
+        plugin = OccurrenceBetweenThresholds(threshold_ranges, 'km')
+        result = plugin.process(self.vis_cube)
+        self.assertArrayAlmostEqual(result.data, expected_data)
+        # check original cube units are not modified
+        self.assertEqual(self.vis_cube.coord('visibility').units, 'm')
+        # check output cube units match original cube
+        self.assertEqual(result.coord('visibility').units, 'm')
+        self.assertArrayAlmostEqual(
+            result.coord('visibility').points, [1000, 10000])
+
+    def test_error_non_probability_cube(self):
+        """Test failure if cube doesn't contain probabilities"""
+        perc_cube = set_up_percentile_cube(
+            np.ones((3, 3, 3), dtype=np.float32),
+            np.array((25, 50, 75), dtype=np.float32))
+        plugin = OccurrenceBetweenThresholds([[25, 50]], 'K')
+        msg = 'Input is not a probability cube'
+        with self.assertRaisesRegex(ValueError, msg):
+            plugin.process(perc_cube)
+
+    def test_error_between_thresholds_cube(self):
+        """Test failure if cube isn't above or below threshold"""
+        # use plugin to generate a "between_thresholds" cube...
+        between_thresholds_cube = (
+            OccurrenceBetweenThresholds([[280, 281], [281, 282]], 'K').process(
+                self.temp_cube))
+        plugin = OccurrenceBetweenThresholds([[281, 282]], 'K')
+        msg = 'Input cube must contain'
+        with self.assertRaisesRegex(ValueError, msg):
+            plugin.process(between_thresholds_cube)
+
+    def test_error_thresholds_unavailable(self):
+        """Test error if cube doesn't contain the required thresholds"""
+        threshold_ranges = [[10, 100], [1000, 30000]]
+        plugin = OccurrenceBetweenThresholds(threshold_ranges, 'm')
+        msg = ('visibility threshold 10 m is not available\n'
+               'visibility threshold 30000 m is not available')
+        with self.assertRaisesRegex(ValueError, msg):
+            plugin.process(self.vis_cube)
+
+    def test_threshold_matching_tolerance(self):
+        """Test threshold matching succeeds for absolute values close to
+        zero"""
+        new_thresholds = np.array(
+            [272.15, 273.15, 274.15, 275.15], dtype=np.float32)
+        self.temp_cube.coord('air_temperature').points = new_thresholds
+        threshold_ranges = [[-1, 0], [0, 2]]
+        expected_data = np.array([
+            [[0.1, 0.1, 0.1], [0.2, 0.2, 0.2], [0.3, 0.3, 0.3]],
+            [[0.9, 0.9, 0.9], [0.7, 0.7, 0.7], [0.6, 0.5, 0.5]]],
+            dtype=np.float32)
+        plugin = OccurrenceBetweenThresholds(threshold_ranges, 'degC')
+        result = plugin.process(self.temp_cube)
+        self.assertArrayAlmostEqual(result.data, expected_data)
+
+    def test_thresholds_indistinguishable(self):
+        """Test behaviour in a case where cube extraction cannot work within a
+        tolerance of 1e-5"""
+        # set threshold ranges in m s-1
+        points = self.precip_cube.coord('rainfall_rate').points.copy()
+        threshold_ranges = [[points[1], points[2]]]
+        msg = 'Plugin cannot distinguish between thresholds at'
+        with self.assertRaisesRegex(ValueError, msg):
+            OccurrenceBetweenThresholds(threshold_ranges, 'm s-1')
+
+    def test_original_units_indistinguishable(self):
+        """Test cubes where thresholds are indistinguisable in SI units can be
+        correctly processed using threshold ranges specified in a unit with
+        more than 1e-5 discrimination"""
+        expected_data = np.array(
+            [[0.8, 0.7, 0.6], [0.7, 0.6, 0.5], [0.6, 0.5, 0.4]],
+            dtype=np.float32)
+        threshold_ranges = [[0.25, 0.5]]
+        plugin = OccurrenceBetweenThresholds(threshold_ranges, 'mm h-1')
+        result = plugin.process(self.precip_cube)
+        self.assertArrayAlmostEqual(result.data, expected_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
