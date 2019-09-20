7afe68717fd38715edab91ebcf67bfc17abed4e8
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2019-09-20 12:10:51 +0100

    Tidied module docstring.

--- lib/improver/cli/between_thresholds.py
+++ lib/improver/cli/between_thresholds.py
@@ -29,8 +29,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
-"""Script to calculate probabilities between thresholds."""
+"""Script to calculate probabilities of occurrence between thresholds."""
 
 import json
 
