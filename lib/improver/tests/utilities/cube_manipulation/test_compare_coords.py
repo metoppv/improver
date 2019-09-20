2542d8106c71278d36cefe9814f6f8f1576f9ed0
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2019-09-20 16:25:13 +0100

    Tidied up concatenation tests.

--- lib/improver/tests/utilities/cube_manipulation/test_compare_coords.py
+++ lib/improver/tests/utilities/cube_manipulation/test_compare_coords.py
@@ -40,8 +40,6 @@ from cf_units import Unit
 from iris.coords import AuxCoord, DimCoord
 from iris.tests import IrisTest
 
-#from improver.tests.ensemble_calibration.ensemble_calibration. \
-#    helper_functions import set_up_temperature_cube
 from improver.tests.set_up_test_cubes import set_up_variable_cube
 from improver.utilities.cube_manipulation import compare_coords
 from improver.utilities.warnings_handler import ManageWarnings
