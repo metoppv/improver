6506fd9f37d769af389a754bcbeeda8d8602ed61
bayliffe <benjamin.ayliffe@metoffice.gov.uk>
2019-10-01 16:01:30 +0100

    IMPRO-1287 - bugfix for day max / night min (#968)
    * Bug fix. Adding temp min and max to units dictionary.
    
    * Correct day to night.

--- lib/improver/units.py
+++ lib/improver/units.py
@@ -78,6 +78,8 @@ DEFAULT_UNITS = {
     "precipitation_rate": {"unit": "m s-1"},
     "pressure": {"unit": "Pa"},
     "temperature": {"unit": "K"},
+    "temperature_at_screen_level_daytime_max": {"unit": "K"},
+    "temperature_at_screen_level_nighttime_min": {"unit": "K"},
     "thickness": {"unit": "m"},
     "ultraviolet_flux": {"unit": "W m-2"},
     "ultraviolet_index": {"unit": "1"},
