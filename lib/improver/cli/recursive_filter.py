94ece08d04d248d304b1c93098f367abb1244d24
Neil <43375279+neilCrosswaite@users.noreply.github.com>
2019-11-04 10:39:39 +0000

    Update lib/improver/cli/recursive_filter.py
    Co-Authored-By: Stephen Moseley <stephen.moseley@metoffice.gov.uk>

--- lib/improver/cli/recursive_filter.py
+++ lib/improver/cli/recursive_filter.py
@@ -137,7 +137,7 @@ def process(cube, mask_cube=None, alphas_x_cube=None, alphas_y_cube=None,
         iterations (int):
             Number of times to apply the filter. (Typically < 3)
             Number of iterations should be less than 3, higher values have been
-            show to lead to poorer conservation.
+            shown to lead to poorer conservation.
             Default is 1 (one).
         re_mask (bool):
             Re-apply mask to recursively filtered output.
