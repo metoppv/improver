1aa72dfbf8838fa4d1899f6a08a539c6e24070d8
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2019-12-13 15:39:09 +0000

    Clizes create-grid-with-halo

--- lib/improver/tests/acceptance/test_create_grid_with_halo.py
+++ lib/improver/tests/acceptance/test_create_grid_with_halo.py
@@ -45,7 +45,7 @@ def test_basic(tmp_path):
     kgo_path = kgo_dir / "kgo.nc"
     input_path = kgo_dir / "source_grid.nc"
     output_path = tmp_path / "output.nc"
-    args = [input_path, output_path]
+    args = [input_path, "--output", output_path]
     run_cli(args)
     acc.compare(output_path, kgo_path)
 
@@ -56,6 +56,6 @@ def test_halo_size(tmp_path):
     kgo_path = kgo_dir / "kgo.nc"
     input_path = kgo_dir / "../basic/source_grid.nc"
     output_path = tmp_path / "output.nc"
-    args = [input_path, output_path, "--halo_radius", "75000"]
+    args = [input_path, "--output", output_path, "--halo-radius", "75000"]
     run_cli(args)
     acc.compare(output_path, kgo_path)
