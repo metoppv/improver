68f051ec1df68a33124da63b1ffe51031fdc4868
bayliffe <benjamin.ayliffe@metoffice.gov.uk>
2019-12-16 14:14:06 +0000

    Modified to use clize. (#1055)

--- lib/improver/cli/wet_bulb_temperature.py
+++ lib/improver/cli/wet_bulb_temperature.py
@@ -32,57 +32,15 @@
 """CLI to generate wet bulb temperatures from air temperature, relative
    humidity, and pressure data. """
 
-from improver.argparser import ArgParser
-
-from improver.psychrometric_calculations.psychrometric_calculations import (
-    WetBulbTemperature)
-from improver.utilities.load import load_cube
-from improver.utilities.save import save_netcdf
+from improver import cli
 
 
-def main(argv=None):
-    """Parser to accept input data and an output destination before invoking
-    the wet bulb temperature plugin. Also accepted is an optional
-    convergence_condition argument that can be used to specify the tolerance of
-    the Newton iterator used to calculate the wet bulb temperatures."""
-
-    parser = ArgParser(
-        description='Calculate a field of wet bulb temperatures.')
-    parser.add_argument('temperature', metavar='TEMPERATURE',
-                        help='Path to a NetCDF file of air temperatures at '
-                        'the points for which the wet bulb temperatures are '
-                        'being calculated.')
-    parser.add_argument('relative_humidity', metavar='RELATIVE_HUMIDITY',
-                        help='Path to a NetCDF file of relative humidities at '
-                        'the points for for which the wet bulb temperatures '
-                        'are being calculated.')
-    parser.add_argument('pressure', metavar='PRESSURE',
-                        help='Path to a NetCDF file of air pressures at the '
-                        'points for which the wet bulb temperatures are being'
-                        ' calculated.')
-    parser.add_argument('output_filepath', metavar='OUTPUT_FILE',
-                        help='The output path for the processed NetCDF.')
-    parser.add_argument('--convergence_condition',
-                        metavar='CONVERGENCE_CONDITION', type=float,
-                        default=0.05,
-                        help='The convergence condition for the Newton '
-                        'iterator in K. When the wet bulb temperature '
-                        'stops changing by more than this amount between'
-                        ' iterations, the solution is accepted.')
-
-    args = parser.parse_args(args=argv)
-    # Load Cubes
-    temperature = load_cube(args.temperature)
-    relative_humidity = load_cube(args.relative_humidity)
-    pressure = load_cube(args.pressure)
-    # Process Cube
-    result = process(temperature, relative_humidity, pressure,
-                     args.convergence_condition)
-    # Save Cube
-    save_netcdf(result, args.output_filepath)
-
-
-def process(temperature, relative_humidity, pressure,
+@cli.clizefy
+@cli.with_output
+def process(temperature: cli.inputcube,
+            relative_humidity: cli.inputcube,
+            pressure: cli.inputcube,
+            *,
             convergence_condition=0.05):
     """Module to generate wet-bulb temperatures.
 
@@ -92,14 +50,17 @@ def process(temperature, relative_humidity, pressure,
 
     Args:
         temperature (iris.cube.Cube):
-            Cube of air temperatures.
+            Cube of air temperatures, where these may be on multiple height
+            levels.
         relative_humidity (iris.cube.Cube):
-            Cube of relative humidities.
+            Cube of relative humidities, where these may be on multiple height
+            levels.
         pressure (iris.cube.Cube):
-            Cube of air pressure.
+            Cube of air pressure, where these may be on multiple height
+            levels.
         convergence_condition (float):
-            The precision to which the Newton iterator must converge before
-            returning wet-bulb temperatures.
+            The precision in Kelvin to which the Newton iterator must converge
+            before returning wet-bulb temperatures.
             Default is 0.05.
 
     Returns:
@@ -107,10 +68,9 @@ def process(temperature, relative_humidity, pressure,
             Cube of wet-bulb temperature (K).
 
     """
+    from improver.psychrometric_calculations.psychrometric_calculations \
+        import WetBulbTemperature
+
     result = (WetBulbTemperature(precision=convergence_condition).
               process(temperature, relative_humidity, pressure))
     return result
-
-
-if __name__ == "__main__":
-    main()
