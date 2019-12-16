70322fc25743301f524c8db8698a3485258bb899
Aaron Hopkinson <aaron.hopkinson@metoffice.gov.uk>
2019-12-16 16:23:21 +0000

    blend-adjacent-points pytest style update

--- lib/improver/tests/acceptance/test_blend_adjacent_points.py
+++ lib/improver/tests/acceptance/test_blend_adjacent_points.py
@@ -45,8 +45,10 @@ def test_basic_mean(tmp_path):
     kgo_path = kgo_dir / "kgo.nc"
     multi_prob = sorted(kgo_dir.glob("multiple_probabilities_rain_*H.nc"))
     output_path = tmp_path / "output.nc"
-    args = ["--coordinate", "forecast_period", "--central-point", "2",
-            "--units", "hours", "--width", "3.0",
+    args = ["--coordinate", "forecast_period",
+            "--central-point", "2",
+            "--units", "hours",
+            "--width", "3",
             *multi_prob,
             "--output", output_path]
     run_cli(args)
@@ -59,8 +61,10 @@ def test_time_bounds(tmp_path):
     kgo_path = kgo_dir / "kgo.nc"
     multi_prob = sorted(kgo_dir.glob("*wind_gust*.nc"))
     output_path = tmp_path / "output.nc"
-    args = ["--coordinate", "forecast_period", "--central-point", "4",
-            "--units", "hours", "--width", "2",
+    args = ["--coordinate", "forecast_period",
+            "--central-point", "4",
+            "--units", "hours",
+            "--width", "2",
             *multi_prob,
             "--output", output_path]
     run_cli(args)
@@ -72,8 +76,10 @@ def test_mismatched_bounds_ranges(tmp_path):
     kgo_dir = acc.kgo_root() / "blend-adjacent-points/basic_mean"
     multi_prob = sorted(kgo_dir.glob("multiple_probabilities_rain_*H.nc"))
     output_path = tmp_path / "output.nc"
-    args = ["--coordinate", "forecast_period", "--central-point", "2",
-            "--units", "hours", "--width", "3.0",
+    args = ["--coordinate", "forecast_period",
+            "--central-point", "2",
+            "--units", "hours",
+            "--width", "3",
             "--blend-time-using-forecast-period",
             *multi_prob,
             "--output", output_path]
@@ -86,8 +92,10 @@ def test_mismatched_args(tmp_path):
     kgo_dir = acc.kgo_root() / "blend-adjacent-points/basic_mean"
     multi_prob = sorted(kgo_dir.glob("multiple_probabilities_rain_*H.nc"))
     output_path = tmp_path / "output.nc"
-    args = ["--coordinate", "model", "--central-point", "2",
-            "--units", "None", "--width", "3.0",
+    args = ["--coordinate", "model",
+            "--central-point", "2",
+            "--units", "None",
+            "--width", "3",
             "--blend-time-using-forecast-period",
             *multi_prob,
             "--output", output_path]
@@ -100,8 +108,10 @@ def test_time(tmp_path):
     kgo_dir = acc.kgo_root() / "blend-adjacent-points/time_bounds"
     multi_prob = sorted(kgo_dir.glob("*wind_gust*.nc"))
     output_path = tmp_path / "output.nc"
-    args = ["--coordinate", "time", "--central-point", "1536908400",
-            "--units", "seconds since 1970-01-01 00:00:00",
+    args = ["--coordinate", "time",
+            "--central-point", "1536908400",
+            "--units",
+            "seconds since 1970-01-01 00:00:00",
             "--width", "7200",
             *multi_prob,
             "--output", output_path]
