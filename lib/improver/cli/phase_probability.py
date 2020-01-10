ed2fa226059b95870419d974b21796ec6b1fdf68
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2020-01-10 14:22:00 +0000

    Adds CLI to generate prob(rain) or prob(snow), with acceptance tests.

---
+++ lib/improver/cli/phase_probability.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2019 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Interface to precip_phase_probability."""
+
+from improver import cli
+
+
+@cli.clizefy
+@cli.with_output
+def process(*cubes: cli.inputcube,
+            radius: float = 10000.):
+    """
+    Converts a phase-change-level cube into the
+    probability of a specific precipitation phase being found at the surface.
+
+    Args:
+        cubes: iris.cube.CubeList or list
+            Contains cubes of the altitude of the phase-change level (this
+            can be snow->sleet, or sleet->rain) and the altitude of the
+            orography. The name of the phase-change level cube must be
+            either "altitude_of_snow_falling_level" or
+            "altitude_of_rain_falling_level". The name of the orography
+            cube must be "surface_altitude".
+        radius: float
+            Neighbourhood radius from which 80th percentile is found (m)
+
+    """
+    from improver.psychrometric_calculations.precip_phase_probability import (
+        ProbPhase)
+    from iris.cube import CubeList
+    return ProbPhase(radius=radius).process(CubeList(cubes))
