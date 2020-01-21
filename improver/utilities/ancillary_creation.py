ad1dd5585257b612e8a5d6b0c18b1790863bb22f
Neil <43375279+neilCrosswaite@users.noreply.github.com>
2020-01-21 08:03:00 +0000

    Update improver/utilities/ancillary_creation.py
    Co-Authored-By: Stephen Moseley <stephen.moseley@metoffice.gov.uk>

--- improver/utilities/ancillary_creation.py
+++ improver/utilities/ancillary_creation.py
@@ -28,7 +28,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""A module for creating ancillarys"""
+"""A module for creating ancillary data"""
 
 import iris
 import numpy as np
