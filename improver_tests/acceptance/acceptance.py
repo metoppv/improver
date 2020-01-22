5d757086dacf6e4513dd3273b4833778719ea41e
Tom Gale <tom.gale@bom.gov.au>
2020-01-22 16:37:23 +0000

    Update recreate_if_needed
    - Add recreate_dir_path argument
    - Raise exception if attempting to recreate in-place

--- improver_tests/acceptance/acceptance.py
+++ improver_tests/acceptance/acceptance.py
@@ -100,13 +100,16 @@ def kgo_exists():
     return ACC_TEST_DIR_ENVVAR in os.environ
 
 
-def recreate_if_needed(output_path, kgo_path):
+def recreate_if_needed(output_path, kgo_path, recreate_dir_path=None):
     """
     Re-create a file in the KGO, depending on configuration.
 
     Args:
         output_path (pathlib.Path): Path to output produced by test
         kgo_path (pathlib.Path): Path to expected/original KGO file
+        recreate_dir_path (Optional[pathlib.Path]): Path to directory where
+            recreated KGOs will be placed. Default is environment variable
+            specified in RECREATE_DIR_ENVVAR constant.
 
     Returns:
         bool: True if KGO was recreated
@@ -117,11 +120,12 @@ def recreate_if_needed(output_path, kgo_path):
         raise IOError("KGO path is not absolute")
     if not output_path.is_file():
         raise IOError("Expected output file not created by running test")
+    if recreate_dir_path is None:
+        recreate_dir_path = pathlib.Path(os.environ[RECREATE_DIR_ENVVAR])
     kgo_root_dir = kgo_root()
-    recreate_dir = pathlib.Path(os.environ[RECREATE_DIR_ENVVAR])
     if kgo_root_dir not in kgo_path.parents:
         raise IOError("KGO path for test is not within KGO root directory")
-    if not recreate_dir.is_absolute():
+    if not recreate_dir_path.is_absolute():
         raise IOError("Recreate KGO path is not absolute")
     print("Comparison found differences - recreating KGO for this test")
     if kgo_path.exists():
@@ -129,20 +133,21 @@ def recreate_if_needed(output_path, kgo_path):
     else:
         print("Original KGO file does not exist")
     kgo_relative = kgo_path.relative_to(kgo_root_dir)
-    recreate_path = recreate_dir / kgo_relative
-    recreate_path.parent.mkdir(exist_ok=True, parents=True)
-    if recreate_path.exists():
-        recreate_path.unlink()
-    shutil.copyfile(str(output_path), str(recreate_path))
-    print(f"Updated KGO file is at {recreate_path}")
-    if recreate_path.resolve() == kgo_path.resolve():
-        print("KGO updated in place, rerunning this test should now pass")
-    else:
-        print(f"Put the updated KGO file in {ACC_TEST_DIR_ENVVAR} to make this"
-              f" test pass. For example:")
-        quoted_kgo = shlex.quote(str(kgo_path))
-        quoted_recreate = shlex.quote(str(recreate_path))
-        print(f"cp {quoted_recreate} {quoted_kgo}")
+    recreate_file_path = recreate_dir_path / kgo_relative
+    if recreate_file_path.resolve() == kgo_path.resolve():
+        err = (f"Recreate KGO path {recreate_file_path} must be different from"
+               f" original KGO path {kgo_path} to avoid overwriting")
+        raise IOError(err)
+    recreate_file_path.parent.mkdir(exist_ok=True, parents=True)
+    if recreate_file_path.exists():
+        recreate_file_path.unlink()
+    shutil.copyfile(str(output_path), str(recreate_file_path))
+    print(f"Updated KGO file is at {recreate_file_path}")
+    print(f"Put the updated KGO file in {ACC_TEST_DIR_ENVVAR} to make this"
+          f" test pass. For example:")
+    quoted_kgo = shlex.quote(str(kgo_path))
+    quoted_recreate = shlex.quote(str(recreate_file_path))
+    print(f"cp {quoted_recreate} {quoted_kgo}")
     return True
 
 
