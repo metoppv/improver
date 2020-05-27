Modifications following review including to correct the units output from the gradient plugin.

improver/utilities/spatial.py
@@ -302,12 +302,17 @@ class GradientBetweenAdjacentGridSquares(BasePlugin):
         self.regrid = regrid
 
     @staticmethod
-    def _update_metadata(gradient, cube):
-        """Rename cube and remove inappropriate metadata.
+    def _update_metadata(gradient, cube, diff, axis):
+        """Rename cube and assign appropriate units.
 
         Args:
             gradient (iris.cube.Cube)
+                Cube containing gradients along the x or y axis.
             cube (iris.cube.Cube)
+                Cube used to calculate differences and gradients.
+            diff (iris.cube.Cube)
+                Cube containing differences along the x or y axis.
+            axis (str)
 
         Returns:
             iris.cube.Cube:
@@ -315,19 +320,20 @@ class GradientBetweenAdjacentGridSquares(BasePlugin):
 
         """
         gradient.rename("gradient_of_" + cube.name())
+        gradient.units = cube.units / diff.coord(axis=axis).units
         return gradient
 
-    def _gradient_from_diff(self, diff_cube, ref_cube, coord_axis):
+    def _gradient_from_diff(self, diff, cube, axis):
         """
         Calculate the gradient along the x or y axis from differences between
         adjacent grid squares.
 
         Args:
-            diff_cube (iris.cube.Cube):
+            diff (iris.cube.Cube):
                 Cube containing differences along the x or y axis
-            ref_cube (iris.cube.Cube):
+            cube (iris.cube.Cube):
                 Cube with correct output dimensions
-            coord_axis (str):
+            axis (str):
                 Short-hand reference for the x or y coordinate, as allowed by
                 iris.util.guess_coord_axis.
 
@@ -335,10 +341,10 @@ class GradientBetweenAdjacentGridSquares(BasePlugin):
             iris.cube.Cube:
                 A cube of the gradients in the coordinate direction specified.
         """
-        grid_spacing = np.diff(diff_cube.coord(axis=coord_axis).points)[0]
-        gradient = diff_cube.copy(data=diff_cube.data / grid_spacing)
+        grid_spacing = np.diff(diff.coord(axis=axis).points)[0]
+        gradient = diff.copy(data=diff.data / grid_spacing)
         if self.regrid:
-            gradient = gradient.regrid(ref_cube, iris.analysis.Linear())
+            gradient = gradient.regrid(cube, iris.analysis.Linear())
         return gradient
 
     def process(self, cube):
@@ -353,15 +359,15 @@ class GradientBetweenAdjacentGridSquares(BasePlugin):
 
         Returns:
             (tuple): tuple containing:
-                **diff_along_y_cube** (iris.cube.Cube):
-                    Cube after the differences have been calculated along the
+                **gradient_along_y_cube** (iris.cube.Cube):
+                    Cube after the gradients have been calculated along the
                     y axis.
-                **diff_along_x_cube** (iris.cube.Cube):
-                    Cube after the differences have been calculated along the
+                **gradient_along_x_cube** (iris.cube.Cube):
+                    Cube after the gradients have been calculated along the
                     x axis.
 
         """
-        diffs = []
+        gradients = []
         for axis in ["x", "y"]:
             coord_name = cube.coord(axis=axis).name()
             diff = DifferenceBetweenAdjacentGridSquares().calculate_difference(
@@ -371,10 +377,10 @@ class GradientBetweenAdjacentGridSquares(BasePlugin):
                 cube, coord_name, diff
             )
             gradient = self._gradient_from_diff(diff, cube, axis)
-            self._update_metadata(gradient, cube)
-            diffs.append(gradient)
+            self._update_metadata(gradient, cube, diff, axis)
+            gradients.append(gradient)
 
-        return diffs[0], diffs[1]
+        return gradients[0], gradients[1]
 
 
 class OccurrenceWithinVicinity(PostProcessingPlugin):

improver_tests/utilities/test_GradientBetweenAdjacentGridSquares.py
@@ -41,12 +41,12 @@ from improver.utilities.spatial import GradientBetweenAdjacentGridSquares
 from ..set_up_test_cubes import set_up_variable_cube
 
 
-@pytest.fixture(name="orography")
-def orography_fixture() -> Cube:
-    """Orography in m"""
+@pytest.fixture(name="wind_speed")
+def wind_speed_fixture() -> Cube:
+    """Wind speed in m/s"""
     data = np.array([[0, 1, 2], [2, 3, 4], [4, 5, 6]], dtype=np.float32)
     cube = set_up_variable_cube(
-        data, name="surface_altitude", units="m", spatial_grid="equalarea"
+        data, name="wind_speed", units="m s^-1", spatial_grid="equalarea"
     )
     for axis in ["x", "y"]:
         cube.coord(axis=axis).points = np.array([0, 1, 2], dtype=np.float32)
@@ -61,10 +61,7 @@ def make_expected_fixture() -> callable:
         """Create a cube filled with data of a specific shape and value."""
         data = np.full(shape, value, dtype=np.float32)
         cube = set_up_variable_cube(
-            data,
-            name="gradient_of_surface_altitude",
-            units="m",
-            spatial_grid="equalarea",
+            data, name="gradient_of_wind_speed", units="s^-1", spatial_grid="equalarea",
         )
         for index, axis in enumerate(["y", "x"]):
             cube.coord(axis=axis).points = np.array(
@@ -82,23 +79,25 @@ def check_assertions(result, expected):
     assert result[1].name() == expected[1].name()
     assert result[0].attributes == expected[0].attributes
     assert result[1].attributes == expected[1].attributes
+    assert result[0].units == expected[0].units
+    assert result[1].units == expected[1].units
     np.testing.assert_allclose(result[0].data, expected[0].data, rtol=1e-5, atol=1e-8)
     np.testing.assert_allclose(result[1].data, expected[1].data, rtol=1e-5, atol=1e-8)
 
 
-def test_with_regrid(orography, make_expected):
+def test_with_regrid(wind_speed, make_expected):
     """Check calculating the gradient with regridding enabled."""
     x_cube = make_expected((3, 3), 1)
     y_cube = make_expected((3, 3), 2)
-    result = GradientBetweenAdjacentGridSquares(regrid=True)(orography)
+    result = GradientBetweenAdjacentGridSquares(regrid=True)(wind_speed)
     check_assertions(result, (x_cube, y_cube))
 
 
-def test_without_regrid(orography, make_expected):
+def test_without_regrid(wind_speed, make_expected):
     """Check calculating the gradient with regridding disabled."""
     x_cube = make_expected((3, 2), 1)
     y_cube = make_expected((2, 3), 2)
-    result = GradientBetweenAdjacentGridSquares()(orography)
+    result = GradientBetweenAdjacentGridSquares()(wind_speed)
     check_assertions(result, (x_cube, y_cube))
 
 