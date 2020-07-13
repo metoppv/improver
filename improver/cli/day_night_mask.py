fde6767ef54b9e88c1521fe1f9a2c6b86e33d79a
katy <katherine.hurst@metoffice.gov.uk>
2020-07-13 14:03:48 +0100

    Adding day night mask CLI

---
+++ improver/cli/day_night_mask.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2020 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Script to generate a daynight mask from UV index map over UK."""
+
+from improver import cli
+
+
+@cli.clizefy
+@cli.with_output
+def process(day_night_mask: cli.inputcube):
+    """Generates a daynight mask for the provided cube.
+       
+        Create blank daynight mask cube
+
+        Args:
+            cube (iris.cube.Cube):
+                cube with the times and coordinates required for mask
+
+        Returns:
+            iris.cube.Cube:
+                Blank daynight mask cube. The resulting cube will be the
+                same shape as the time, y, and x coordinate, other coordinates
+                will be ignored although they might appear as attributes
+                on the cube as it is extracted from the first slice.
+
+    """
+    from improver.utilities.solar import DayNightMask
+
+    result = DayNightMask()(day_night_mask)
+    return result
