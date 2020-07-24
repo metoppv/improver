a9a36db9b3436fbdfcc93de855bc0ee83278326c
Chris Sampson <christopher.sampson@metoffice.gov.uk>
2020-07-24 08:26:08 +0100

    Black'd the code

--- improver/cli/__init__.py
+++ improver/cli/__init__.py
@@ -295,7 +295,11 @@ def with_output(
 
     result = wrapped(*args, **kwargs)
     # Clize automatically converts args to match the default value, however, the default value for least_significant_digit is None
-    least_significant_digit = int(least_significant_digit) if least_significant_digit is not None else least_significant_digit
+    least_significant_digit = (
+        int(least_significant_digit)
+        if least_significant_digit is not None
+        else least_significant_digit
+    )
     if output:
         save_netcdf(result, output, compression_level, least_significant_digit)
         return
