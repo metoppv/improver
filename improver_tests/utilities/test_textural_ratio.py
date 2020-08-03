66e98b1d383b986921544011d924d1790f369548
katy <katherine.hurst@metoffice.gov.uk>
2020-08-03 15:44:12 +0100

    Adding cloud texture plugin and unit test

---
+++ improver_tests/utilities/test_textural_ratio.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2020 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+""" Tests of textural ratio plugin."""
+
+import numpy as np
+import pytest
+import iris
+from iris.cube import Cube
+
+from improver.metadata.constants.attributes import MANDATORY_ATTRIBUTE_DEFAULTS
+from improver.utilities.textural_ratio import FieldTexture
+
+from improver.synthetic_data.set_up_test_cubes import set_up_variable_cube
+
+
+@pytest.fixture(name="input_multi_cloud_data")
+def multi_cloud_fixture():
+    """Multi-realization cloud data cube"""
+
+    # Create array of data
+
+    cloud_data = np.array(
+        [
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ]
+    )
+
+    # stack to create 3 realizations of the data
+
+    cloud_data = np.float32(cloud_data)
+    cloud_data = np.stack([cloud_data, cloud_data, cloud_data])
+
+    # convert numpy array into a cube
+
+    input_cube = set_up_variable_cube(cloud_data, name="cloud_data", spatial_grid = "equalarea")
+    return input_cube
+
+
+def test__calculate_ratio(input_multi_cloud_data):
+    """Test the ratio calculation function"""
+
+    expected_data = np.array(
+        [
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.3333, 0.3333, 0.3333, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.3333, 0.3333, 0.3333, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.3333, 0.3333, 0.3333, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        ]
+    )
+
+    expected_data = np.float32(expected_data)
+    expected_cube = set_up_variable_cube(expected_data, name="expected_ratio_data", spatial_grid = "equalarea")
+    nbhood_radius = 10000
+    ratio_threshold = 0.4
+    plugin = FieldTexture(nbhood_radius, ratio_threshold)
+
+    # run the _calculate_ratio function within the plugin with one realization of the input cube
+
+    ratio_result = plugin._calculate_ratio(input_multi_cloud_data[0], nbhood_radius)
+
+    # Check the actual result matches the expected result to 4 decimal places
+
+    np.testing.assert_almost_equal(ratio_result.data, expected_cube.data, decimal=4)
+
+
+def test__calculate_clumpiness(input_multi_cloud_data):
+    """Test the clumpiness calculation function"""
+
+    expected_data = np.array(
+        [
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        ]
+    )
+
+    expected_data = np.float32(expected_data)
+    expected_cube = set_up_variable_cube(expected_data, name="expected_ratio_data", spatial_grid = "equalarea")
+    nbhood_radius = 10000
+    ratio_threshold = 0.4
+    plugin = FieldTexture(nbhood_radius, ratio_threshold)
+    
+    # Run the _calculate_clumpiness function within the plugin with multi-realization input cube
+
+    clumpiness_result = plugin._calculate_clumpiness(input_multi_cloud_data)
+    
+    # Check the actual result matches the expected result to 4 decimal places
+
+    np.testing.assert_almost_equal(clumpiness_result.data, expected_cube.data, decimal=4)
+
+def test__calculate_transitions(input_multi_cloud_data):
+    """Test the transition calculation function"""
+
+    expected_data = np.array(
+        [
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+
+        ]
+    )
+    expected_data = np.float32(expected_data)
+    expected_cube = set_up_variable_cube(expected_data, name="expected_ratio_data", spatial_grid = "equalarea")
+    # Run the _calculate_transitions function within the plugin with single-realization input cube
+    nbhood_radius = 10000
+    ratio_threshold = 0.4
+    plugin = FieldTexture(nbhood_radius, ratio_threshold)
+
+    transition_result = plugin._calculate_transitions(input_multi_cloud_data[0].data)
+
+    # Check the actual result matches the expected result to 4 decimal places
+
+    np.testing.assert_almost_equal(transition_result.data, expected_cube.data, decimal=4)
