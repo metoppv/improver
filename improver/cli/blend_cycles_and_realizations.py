8f826f3d844848e93fcb723e4e61f23369c59fce
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2020-08-12 16:44:05 +0100

    Adds new blend-cycles-and-realizations CLI
    - This uses the WeightAndBlend plugin twice. First to collapse the realization coord on all input cubes individually and second to collapse the forecast_reference_time coord and all cubes at once.
    - Adds acceptance tests to show this working for two enukx cycles (cut-out domain).

---
+++ improver/cli/blend_cycles_and_realizations.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2020 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Script to run weighted blending to collapse realization and forecast_reference_time
+coords using equal weights."""
+
+from improver import cli
+
+
+@cli.clizefy
+@cli.with_output
+def process(
+    *cubes: cli.inputcube, cycletime: str = None,
+):
+    """Runs equal-weighted blending.
+
+    Calculates an equal-weighted blend of input cube data across the realization and
+    forecast_reference_time coordinates.
+
+    Args:
+        cubes (iris.cube.CubeList):
+            Cubelist of cubes to be blended.
+        cycletime (str):
+            The forecast reference time to be used after blending has been
+            applied, in the format YYYYMMDDTHHMMZ. If not provided, the
+            blended file takes the latest available forecast reference time
+            from the input datasets supplied.
+
+    Returns:
+        iris.cube.Cube:
+            Merged and blended Cube.
+    """
+    from improver.blending.calculate_weights_and_blend import WeightAndBlend
+    from iris.cube import CubeList
+
+    plugin = WeightAndBlend("realization", "linear", y0val=0.5, ynval=0.5,)
+    cubelist = CubeList([])
+    for cube in cubes:
+        cubelist.append(plugin(cube))
+
+    plugin = WeightAndBlend("forecast_reference_time", "linear", y0val=0.5, ynval=0.5,)
+    cube = plugin(cubelist, cycletime=cycletime,)
+    return cube
