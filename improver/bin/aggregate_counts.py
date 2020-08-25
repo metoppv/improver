03624274dab19a93fac2dd934326c6f8eeae4602
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2020-08-25 08:34:41 +0100

    Modified counts script to take a list of input forecasts.

--- improver/bin/aggregate_counts.py
+++ improver/bin/aggregate_counts.py
@@ -32,6 +32,7 @@
 """Script to calculate hits, misses, false alarms and no detections and append
 counts to a log file."""
 
+import argparse
 import os
 import sys
 import numpy as np
@@ -58,46 +59,47 @@ def get_counts(obs, fcst, threshold):
     return hits, misses, false, no_det
 
 
-def main(obs_path, fcst_path, log_path, model, thresholds_mmh):
+def main(obs_path, fcst_paths, log_path, model, thresholds_mmh):
     """
     Args:
         obs_path (str):
             Full path to observation netcdf file
-        fcst_path (str):
-            Full path to forecast (nowcast or UKV) netcdf file
+        fcst_paths (list of str):
+            List of full paths to forecast (nowcast or UKV) netcdf files
         log_path (str):
             Directory to write / append log file
         model (str):
             Source model identifier for output file name
         thresholds_mmh (list of float)
     """
-    # Read inputs
+    # Load "truth" and set up constants
     obs = load_cube(obs_path)
-    all_fcsts = load_cube(fcst_path)
-
-    # Extract required forecast validity time
-    obs_time = obs.coord("time").cell(0).point
-    try:
-        fcst = extract_nearest_time_point(all_fcsts, obs_time, allowed_dt_difference=0)
-    except ValueError:
-        # we expect the UKV not to have matching 15 minute forcasts; so if a
-        # matching forecast is not available, exit without error
-        return
-
-    # Convert to threshold units
     obs.convert_units('mm h-1')
-    fcst.convert_units('mm h-1')
-
-    # Calculate hits, misses, false alarms and missed detections for these thresholds
+    obs_time = obs.coord("time").cell(0).point
     cycletime = datetime_to_cycletime(obs_time)
-    lead_time_minutes = int(fcst.coord("forecast_period").points[0] / 60)
-    lines = []
-    for threshold in thresholds_mmh:
-        hits, misses, false_alarms, no_det = get_counts(obs.data, fcst.data, threshold)
-        line = f'{cycletime} {lead_time_minutes:3} {threshold:5.3} {hits:6} {misses:6} {false_alarms:6} {no_det:6}'
-        lines.append(line)
 
-    # Append line to log file (one per month)
+    lines = []
+    for fcst_path in fcst_paths:    
+
+        all_fcsts = load_cube(fcst_path)
+
+        try:
+            fcst = extract_nearest_time_point(all_fcsts, obs_time, allowed_dt_difference=0)
+        except ValueError:
+            # we expect the UKV not to have matching 15 minute forcasts; so if a
+            # matching forecast is not available, exit without error
+            continue
+
+        fcst.convert_units('mm h-1')
+        lead_time_minutes = int(fcst.coord("forecast_period").points[0] / 60)
+
+        for threshold in thresholds_mmh:
+            hits, misses, false_alarms, no_det = get_counts(obs.data, fcst.data, threshold)
+            line = (f'{cycletime} {lead_time_minutes:3} {threshold:5.3} {hits:6} '
+                    f'{misses:6} {false_alarms:6} {no_det:6}')
+            lines.append(line)
+
+    # Append lines to log file
     fname = os.path.join(log_path, f'{cycletime[:6]}_{model}_counts.log')
     with open(fname, "a") as dtf:
         for line in lines:
@@ -106,7 +108,11 @@ def main(obs_path, fcst_path, log_path, model, thresholds_mmh):
 
 if __name__ == "__main__":
     """Execute with arguments"""
-    args = sys.argv[1:5]
-    args.append([float(t) for t in sys.argv[5:]])
-    main(*args)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('obs_path', type=str)
+    parser.add_argument('fcst_paths', type=str, nargs='+')
+    parser.add_argument('--log_path', type=str)
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--thresholds', type=float, nargs='+')
+    args = parser.parse_args()
+    main(args.obs_path, args.fcst_paths, args.log_path, args.model, args.thresholds)
