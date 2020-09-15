0f8ca28a3a0eee8a42a8f84f53532881df3a5993
Victoria Smart <victoria.smart@metoffice.gov.uk>
2020-09-15 13:53:26 +0100

    1273 generate metadata cli (#1326)
    * Add new CLI for creating metadata cubes
    
    - Add CLI for accepting input to create metadata cube as per the users specification.
    - Add generate_metadata() function which processes the values provided accordingly to pass to either set_up_variable_cube, set_up_percentile_cube or set_up_probability_cube. Returns the resulting cube.
    - Create tests to test generate_metadata
    - Create acceptance tests for the CLI
    - Add sha256sums of new data to SHA256SUMS
    
    * Remove redundant variables and whitespace, and update spp__relative_to_threshold
    
    * Separate create time bounds into separate function, add comma separated list of float value convertor.
    
    * Move all data array processing into _create_data_array
    
    * Black reformat
    
    * Change time_period to use minutes.
    
    - Modify time period input to be in minutes instead of hours.
    - Update tests.
    
    * Update docstrings to detail that, if creating a probability cube, the name and units inputs are for the underlying variable to which the probability field applies.
    
    * Split long docstrings onto multiples lines.
    
    * Create data as float32 type, use numpy testing to compare arrays, remove one line assertion function.
    
    * Split docstrings and comments into shorter lines.
    
    * Use local variables instead of passing each argument individually.
    
    * Create common_args variable to pass to the cube set up functions.
    
    * Change resolution to grid_spacing
    
    * Modify CLI to take single json input which can contain any values that are directly used in the metadata cube.
    
    * Remove extraction of variables from dictionary, add test to use JSON input for all cube metadata.
    
    * Move additional functions from generate-metadata-cube CLI to separate synthetic_data.utilities module
    
    - Move additional functions from generate_metadata_cube to new synthetic_data.utilities module
    - Write tests for functions
    
    * Include time, spatial_grid and height_levels in kwargs in generate_metadata.
    
    * Remove `time` and `frt` from CLI options so can only be set using `json_input`
    
    - Remove `time` and `frt` from CLI options
    - Update tests and acceptance test data
    
    * Update docstrings, remove redundant tests and modify generate_metadata_args creation.
    
    * Remove trailing whitespace.
    
    * Remove trailing whitespace.

---
+++ improver_tests/acceptance/test_generate_metadata_cube.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2020 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Tests for the generate-metadata-cube CLI."""
+
+import pytest
+
+from . import acceptance as acc
+
+pytestmark = [pytest.mark.acc, acc.skip_if_kgo_missing]
+CLI = acc.cli_name_with_dashes(__file__)
+run_cli = acc.run_cli(CLI)
+
+
+def test_default(tmp_path):
+    """Test default metadata cube generation"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_default.nc"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_ensemble_members(tmp_path):
+    """Test creating variable cube with all options set"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_ensemble_members_all_options.nc"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--name",
+        "air_temperature",
+        "--units",
+        "degrees",
+        "--spatial-grid",
+        "equalarea",
+        "--time-period",
+        "120",
+        "--ensemble-members",
+        "4",
+        "--grid-spacing",
+        "1000",
+        "--domain-corner",
+        "0,0",
+        "--npoints",
+        "50",
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_json_all_inputs(tmp_path):
+    """Test creating variable cube with all options set"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_variable_cube_json_inputs.nc"
+    json_input_path = kgo_dir / "variable_cube_all_inputs.json"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--json-input",
+        json_input_path,
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_realization_json(tmp_path):
+    """Test variable/realization metadata cube generated using leading dimension json
+    input"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_realization.nc"
+    realizations_path = kgo_dir / "realizations.json"
+    output_path = tmp_path / "output.nc"
+    args = ["--json-input", realizations_path, "--output", output_path]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_percentile_cube(tmp_path):
+    """Test percentile metadata cube generated using leading dimension json input"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_percentile.nc"
+    percentiles_path = kgo_dir / "percentiles.json"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--json-input",
+        percentiles_path,
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_probability_cube(tmp_path):
+    """Test probability metadata cube generated using leading dimension json input"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_probability.nc"
+    thresholds_path = kgo_dir / "thresholds.json"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--json-input",
+        thresholds_path,
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_height_levels(tmp_path):
+    """Test metadata cube generated with height levels from json"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_height_levels.nc"
+    height_levels_path = kgo_dir / "height_levels.json"
+    output_path = tmp_path / "output.nc"
+    args = ["--json-input", height_levels_path, "--output", output_path]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_single_height_level(tmp_path):
+    """Test metadata cube generation giving single value (rather than comma separated
+    list) for height levels option demotes height to scalar coordinate"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_single_height_level.nc"
+    height_level_path = kgo_dir / "single_height_level.json"
+    output_path = tmp_path / "output.nc"
+    args = ["--json-input", height_level_path, "--output", output_path]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_pressure_levels(tmp_path):
+    """Test metadata cube generated with pressure in Pa instead of height in metres"""
+    kgo_dir = acc.kgo_root() / "generate-metadata-cube"
+    kgo_path = kgo_dir / "kgo_pressure_levels.nc"
+    pressure_levels_path = kgo_dir / "pressure_levels.json"
+    output_path = tmp_path / "output.nc"
+    args = [
+        "--json-input",
+        pressure_levels_path,
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
---
+++ improver_tests/synthetic_data/test_utilities.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown Copyright 2017-2020 Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""
+Unit tests for synthetic data utilities.
+"""
+
+import numpy as np
+import pytest
+
+from improver.synthetic_data import utilities
+
+
+@pytest.mark.parametrize(
+    "coord_data,expected_cube_type",
+    [
+        ({"realizations": [0], "thresholds": [0]}, None),
+        ({"realizations": [0, 1, 2]}, "variable"),
+        ({"percentiles": [10, 90]}, "percentile"),
+        ({"thresholds": [2, 3, 4]}, "probability"),
+    ],
+)
+def test_get_leading_dimension(coord_data, expected_cube_type):
+    """ Tests leading dimension data extracted from dictionary and the correct cube
+    type is assigned, or if more than one leading dimension present raises an error """
+    if expected_cube_type is None:
+        msg = "Only one"
+        with pytest.raises(ValueError, match=msg):
+            utilities.get_leading_dimension(coord_data=coord_data)
+    else:
+        leading_dimension, cube_type = utilities.get_leading_dimension(
+            coord_data=coord_data
+        )
+
+        dimension_key = list(coord_data)[0]
+        np.testing.assert_array_equal(coord_data[dimension_key], leading_dimension)
+        assert expected_cube_type == cube_type
+
+
+@pytest.mark.parametrize(
+    "coord_data,expected_pressure",
+    [
+        ({"realizations": [0]}, False),
+        ({"heights": [0, 1, 2]}, False),
+        ({"pressures": [10, 20, 30]}, True),
+    ],
+)
+def test_get_height_levels(coord_data, expected_pressure):
+    """ Tests height level data extracted successfully and pressure flag set correctly """
+    dimension_key = list(coord_data)[0]
+
+    if dimension_key == "realizations":
+        expected_height_levels = None
+    else:
+        expected_height_levels = coord_data[dimension_key]
+
+    height_levels, pressure = utilities.get_height_levels(coord_data=coord_data)
+
+    np.testing.assert_array_equal(expected_height_levels, height_levels)
+    assert expected_pressure == pressure
