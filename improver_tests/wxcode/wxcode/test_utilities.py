1ac9d084de6899184e041f17aa5ad34bbdde71c1
Gavin Evans <gavin.evans@metoffice.gov.uk>
2020-09-22 14:30:48 +0100

    Modifications to tests following updating the global weather symbol decision tree.

--- improver_tests/wxcode/wxcode/test_utilities.py
+++ improver_tests/wxcode/wxcode/test_utilities.py
@@ -518,6 +518,7 @@ class Test_interrogate_decision_tree(IrisTest):
             "global",
             (
                 "probability_of_cloud_area_fraction_above_threshold (1): 0.1875, 0.8125\n"
+                "probability_of_convective_ratio_above_threshold (1): 0.5\n"
                 "probability_of_low_type_cloud_area_fraction_above_threshold (1): 0.85\n"
                 "probability_of_lwe_precipitation_rate_above_threshold (mm hr-1): 0.1, 1.0\n"
                 "probability_of_lwe_snowfall_rate_above_threshold (mm hr-1): 0.1, 1.0\n"
@@ -545,6 +546,7 @@ class Test_interrogate_decision_tree(IrisTest):
 def test_interrogate_decision_tree(tree_name, expected):
     """Test that the function returns the right strings."""
     result = interrogate_decision_tree(tree_name)
+    print("result = ", result)
     assert result == expected
 
 
