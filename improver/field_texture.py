3be6cc8ec54ef5d44d0af5d6d237eba4a53e4f31
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2020-09-23 09:18:32 +0100

    The incorrect threshold is being used. This PR corrects this mistake.

--- improver/field_texture.py
+++ improver/field_texture.py
@@ -261,7 +261,7 @@ class FieldTexture(BasePlugin):
             ratios.append(self._calculate_ratio(cslice, cube_name, self.nbhood_radius))
 
         ratios = ratios.merge_cube()
-        thresholded = BasicThreshold(self.diagnostic_threshold).process(ratios)
+        thresholded = BasicThreshold(self.textural_threshold).process(ratios)
 
         # Squeeze scalar threshold coordinate.
         try:
