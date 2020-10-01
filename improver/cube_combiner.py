8e94105de86a0148c13ee112d36207eb55a1d293
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2020-10-01 12:15:12 +0100

    Modifications to cube combiner to keep probabilistic naming elements.
    These probabilistic naming elements are only preserved when broadcasting
    to a threshold coordinate has been used. This ensures that the
    information about how the data has been thresholded is preserved. The
    naming elements are taken from the first cube in the input cubelist
    which is always expected to include the coordinate to which broadcasting
    is happening for subsequent cubes.

--- improver/cube_combiner.py
+++ improver/cube_combiner.py
@@ -36,7 +36,10 @@ from iris.cube import CubeList
 from iris.exceptions import CoordinateNotFoundError
 
 from improver import BasePlugin
-from improver.metadata.probabilistic import find_threshold_coordinate
+from improver.metadata.probabilistic import (
+    find_threshold_coordinate,
+    extract_diagnostic_name,
+)
 from improver.utilities.cube_manipulation import (
     enforce_coordinate_ordering,
     expand_bounds,
@@ -282,6 +285,13 @@ class CubeCombiner(BasePlugin):
                 result = expand_bounds(
                     result, cube_list, expanded_coord_names, use_midpoint=use_midpoint
                 )
+
+        if self.broadcast_coords and "threshold" in self.broadcast_coords:
+            probabilistic_name = cube_list[0].name()
+            diagnostic_name = extract_diagnostic_name(probabilistic_name)
+            new_diagnostic_name = probabilistic_name.replace(
+                diagnostic_name, new_diagnostic_name
+            )
 
         result.rename(new_diagnostic_name)
 
