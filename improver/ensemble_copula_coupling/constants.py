49e4735c77741e95df519003745ec610965fe98d
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2020-11-02 11:06:55 +0000

    Adds sleet accumulation bounds information to ensemble copula coupling constants.

--- improver/ensemble_copula_coupling/constants.py
+++ improver/ensemble_copula_coupling/constants.py
@@ -71,6 +71,7 @@ BOUNDS_FOR_ECDF = {
     "relative_humidity": Bounds((0, 1.2), "1"),
     "lwe_thickness_of_precipitation_amount": Bounds((0, 0.5), "m"),
     "lwe_thickness_of_precipitation_amount_in_vicinity": Bounds((0, 0.5), "m"),
+    "lwe_thickness_of_sleetfall_amount": Bounds((0, 0.5), "m"),
     "lwe_thickness_of_snowfall_amount": Bounds((0, 0.5), "m"),
     "lwe_sleetfall_rate": Bounds((0, 128.0), "mm h-1"),
     "lwe_snowfall_rate": Bounds((0, 128.0), "mm h-1"),
