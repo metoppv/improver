2326bcf09a197bf1f5a6dc17e42b04f814a79cd6
Kathryn.Howard <kathryn.howard@metoffice.gov.uk>
2020-11-27 14:13:48 +0000

    isort changes

--- improver/threshold.py
+++ improver/threshold.py
@@ -33,15 +33,13 @@
 
 import operator
 
-import iris
 import numpy as np
-from cf_units import Unit
 
+import iris
+from cf_units import Unit
 from improver import PostProcessingPlugin
 from improver.metadata.constants import FLOAT_DTYPE
-from improver.metadata.probabilistic import (
-	probabilistic_data_above_or_below,
-)
+from improver.metadata.probabilistic import probabilistic_data_above_or_below
 from improver.utilities.cube_manipulation import enforce_coordinate_ordering
 from improver.utilities.rescale import rescale
 
