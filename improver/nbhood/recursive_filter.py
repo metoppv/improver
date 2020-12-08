e0115dc095ea6dab5f99786027ad5a921c51162d
Caroline Sandford <caroline.sandford@metoffice.gov.uk>
2020-12-08 10:46:34 +0000

    Removed VDI latency typos.

--- improver/nbhood/recursive_filter.py
+++ improver/nbhood/recursive_filter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# (C) British Crown Copyright  2017-2020 Met Office.
+# (C) British Crown Copyright 2017-2020 Met Office.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 #
-# * Redistributions in binary form must reproduce the         above copyright notice,
+# * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
 #
