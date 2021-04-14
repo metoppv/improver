5dcf7398e4d328e1997dbd054eba3f0ed38f5e6c
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2021-04-14 08:41:16 +0100

    Modified behaviour to be more useful.

--- improver/cli/interpret_metadata.py
+++ improver/cli/interpret_metadata.py
@@ -35,7 +35,7 @@ from improver import cli
 
 
 @cli.clizefy
-def process(*cubes: cli.inputcube, verbose=False):
+def process(*file_paths: cli.inputpath, verbose=False, failures=False):
     """Intepret the metadata of an IMPROVER output into human readable format
     according to the IMPROVER standard. An optional verbosity flag, if set to
     True, will specify the source of each interpreted element.
@@ -44,24 +44,35 @@ def process(*cubes: cli.inputcube, verbose=False):
     metadata within the code base.
 
     Args:
-        cubes (iris.cube.Cube or list of iris.cube.Cube):
-            The cube(s) for which the metadata should be interpreted.
+        file_paths (list of Path objects):
+            File paths to netCDF files for which the metadata should be interpreted.
         verbose (bool):
             Boolean flag to output information about sources of metadata
-            interpretation
-
-    Returns:
-        str:
-            A human-readable interpretation of the cube's metadata is returned
-            to standard out.
+            interpretation.
+        failures (bool):
+            Boolean flag that, if set, means only information about non-compliant
+            files is printed.
     """
+    from iris import load
     from improver.developer_tools.metadata_interpreter import (
         MOMetadataInterpreter,
         display_interpretation,
     )
 
-    for cube in cubes:
-        interpreter = MOMetadataInterpreter()
-        interpreter.run(cube)
-        output = display_interpretation(interpreter, verbose=verbose)
-        print(output)
+    cubelists = [load(file_path.as_posix()) for file_path in file_paths]
+
+    output = ''
+    for file, cubelist in zip(file_paths, cubelists):
+        for cube in cubelist:
+            interpreter = MOMetadataInterpreter()
+            try:
+                interpreter.run(cube)
+            except ValueError as err:
+                output = "Non-compliant :\n{}".format(str(err))
+            else:
+                if failures:
+                    continue
+                output = display_interpretation(interpreter, verbose=verbose)
+            print(f'\nfile : {file}')
+            print(f'cube name : {cube.name()}')
+            print(output)
--- improver_tests/acceptance/test_interpret_metadata.py
+++ improver_tests/acceptance/test_interpret_metadata.py
@@ -41,28 +41,22 @@ pytestmark = [pytest.mark.acc]
 CLI = acc.cli_name_with_dashes(__file__)
 run_cli = acc.run_cli(CLI)
 
-
-def test_single_file(capsys):
-    """Test metadata interpretation of a single file"""
-    kgo_dir = acc.kgo_root() / "interpret_metadata"
-    input_path = kgo_dir / "temperature_realizations.nc"
-    KGO = """
+KGO_compliant = """
+cube name : probability_of_air_temperature_above_threshold
 This is a gridded probabilities file
 It contains probabilities of air temperature greater than or equal to thresholds
 It has undergone some significant post-processing
 It contains data from MOGREPS-UK
 """
-    args = [input_path]
-    run_cli(args)
-    captured = capsys.readouterr()
-    assert KGO in captured.out
 
+KGO_non_compliant = """
+cube name : weather_code
+Non-compliant :
+Attributes dict_keys(['institution', 'title', 'weather_code', 'weather_code_meaning', 'Conventions']) missing one or more mandatory values ['source']
+No mosg__model_configuration on blended file
+"""
 
-def test_single_file_verbose(capsys):
-    """Test metadata verbosity can be set"""
-    kgo_dir = acc.kgo_root() / "interpret_metadata"
-    input_path = kgo_dir / "temperature_realizations.nc"
-    KGO = """
+KGO_verbose = """
 This is a gridded probabilities file
     Source: name, coordinates
 It contains probabilities of air temperature greater than or equal to thresholds
@@ -71,35 +65,37 @@ It has undergone some significant post-processing
     Source: title attribute
 It contains data from MOGREPS-UK
     Source: model ID attribute
-
 """
-    args = [input_path, "--verbose"]
-    run_cli(args)
-    captured = capsys.readouterr()
-    assert KGO in captured.out
+ALL_KGOS = [KGO_compliant, KGO_non_compliant, KGO_verbose]
 
-
-def test_multiple_files(capsys):
-    """Test metadata interpretation of multiple files"""
+@pytest.mark.parametrize(
+    "inputs,kgos,options",
+    (
+        (["temperature_realizations.nc"], [KGO_compliant], []),
+        (["temperature_realizations.nc"], [KGO_verbose], ["--verbose"]),
+        (["non_compliant_weather_codes.nc", "temperature_realizations.nc"],
+         [KGO_compliant, KGO_non_compliant], []),
+        (["non_compliant_weather_codes.nc", "temperature_realizations.nc"],
+         [KGO_non_compliant], ["--failures"]),
+    ),
+)
+def test_interpretation(capsys, inputs, kgos, options):
+    """Test metadata interpretation. Four tests are run:
+    - A single compliant file
+    - A single compliant file with verbose output
+    - Multiple files, the first of which is non-compliant
+    - Using the --failures option to only print output for non-compliant files.
+    """
     kgo_dir = acc.kgo_root() / "interpret_metadata"
-    input_path1 = kgo_dir / "temperature_realizations.nc"
-    input_path2 = kgo_dir / "precipacc_percentiles.nc"
-    KGO = """
-This is a gridded probabilities file
-It contains probabilities of air temperature greater than or equal to thresholds
-It has undergone some significant post-processing
-It contains data from MOGREPS-UK
-
-This is a gridded percentiles file
-It contains percentiles of lwe thickness of precipitation amount
-These percentiles are of lwe thickness of precipitation amount sum over time
-It has undergone some significant post-processing
-It contains blended data from models: MOGREPS-G, MOGREPS-UK
-"""
-    args = [input_path1, input_path2]
+    input_path = [kgo_dir / input for input in inputs]
+    args = [*input_path, *options]
     run_cli(args)
     captured = capsys.readouterr()
-    assert KGO in captured.out
+    for kgo in kgos:
+        assert kgo in captured.out
+    excluded_kgos = list(set(ALL_KGOS)^set(kgos))
+    for kgo in excluded_kgos:
+        assert kgo not in captured.out
 
 
 def test_no_output_option_accepted(capsys):
