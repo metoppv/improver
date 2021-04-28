6ea84e8554c0087f22873867a9d00b92f62778fa
Daniel Mentiplay <daniel.mentiplay@bom.gov.au>
2021-04-28 11:21:58 +1000

    Use rst not md for developer guide in docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,6 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
-    "myst_parser",
 ]
 
 autodoc_default_flags = ["members", "private-members"]
@@ -84,8 +83,8 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = [".rst", ".md"]
-# source_suffix = ".rst"
+# source_suffix = [".rst", ".md"]
+source_suffix = ".rst"
 
 # The encoding of source files.
 #
