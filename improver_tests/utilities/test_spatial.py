a463397bf27b41360ff448e4020a39db6f42d687
Zhiliang Fan <zhiliang.fan@bom.gov.au>
2021-05-11 16:09:10 +1000

    add more test

--- improver_tests/utilities/test_spatial.py
+++ improver_tests/utilities/test_spatial.py
@@ -202,10 +202,19 @@ class Test_calculate_grid_spacing(IrisTest):
         """Test outputs with lat-lon grid in degrees"""
         result = calculate_grid_spacing(self.lat_lon_cube, "degrees")
         self.assertAlmostEqual(result, 10.0)
+
+    def test_lat_lon_equal_spacing_with_tolerance(self):
         cube_copy = self.lat_lon_cube.copy()
         cube_copy.coord("longitude").points = [-19.99999, -10.0, 0.0, 10.0, 20.00001]
         result = calculate_grid_spacing(cube_copy, "degrees")
         self.assertAlmostEqual(result, 10.0)
+
+    def test_lat_lon_not_equal_spacing(self):
+        cube_copy = self.lat_lon_cube.copy()
+        cube_copy.coord("longitude").points = [-19.998, -10.0, 0.0, 10.0, 20.00001]
+        msg = "Coordinate longitude points are not equally spaced"
+        with self.assertRaisesRegex(ValueError, msg):
+            calculate_grid_spacing(cube_copy, "degrees")
 
     def test_incorrect_units(self):
         """Test ValueError for incorrect units"""
