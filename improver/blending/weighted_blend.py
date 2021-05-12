995eca6cedf76330506368b02c2126c8542141a5
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2021-05-12 08:01:54 +0100

    Ran isort

--- improver/blending/weighted_blend.py
+++ improver/blending/weighted_blend.py
@@ -44,9 +44,7 @@ from numpy import ndarray
 
 from improver import BasePlugin, PostProcessingPlugin
 from improver.blending import MODEL_BLEND_COORD, MODEL_NAME_COORD
-from improver.blending.utilities import (
-    find_blend_dim_coord,
-)
+from improver.blending.utilities import find_blend_dim_coord
 from improver.metadata.constants import FLOAT_DTYPE, PERC_COORD
 from improver.metadata.forecast_times import rebadge_forecasts_as_latest_cycle
 from improver.utilities.cube_manipulation import (
