cfa01faef4e1e3d5e942761b532026b21cf68214
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2021-05-17 11:41:56 +0100

    Update acceptance test to check everything is working.

--- improver_tests/acceptance/test_spot_extract.py
+++ improver_tests/acceptance/test_spot_extract.py
@@ -41,6 +41,9 @@ CLI = acc.cli_name_with_dashes(__file__)
 run_cli = acc.run_cli(CLI)
 
 
+UK_SPOT_TITLE = "IMPROVER UK Spot Values"
+
+
 @pytest.mark.parametrize(
     "neighbour_cube,extra_args,kgo_file",
     (
@@ -56,7 +59,15 @@ def test_nearest_uk(tmp_path, neighbour_cube, extra_args, kgo_file):
     diag_path = kgo_dir / "inputs/ukvx_temperature.nc"
     kgo_path = kgo_dir / f"outputs/{kgo_file}"
     output_path = tmp_path / "output.nc"
-    args = [neighbour_path, diag_path, "--output", output_path, *extra_args]
+    args = [
+        neighbour_path,
+        diag_path,
+        "--output",
+        output_path,
+        *extra_args,
+        "--new-title",
+        UK_SPOT_TITLE,
+    ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
 
@@ -76,6 +87,8 @@ def test_lapse_rate_adjusted_uk(tmp_path):
         "--output",
         output_path,
         "--apply-lapse-rate-correction",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -118,6 +131,8 @@ def test_lapse_rate_mismatch(tmp_path):
         "--output",
         output_path,
         "--apply-lapse-rate-correction",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     with pytest.warns(UserWarning, match=".*height.*not adjusted.*"):
         run_cli(args)
@@ -138,6 +153,8 @@ def test_lapse_rate_wrong_height(tmp_path):
         "--output",
         output_path,
         "--apply-lapse-rate-correction",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     with pytest.raises(ValueError, match=".*single valued height.*"):
         run_cli(args)
@@ -154,7 +171,7 @@ def test_new_spot_title(tmp_path):
         neighbour_path,
         diag_path,
         "--new-title",
-        "IMPROVER Spot Values",
+        UK_SPOT_TITLE,
         "--output",
         output_path,
     ]
@@ -195,6 +212,8 @@ def test_multiple_constraints(tmp_path):
         output_path,
         "--similar-altitude",
         "--land-constraint",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -214,6 +233,8 @@ def test_percentile_thresholded_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "50",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -233,6 +254,8 @@ def test_percentile_percentile_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "50",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -270,6 +293,8 @@ def test_percentile_deterministic(tmp_path):
         output_path,
         "--extract-percentiles",
         "50",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     with pytest.warns(UserWarning) as collected_warns:
         run_cli(args)
@@ -295,6 +320,8 @@ def test_percentile_deterministic_quiet(tmp_path):
         output_path,
         "--extract-percentiles",
         "50",
+        "--new-title",
+        UK_SPOT_TITLE,
         "--suppress-warnings",
     ]
     with pytest.warns(None) as collected_warns:
@@ -318,6 +345,8 @@ def test_multiple_percentile_thresholded_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "25, 50, 75",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -337,6 +366,8 @@ def test_multiple_percentile_percentile_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "25, 50, 75",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -356,6 +387,8 @@ def test_percentile_realization_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "50",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -375,6 +408,8 @@ def test_multiple_percentile_realization_input(tmp_path):
         output_path,
         "--extract-percentiles",
         "25, 50, 75",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
@@ -394,6 +429,8 @@ def test_invalid_lapse_rate(tmp_path):
         "--output",
         output_path,
         "--apply-lapse-rate-correction",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     with pytest.raises(ValueError, match=".*lapse rate.*"):
         run_cli(args)
@@ -411,6 +448,8 @@ def test_no_lapse_rate_data(tmp_path):
         "--output",
         output_path,
         "--apply-lapse-rate-correction",
+        "--new-title",
+        UK_SPOT_TITLE,
     ]
     with pytest.warns(UserWarning, match=".*lapse rate.*"):
         run_cli(args)
