8986c0a8844a8687ffe7778240b8ff3a8de2517c
Belinda Trotta <btrotta-bom@users.noreply.github.com>
2021-10-13 14:02:08 +1100

    Fix licence text

--- improver/ensemble_copula_coupling/numba_utilities.py
+++ improver/ensemble_copula_coupling/numba_utilities.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
 # (C) British Crown Copyright 2017-2021 Met Office.
 # All rights reserved.
 #
