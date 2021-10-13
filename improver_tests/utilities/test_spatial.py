381a1c95e9f09740fc0d86676f517abce81d46fe
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2021-10-13 12:06:37 +0100

    Modifies lat-lon test to check both x and y axes

--- improver_tests/utilities/test_spatial.py
+++ improver_tests/utilities/test_spatial.py
@@ -249,10 +249,15 @@ class Test_calculate_grid_spacing_with_tolerance(GridSpacingTest):
         self.assertAlmostEqual(result, self.expected)
 
     def test_lat_lon_negative_spacing(self):
-        """Test negative-striding x-axis grid spacing is positive with lat-lon grid in degrees"""
-        self.lat_lon_cube.coord("longitude").points = self.longitude_points[::-1]
-        result = calculate_grid_spacing(self.lat_lon_cube, "degrees", rtol=self.rtol)
-        self.assertAlmostEqual(result, self.expected)
+        """Test negative-striding axes grid spacing is positive with lat-lon grid in degrees"""
+        for axis in "yx":
+            self.lat_lon_cube.coord(axis=axis).points = self.lat_lon_cube.coord(
+                axis=axis
+            ).points[::-1]
+            result = calculate_grid_spacing(
+                self.lat_lon_cube, "degrees", rtol=self.rtol, axis=axis
+            )
+            self.assertAlmostEqual(result, self.expected)
 
     def test_lat_lon_not_equal_spacing(self):
         """Test outputs with lat-lon grid in degrees"""
