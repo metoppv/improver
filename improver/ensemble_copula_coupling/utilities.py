62551b11ecb97e0690c45ef8a971b4c1305015fb
cpelley <carwyn.pelley@metoffice.gov.uk>
2021-11-12 10:28:05 +0000

    MAINT: Proposed review suggestions

--- improver/ensemble_copula_coupling/utilities.py
+++ improver/ensemble_copula_coupling/utilities.py
@@ -308,19 +308,28 @@ def slow_interp_same_y(x: np.ndarray, xp: np.ndarray, fp: np.ndarray) -> np.ndar
     return result
 
 
-try:
-    import numba  # noqa: F401
+def interpolate_multiple_rows_same_y(*args):
+    """For each row i of xp, do the equivalent of np.interp(x, xp[i], fp).
 
-    from improver.ensemble_copula_coupling.numba_utilities import fast_interp_same_y
+    Calls a fast numba implementation where numba is available (see
+    `improver.ensemble_copula_coupling.numba_utilities.fast_interp_same_y`) and calls a
+    the native python implementation otherwise (see :func:`slow_interp_same_y`).
 
-    def interpolate_multiple_rows_same_y(*args):
+    Args:
+        x: 1-d array
+        xp: n * m array, each row must be in non-decreasing order
+        fp: 1-d array with length m
+    Returns:
+        n * len(x) array where each row i is equal to np.interp(x, xp[i], fp)
+    """
+    try:
+        import numba  # noqa: F401
+
+        from improver.ensemble_copula_coupling.numba_utilities import fast_interp_same_y
+
         return fast_interp_same_y(*args)
-
-
-except ImportError:
-    warnings.warn(
-        "Module numba unavailable. ConvertProbabilitiesToPercentiles will be slower."
-    )
-
-    def interpolate_multiple_rows_same_y(*args):
+    except ImportError:
+        warnings.warn(
+            "Module numba unavailable. ConvertProbabilitiesToPercentiles will be slower."
+        )
         return slow_interp_same_y(*args)
