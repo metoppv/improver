bf64d564d503057a954128da898ac0cf44c83319
Lucy Liu <lucy.liu@bom.gov.au>
2022-02-10 18:03:55 +1100

    implmenent point by point

--- improver/calibration/reliability_calibration.py
+++ improver/calibration/reliability_calibration.py
@@ -935,7 +935,74 @@ class ManipulateReliabilityTable(BasePlugin):
         reliability_table.replace_coord(probability_bin_coord)
         return reliability_table
 
-    def process(self, reliability_table: Cube) -> CubeList:
+    def _enforce_min_count_and_montonicity(self, rel_table_slice: Cube) -> Cube:
+        """Apply the steps needed to produce a reliability diagram on a single
+        slice of reliability table cube.
+
+        Args:
+            reliability_table_slice:
+                The reliability table slice to be manipulated. The only
+                coordinates expected on this cube are a table_row_index
+                coordinate and corresponding table_row_name coordinate and a
+                probability_bin coordinate.
+        Returns:
+            Processed reliability table slice, with reliability steps applied.
+        """
+        (
+            observation_count,
+            forecast_probability_sum,
+            forecast_count,
+            probability_bin_coord,
+        ) = self._extract_reliability_table_components(rel_table_slice)
+
+        if np.any(forecast_count < self.minimum_forecast_count):
+            (
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            ) = self._combine_undersampled_bins(
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            )
+            rel_table_slice = self._update_reliability_table(
+                rel_table_slice,
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            )
+
+        # If the observation frequency is non-monotonic adjust the
+        # reliability table
+        observation_frequency = np.array(observation_count / forecast_count)
+        if not np.all(np.diff(observation_frequency) >= 0):
+            (
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            ) = self._combine_bin_pair(
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            )
+            observation_count = self._assume_constant_observation_frequency(
+                observation_count, forecast_count
+            )
+            rel_table_slice = self._update_reliability_table(
+                rel_table_slice,
+                observation_count,
+                forecast_probability_sum,
+                forecast_count,
+                probability_bin_coord,
+            )
+        return rel_table_slice
+
+    def process(self, reliability_table: Cube, point_by_point: bool) -> CubeList:
         """
         Apply the steps needed to produce a reliability diagram with a
         monotonic observation frequency.
@@ -946,6 +1013,8 @@ class ManipulateReliabilityTable(BasePlugin):
                 expected on this cube are a threshold coordinate,
                 a table_row_index coordinate and corresponding table_row_name
                 coordinate and a probability_bin coordinate.
+            point_by_point:
+                Whether to process each point in the input cube independently.
 
         Returns:
             CubeList containing a reliability table cube for each threshold in
@@ -959,61 +1028,24 @@ class ManipulateReliabilityTable(BasePlugin):
             to reach the minimum_forecast_count.
         """
         threshold_coord = find_threshold_coordinate(reliability_table)
+        if point_by_point:
+            y_name = reliability_table.coord(axis="y").name()
+            x_name = reliability_table.coord(axis="x").name()
+
         reliability_table_cubelist = iris.cube.CubeList()
-        for rel_table_slice in reliability_table.slices_over(threshold_coord):
-            (
-                observation_count,
-                forecast_probability_sum,
-                forecast_count,
-                probability_bin_coord,
-            ) = self._extract_reliability_table_components(rel_table_slice)
-
-            if np.any(forecast_count < self.minimum_forecast_count):
-                (
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
-                ) = self._combine_undersampled_bins(
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
+        for rel_table_threshold in reliability_table.slices_over(threshold_coord):
+            if point_by_point:
+                rel_table_points_cubelist = iris.cube.CubeList()
+                for rel_table_point in rel_table_threshold.slices_over([y_name, x_name]):
+                    rel_table_points_cubelist.append(
+                        self._enforce_min_count_and_montonicity(rel_table_point)
+                    )
+                rel_table_threshold = rel_table_points_cubelist.merge_cube()
+            else:
+                rel_table_threshold = self._enforce_min_count_and_montonicity(
+                    rel_table_threshold
                 )
-                rel_table_slice = self._update_reliability_table(
-                    rel_table_slice,
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
-                )
-
-            # If the observation frequency is non-monotonic adjust the
-            # reliability table
-            observation_frequency = np.array(observation_count / forecast_count)
-            if not np.all(np.diff(observation_frequency) >= 0):
-                (
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
-                ) = self._combine_bin_pair(
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
-                )
-                observation_count = self._assume_constant_observation_frequency(
-                    observation_count, forecast_count
-                )
-                rel_table_slice = self._update_reliability_table(
-                    rel_table_slice,
-                    observation_count,
-                    forecast_probability_sum,
-                    forecast_count,
-                    probability_bin_coord,
-                )
-            reliability_table_cubelist.append(rel_table_slice)
+            reliability_table_cubelist.append(rel_table_threshold)
         return reliability_table_cubelist
 
 
