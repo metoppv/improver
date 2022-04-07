Fix units

improver/calibration/dataframe_utilities.py
@@ -475,7 +475,7 @@ def forecast_dataframe_to_cube(
             A string such as "<", "lt", "LT", etc that can be interpreted as a
             comparison operator by
             :func:`~improver.utilities.probability_manipulation.comparison_operator_dict`
-            
+
     Raises:
         ValueError:
             If forecast representation is threshold, and comparison operator is
@@ -558,7 +558,7 @@ def forecast_dataframe_to_cube(
                     np.float32(var_val),
                     standard_name=cf_name,
                     var_name="threshold",
-                    units="1",
+                    units=var_df["units"].values[0],
                 )
                 var_coord.attributes.update({"spp__relative_to_threshold": spp_string})
             elif representation_type == "percentile":
@@ -684,7 +684,7 @@ def truth_dataframe_to_cube(
                     np.float32(var_val),
                     standard_name=cf_name,
                     var_name="threshold",
-                    units="1",
+                    units=var_df["units"].values[0],
                 )
                 var_coord.attributes.update({"spp__relative_to_threshold": spp_string})
 

improver_tests/calibration/dataframe_utilities/test_dataframe_utilities.py
@@ -248,7 +248,7 @@ class SetupConstructedForecastCubes(SetupSharedDataFrames):
         threshold_fc.rename(f"probability_of_{self.cf_name}_above_threshold")
         threshold_fc.coord("realization").rename(self.cf_name)
         threshold_fc.coord(self.cf_name).var_name = "threshold"
-        threshold_fc.coord(self.cf_name).units = "1"
+        threshold_fc.coord(self.cf_name).units = "Celsius"
         threshold_fc.coord(self.cf_name).points = self.thresholds
         threshold_fc.coord(self.cf_name).attributes[
             "spp__relative_to_threshold"
@@ -308,7 +308,7 @@ class SetupConstructedTruthCubes(SetupSharedDataFrames):
                 np.float32(threshold),
                 standard_name=self.cf_name,
                 var_name="threshold",
-                units="1",
+                units="Celsius",
             )
             threshold_coord.attributes["spp__relative_to_threshold"] = "greater_than"
             threshold_cube.add_aux_coord(threshold_coord)