d91da86fe3b99714be079db65bb8b882b39831ec
Tom Gale <tom.gale@bom.gov.au>
2022-05-25 11:23:56 +1000

    Add docstring and move import inside function

--- improver_tests/conftest.py
+++ improver_tests/conftest.py
@@ -31,11 +31,24 @@
 """Test wide setup and configuration"""
 
 import pytest
-from threadpoolctl import threadpool_limits
-
 
 @pytest.fixture(autouse=True)
 def thread_control(monkeypatch):
-    with threadpool_limits(limits=1):
+    """
+    Wrap all tests with a limit to one thread via threadpoolctl.
+
+    The threadpoolctl library handles a variety of numerical libraries including
+    OpenBLAS, MKL and OpenMP, using their library specific interfaces during runtime.
+    Environment variable settings for these need to be applied before starting the
+    python interpreter, which is not possible from inside pytest.
+    This limitation to one thread avoids thread contention when parallel processing
+    is handled at larger scale such as Dask or pytest-xdist. See dask documentation:
+    https://docs.dask.org/en/stable/array-best-practices.html#avoid-oversubscribing-threads
+    """
+    try:
+        from threadpoolctl import threadpool_limits
+        with threadpool_limits(limits=1):
+            yield
+    except ModuleNotFoundError:
         yield
     return
