d9053b532573c4d608d2b85234132bc9fe87de11
Benjamin Owen <benjamin.owen@bom.gov.au>
2022-06-03 16:24:17 +1000

    Refactor RainForests tests to work over subclasses.

---
+++ improver_tests/calibration/rainforests_calibration/test_ApplyRainForestsCalibration.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Unit tests for the ApplyRainForestsCalibration class."""
+import sys
+
+import pytest
+
+from improver.calibration.rainforest_calibration import ApplyRainForestsCalibration
+
+try:
+    import treelite_runtime
+except ModuleNotFoundError:
+    TREELITE_ENABLED = False
+else:
+    TREELITE_ENABLED = True
+
+lightgbm = pytest.importorskip("lightgbm")
+treelite_available = pytest.mark.skipif(
+    not TREELITE_ENABLED, reason="Required dependency missing."
+)
+
+
+class MockBooster:
+    def __init__(self, model_file, **kwargs):
+        self.model_class = "lightgbm-Booster"
+        self.model_file = model_file
+
+    def reset_parameter(self, params):
+        self.threads = params.get("num_threads")
+        return self
+
+
+class MockPredictor:
+    def __init__(self, libpath, nthread, **kwargs):
+        self.model_class = "treelite-Predictor"
+        self.threads = nthread
+        self.model_file = libpath
+
+
+@pytest.mark.parametrize("lightgbm_keys", (True, False))
+@pytest.mark.parametrize("treelite_model", (TREELITE_ENABLED, False))
+@pytest.mark.parametrize("treelite_file", (True, False))
+def test__new__(
+    lightgbm_keys, treelite_model, treelite_file, monkeypatch, model_config,
+):
+    """Test treelite models are loaded if model_config correctly defines them. If all thresholds
+    contain treelite model AND the treelite module is available, treelite Predictor is returned,
+    otherwise return lightgbm Boosters. Checks outputs are ordered when inputs can be unordered.
+    If neither treelite nor lightgbm configs are complete, a ValueError is expected."""
+    if treelite_model:
+        monkeypatch.setattr(treelite_runtime, "Predictor", MockPredictor)
+    else:
+        monkeypatch.setitem(sys.modules, "treelite_runtime", None)
+    monkeypatch.setattr(lightgbm, "Booster", MockBooster)
+    if not treelite_file:
+        # Model type should default to lightgbm if there are any treelite models
+        # missing across any thresholds
+        model_config["0.0000"].pop("treelite_model", None)
+    if not lightgbm_keys:
+        model_config["0.0000"].pop("lightgbm_model", None)
+
+    if treelite_model and treelite_file:
+        expected_class = "ApplyRainForestsCalibrationTreelite"
+    elif lightgbm_keys:
+        expected_class = "ApplyRainForestsCalibrationLightGBM"
+    else:
+        with pytest.raises(ValueError, match="Path to lightgbm model missing"):
+            ApplyRainForestsCalibration(model_config)
+        return
+
+    result = ApplyRainForestsCalibration(model_config)
+    assert type(result).__name__ == expected_class
--- improver_tests/calibration/rainforests_calibration/test_ApplyRainForestsCalibrationLightGBM.py
+++ improver_tests/calibration/rainforests_calibration/test_ApplyRainForestsCalibrationLightGBM.py
@@ -29,26 +29,16 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """Unit tests for the ApplyRainForestsCalibration class."""
-import sys
-
 import numpy as np
 import pytest
 from iris import Constraint
 
-from improver.calibration.rainforest_calibration import ApplyRainForestsCalibration
+from improver.calibration.rainforest_calibration import (
+    ApplyRainForestsCalibrationLightGBM,
+)
 from improver.synthetic_data.set_up_test_cubes import set_up_variable_cube
 
-try:
-    import treelite_runtime
-except ModuleNotFoundError:
-    TREELITE_ENABLED = False
-else:
-    TREELITE_ENABLED = True
-
 lightgbm = pytest.importorskip("lightgbm")
-treelite_available = pytest.mark.skipif(
-    not TREELITE_ENABLED, reason="Required dependency missing."
-)
 
 
 class MockBooster:
@@ -61,81 +51,51 @@ class MockBooster:
         return self
 
 
-class MockPredictor:
-    def __init__(self, libpath, nthread, **kwargs):
-        self.model_class = "treelite-Predictor"
-        self.threads = nthread
-        self.model_file = libpath
+def test__new__(model_config, monkeypatch):
+    monkeypatch.setattr(lightgbm, "Booster", MockBooster)
+    # Check that we get the expected subclass
+    result = ApplyRainForestsCalibrationLightGBM(model_config)
+    assert type(result).__name__ == "ApplyRainForestsCalibrationLightGBM"
+    model_config
+    # Test exception raised when file path is missing.
+    model_config["0.0000"].pop("lightgbm_model", None)
+    with pytest.raises(ValueError):
+        ApplyRainForestsCalibrationLightGBM(model_config)
 
 
-@pytest.mark.parametrize("lightgbm_keys", (True, False))
 @pytest.mark.parametrize("ordered_inputs", (True, False))
-@pytest.mark.parametrize("treelite_model", (TREELITE_ENABLED, False))
-@pytest.mark.parametrize("treelite_file", (True, False))
+@pytest.mark.parametrize("default_threads", (True, False))
 def test__init__(
-    lightgbm_keys,
-    ordered_inputs,
-    treelite_model,
-    treelite_file,
-    monkeypatch,
-    model_config,
-    error_thresholds,
+    model_config, ordered_inputs, default_threads, error_thresholds, monkeypatch
 ):
-    """Test treelite models are loaded if model_config correctly defines them. If all thresholds
-    contain treelite model AND the treelite module is available, treelite Predictor is returned,
-    otherwise return lightgbm Boosters. Checks outputs are ordered when inputs can be unordered.
-    If neither treelite nor lightgbm configs are complete, a ValueError is expected."""
-    if treelite_model:
-        monkeypatch.setattr(treelite_runtime, "Predictor", MockPredictor)
-    else:
-        monkeypatch.setitem(sys.modules, "treelite_runtime", None)
     monkeypatch.setattr(lightgbm, "Booster", MockBooster)
 
-    if not treelite_file:
-        # Model type should default to lightgbm if there are any treelite models
-        # missing across any thresholds
-        model_config["0.0000"].pop("treelite_model", None)
     if not ordered_inputs:
         tmp_value = model_config.pop("0.0000", None)
         model_config["0.0000"] = tmp_value
-    if not lightgbm_keys:
-        for t, d in model_config.items():
-            d.pop("lightgbm_model")
 
-    if treelite_model and treelite_file:
-        expected_class = "treelite-Predictor"
-    elif lightgbm_keys:
-        expected_class = "lightgbm-Booster"
+    if default_threads:
+        expected_threads = 1
+        result = ApplyRainForestsCalibrationLightGBM(model_config)
     else:
-        with pytest.raises(ValueError, match="Path to lightgbm model missing"):
-            ApplyRainForestsCalibration(model_config, threads=8)
-        return
-
-    result = ApplyRainForestsCalibration(model_config, threads=8)
-
-    for model in result.tree_models:
-        assert model.model_class == expected_class
-        assert model.threads == 8
-    assert result.treelite_enabled is treelite_model
+        expected_threads = 8
+        result = ApplyRainForestsCalibrationLightGBM(
+            model_config, threads=expected_threads
+        )
+    # Check thresholds and model types match
     assert np.all(result.error_thresholds == error_thresholds)
+    for model in result.tree_models:
+        assert model.model_class == "lightgbm-Booster"
+        assert model.threads == expected_threads
+    # Ensure threshold and files match
     for threshold, model in zip(result.error_thresholds, result.tree_models):
         assert f"{threshold:06.4f}" in model.model_file
 
 
-def test__check_num_features_lightgbm(ensemble_features, dummy_lightgbm_models):
+def test__check_num_features(ensemble_features, dummy_lightgbm_models):
     """Test number of features expected by tree_models matches features passed in."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
+    plugin = ApplyRainForestsCalibrationLightGBM(model_config_dict={})
     plugin.tree_models, _ = dummy_lightgbm_models
-    plugin._check_num_features(ensemble_features)
-    with pytest.raises(ValueError):
-        plugin._check_num_features(ensemble_features[:-1])
-
-
-@treelite_available
-def test__check_num_features_treelite(ensemble_features, dummy_treelite_models):
-    """Test number of features expected by tree_models matches features passed in."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
-    plugin.tree_models, _ = dummy_treelite_models
     plugin._check_num_features(ensemble_features)
     with pytest.raises(ValueError):
         plugin._check_num_features(ensemble_features[:-1])
@@ -150,7 +110,7 @@ def test__align_feature_variables_ensemble(ensemble_features, ensemble_forecast)
     dervied_field_cube.remove_coord("realization")
     ensemble_features.append(dervied_field_cube)
 
-    (aligned_features, aligned_forecast,) = ApplyRainForestsCalibration(
+    (aligned_features, aligned_forecast,) = ApplyRainForestsCalibrationLightGBM(
         model_config_dict={}
     )._align_feature_variables(ensemble_features, ensemble_forecast)
 
@@ -172,7 +132,7 @@ def test__align_feature_variables_deterministic(
     deterministic_forecast = deterministic_forecast.extract(Constraint(realization=0))
     deterministic_forecast.remove_coord("realization")
 
-    (aligned_features, aligned_forecast,) = ApplyRainForestsCalibration(
+    (aligned_features, aligned_forecast,) = ApplyRainForestsCalibrationLightGBM(
         model_config_dict={}
     )._align_feature_variables(deterministic_features, deterministic_forecast)
 
@@ -191,9 +151,9 @@ def test__align_feature_variables_misaligned_dim_coords(ensemble_features):
         realizations=np.arange(5),
     )
     with pytest.raises(ValueError):
-        ApplyRainForestsCalibration(model_config_dict={})._align_feature_variables(
-            ensemble_features, misaligned_forecast_cube
-        )
+        ApplyRainForestsCalibrationLightGBM(
+            model_config_dict={}
+        )._align_feature_variables(ensemble_features, misaligned_forecast_cube)
     # Test case where realization dimension differ.
     misaligned_forecast_cube = set_up_variable_cube(
         np.maximum(0, np.random.normal(0.002, 0.001, (10, 10, 10))).astype(np.float32),
@@ -202,9 +162,9 @@ def test__align_feature_variables_misaligned_dim_coords(ensemble_features):
         realizations=np.arange(10),
     )
     with pytest.raises(ValueError):
-        ApplyRainForestsCalibration(model_config_dict={})._align_feature_variables(
-            ensemble_features, misaligned_forecast_cube
-        )
+        ApplyRainForestsCalibrationLightGBM(
+            model_config_dict={}
+        )._align_feature_variables(ensemble_features, misaligned_forecast_cube)
 
 
 def test__prepare_error_probability_cube(
@@ -212,7 +172,7 @@ def test__prepare_error_probability_cube(
 ):
     """Test the preparation of error probability cube from input
     forecast cube."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
+    plugin = ApplyRainForestsCalibrationLightGBM(model_config_dict={})
     plugin.error_thresholds = error_thresholds
     result = plugin._prepare_error_probability_cube(ensemble_forecast)
 
@@ -228,7 +188,7 @@ def test__prepare_features_dataframe(ensemble_features):
     expected_size = ensemble_features.extract_cube(
         "lwe_thickness_of_precipitation_amount"
     ).data.size
-    result = ApplyRainForestsCalibration(
+    result = ApplyRainForestsCalibrationLightGBM(
         model_config_dict={}
     )._prepare_features_dataframe(ensemble_features)
 
@@ -243,9 +203,9 @@ def test__prepare_features_dataframe(ensemble_features):
     cube_lacking_realization.remove_coord("realization")
     ensemble_features.append(cube_lacking_realization)
     with pytest.raises(ValueError):
-        ApplyRainForestsCalibration(model_config_dict={})._prepare_features_dataframe(
-            ensemble_features
-        )
+        ApplyRainForestsCalibrationLightGBM(
+            model_config_dict={}
+        )._prepare_features_dataframe(ensemble_features)
 
 
 def test_make_decreasing():
@@ -254,21 +214,21 @@ def test_make_decreasing():
     # Test on standard use case.
     input_array = np.array([[5, 5], [2, 3], [3, 4], [4, 2], [1, 1]]) / 5.0
     expected = np.array([[5, 5], [3, 3.5], [3, 3.5], [3, 2], [1, 1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
     # Test on case where all data is already monotonically decreasing.
     input_array = np.array([[5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]) / 5.0
     expected = np.array([[5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
     # Test on case where some data is monotonically increasing.
     input_array = np.array([[1, 5], [2, 3], [3, 4], [4, 2], [5, 1]]) / 5.0
     expected = np.array([[3, 5], [3, 3.5], [3, 3.5], [3, 2], [3, 1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
@@ -277,38 +237,38 @@ def test_make_decreasing():
     # decreasing.
     input_array = np.array([[4, 5], [2, 3], [3, 4], [1, 2], [1, 1]]) / 5.0
     expected = np.array([[4, 5], [2.5, 3.5], [2.5, 3.5], [1, 2], [1, 1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
     # Test on case where data has shape (n, 1).
     input_array = np.array([[5], [3], [4], [2], [1]]) / 5.0
     expected = np.array([[5], [3.5], [3.5], [2], [1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
     # Test on case where data has shape (1, n).
     input_array = np.array([[5, 3, 4, 2, 1]]) / 5.0
     expected = np.array([[5, 3, 4, 2, 1]]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
     # Test on case where data has shape (n).
     input_array = np.array([5, 3, 4, 2, 1]) / 5.0
     expected = np.array([5, 3.5, 3.5, 2, 1]) / 5.0
-    result = ApplyRainForestsCalibration(model_config_dict={})._make_decreasing(
+    result = ApplyRainForestsCalibrationLightGBM(model_config_dict={})._make_decreasing(
         input_array
     )
     np.testing.assert_almost_equal(expected, result)
 
 
-def test__calculate_error_probabilities_lightgbm(
+def test__calculate_error_probabilities(
     ensemble_features, ensemble_forecast, dummy_lightgbm_models
 ):
     """Test calculation of error probability cube when using lightgbm Boosters."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
+    plugin = ApplyRainForestsCalibrationLightGBM(model_config_dict={})
     plugin.tree_models, plugin.error_thresholds = dummy_lightgbm_models
     result = plugin._calculate_error_probabilities(ensemble_forecast, ensemble_features)
 
@@ -326,32 +286,9 @@ def test__calculate_error_probabilities_lightgbm(
     assert np.all(np.diff(result.data, axis=0) <= 0.0)
 
 
-@treelite_available
-def test__calculate_error_probabilities_treelite(
-    ensemble_features, ensemble_forecast, dummy_treelite_models
-):
-    """Test calculation of error probability cube when using treelite Predictors."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
-    plugin.tree_models, plugin.error_thresholds = dummy_treelite_models
-    result = plugin._calculate_error_probabilities(ensemble_forecast, ensemble_features)
-
-    # Check that data has sensible probability values
-    # Note: here we are NOT checking the returned value against an expected value
-    # as this will be sensitive to changes in associated GBDT libraries, given that
-    # the tree models are created dynamically within fixtures. Here we implicitly trust
-    # the output from the tree models are correct based on the specified inputs, and so
-    # only test to ensure that the dataset overall conforms to the bounds for probability
-    # data.
-    assert np.all(result.data >= 0.0)
-    assert np.all(result.data <= 1.0)
-    assert np.all(np.isfinite(result.data))
-    # Check that data is monotonically decreasing
-    assert np.all(np.diff(result.data, axis=0) <= 0.0)
-
-
 def test__extract_error_percentiles(error_threshold_cube, error_percentile_cube):
     """Test the extraction of error percentiles from error-probability cube."""
-    result = ApplyRainForestsCalibration(
+    result = ApplyRainForestsCalibrationLightGBM(
         model_config_dict={}
     )._extract_error_percentiles(error_threshold_cube, 4)
 
@@ -363,9 +300,9 @@ def test__extract_error_percentiles(error_threshold_cube, error_percentile_cube)
 
 def test__apply_error_to_forecast(ensemble_forecast, error_percentile_cube):
     """Test the application of forecast error (percentile) values to the forecast cube."""
-    result = ApplyRainForestsCalibration(model_config_dict={})._apply_error_to_forecast(
-        ensemble_forecast, error_percentile_cube
-    )
+    result = ApplyRainForestsCalibrationLightGBM(
+        model_config_dict={}
+    )._apply_error_to_forecast(ensemble_forecast, error_percentile_cube)
 
     assert result.standard_name == ensemble_forecast.standard_name
     assert result.long_name == ensemble_forecast.long_name
@@ -390,9 +327,9 @@ def test__stack_subensembles(error_percentile_cube):
         column.reshape(5, 4)[:, :, np.newaxis, np.newaxis], (5, 4, 10, 10)
     )
     expected_data = np.broadcast_to(column[:, np.newaxis, np.newaxis], (20, 10, 10))
-    result = ApplyRainForestsCalibration(model_config_dict={})._stack_subensembles(
-        error_percentile_cube
-    )
+    result = ApplyRainForestsCalibrationLightGBM(
+        model_config_dict={}
+    )._stack_subensembles(error_percentile_cube)
     # Result should not contain percentile coordinate
     assert result.coords("percentile") == []
     # Result should have realization coordinate composed on of length
@@ -417,16 +354,16 @@ def test__stack_subensembles(error_percentile_cube):
     # Test the case where cubes are not in expected order.
     error_percentile_cube.transpose([1, 0, 2, 3])
     with pytest.raises(ValueError):
-        ApplyRainForestsCalibration(model_config_dict={})._stack_subensembles(
+        ApplyRainForestsCalibrationLightGBM(model_config_dict={})._stack_subensembles(
             error_percentile_cube
         )
 
 
 def test__combine_subensembles(error_percentile_cube):
     """Test extraction of realization values from full superensemble."""
-    result = ApplyRainForestsCalibration(model_config_dict={})._combine_subensembles(
-        error_percentile_cube, output_realizations_count=None
-    )
+    result = ApplyRainForestsCalibrationLightGBM(
+        model_config_dict={}
+    )._combine_subensembles(error_percentile_cube, output_realizations_count=None)
 
     assert (
         result.coord("realization").points.size
@@ -434,55 +371,17 @@ def test__combine_subensembles(error_percentile_cube):
         * error_percentile_cube.coord("percentile").points.size
     )
 
-    result = ApplyRainForestsCalibration(model_config_dict={})._combine_subensembles(
-        error_percentile_cube, output_realizations_count=10
-    )
+    result = ApplyRainForestsCalibrationLightGBM(
+        model_config_dict={}
+    )._combine_subensembles(error_percentile_cube, output_realizations_count=10)
 
     assert result.coord("realization").points.size == 10
 
 
-def test_process_with_lightgbm(
-    ensemble_forecast, ensemble_features, dummy_lightgbm_models
-):
+def test_process(ensemble_forecast, ensemble_features, dummy_lightgbm_models):
     """Test process routine using lightgbm booster."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
+    plugin = ApplyRainForestsCalibrationLightGBM(model_config_dict={})
     plugin.tree_models, plugin.error_thresholds = dummy_lightgbm_models
-
-    for output_realization_count in (None, 10):
-        result = plugin.process(
-            ensemble_forecast,
-            ensemble_features,
-            error_percentiles_count=4,
-            output_realizations_count=output_realization_count,
-        )
-
-    assert result.standard_name == ensemble_forecast.standard_name
-    assert result.long_name == ensemble_forecast.long_name
-    assert result.var_name == ensemble_forecast.var_name
-    assert result.units == ensemble_forecast.units
-
-    # Check that all non-realization are equal
-    assert result.coords(dim_coords=True)[1:], ensemble_forecast.coords(
-        dim_coords=True
-    )[1:]
-    if output_realization_count is None:
-        assert (
-            result.coord("realization").points.size
-            == 4 * ensemble_forecast.coord("realization").points.size
-        )
-    else:
-        assert result.coord("realization").points.size == output_realization_count
-    assert result.coords(dim_coords=False) == ensemble_forecast.coords(dim_coords=False)
-    assert result.attributes == ensemble_forecast.attributes
-
-
-@treelite_available
-def test_process_with_treelite(
-    ensemble_forecast, ensemble_features, dummy_treelite_models
-):
-    """Test process routine using treelite Predictor."""
-    plugin = ApplyRainForestsCalibration(model_config_dict={})
-    plugin.tree_models, plugin.error_thresholds = dummy_treelite_models
 
     for output_realization_count in (None, 10):
         result = plugin.process(
