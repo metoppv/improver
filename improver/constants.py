4c0135aab2a7cf0215af9de09c2dfbf490f8e9be
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-06-28 14:42:53 +0100

    Removes internal constants from being picked up by Sphinx

--- improver/constants.py
+++ improver/constants.py
@@ -30,15 +30,15 @@
 # POSSIBILITY OF SUCH DAMAGE.
 """Module to contain generally useful constants."""
 
-#: Cube comparison tolerances
+# Cube comparison tolerances
 TIGHT_TOLERANCE = 1e-5
 DEFAULT_TOLERANCE = 1e-4
 LOOSE_TOLERANCE = 1e-3
 
-#: Real Missing Data Indicator
+# Real Missing Data Indicator
 RMDI = -32767.0
 
-#: Default percentile boundaries to calculate at for IMPROVER.
+# Default percentile boundaries to calculate at for IMPROVER.
 DEFAULT_PERCENTILES = (0, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100)
 
 # Temporal constants
