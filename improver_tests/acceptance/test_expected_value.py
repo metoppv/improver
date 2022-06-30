98691c899f12ca99d8d6c3a4e5b275cd7ada2cae
Tom Gale <tom.gale@bom.gov.au>
2022-06-30 13:35:24 +1000

    Implement expected value via integration over probability thresholds (#1734)
    * Implement expected value over thresholds
    
    * Add tests for non-monotonic data and thresholds lt/gt
    
    * Update acceptance test docstring
    
    * min/max of threshold spacing and ECC bounds
    
    * Fix interpolation mismatched with thresholds
    
    Also add parameterized tests to pick up this issue
    
    * Add unit test for unequally spaced thresholds
    
    * Fix black
    
    * Remove duplicated data equality check
    
    * Update comment explaining extra thresholds

--- improver_tests/acceptance/test_expected_value.py
+++ improver_tests/acceptance/test_expected_value.py
@@ -43,18 +43,18 @@ run_cli = acc.run_cli(CLI)
 
 
 @pytest.mark.parametrize(
-    "input_kind,atol,significant_diff_fraction",
-    [("realization", 0.001, 0.0), ("percentile", 0.09, 0.0), ("threshold", 0.18, 0.2)],
+    "input_kind,atol,mae,bias",
+    [
+        ("realization", 0.001, 0.0, 0.0),
+        ("percentile", 0.09, 0.02, 0.011),
+        ("threshold", 0.156, 0.06, 0.007),
+    ],
 )
-def test_probabilistic(tmp_path, input_kind, atol, significant_diff_fraction):
+def test_probabilistic(tmp_path, input_kind, atol, mae, bias):
     """Test processing of probabilistic data.
 
-    The same KGO is shared across all representations. Processing realizations is
-    straightforward and accurate, so has low error tolerances. Processing percentiles
-    is also straightforward, but with some variation in the result due to representation
-    differences. Processing thresholds is quite rough, due to the currently implemented
-    method of conversion to percentiles. Expect this accuracy to improve if the expected
-    value is directly calculcated from the threshold data.
+    The same KGO is shared across all three representations as percentile and
+    threshold inputs are derived from the realization input.
     """
     kgo_dir = acc.kgo_root() / "expected-value"
     kgo_path = kgo_dir / "kgo.nc"
@@ -63,13 +63,17 @@ def test_probabilistic(tmp_path, input_kind, atol, significant_diff_fraction):
     args = [input_path, "--output", output_path]
     run_cli(args)
     # standard comparison, checking the maximum difference using atol
-    acc.compare(output_path, kgo_path, rtol=0.0, atol=atol)
-    # custom comparison - fraction of the grid with difference more than 0.1 kelvin
+    # KGO comes from realizations, don't recreate if using other kinds of input
+    is_realization = input_kind == "realization"
+    acc.compare(output_path, kgo_path, rtol=0.0, atol=atol, recreate=is_realization)
     kgo_data = iris.load(str(kgo_path))[0].data
     output_data = iris.load(str(output_path))[0].data
-    large_diffs = np.abs(output_data - kgo_data) > 0.1
-    fraction_of_large_diffs = np.mean(large_diffs.astype(np.float32))
-    assert significant_diff_fraction >= fraction_of_large_diffs
+    # custom comparison - mean absolute error
+    data_mae = np.mean(np.abs(output_data - kgo_data))
+    assert data_mae <= mae
+    # custom comparison - bias
+    data_bias = np.abs(np.mean(output_data - kgo_data))
+    assert data_bias <= bias
 
 
 def test_deterministic(tmp_path):
@@ -79,5 +83,5 @@ def test_deterministic(tmp_path):
     output_path = tmp_path / "output.nc"
     args = [input_path, "--output", output_path]
     # iris CoordinateNotFoundError is a KeyError
-    with pytest.raises(Exception, match="realization"):
+    with pytest.raises(KeyError, match="realization"):
         run_cli(args)
--- improver_tests/expected_value/test_expected_value.py
+++ improver_tests/expected_value/test_expected_value.py
@@ -41,6 +41,7 @@ from improver.synthetic_data.set_up_test_cubes import (
     set_up_probability_cube,
     set_up_variable_cube,
 )
+from improver.utilities.probability_manipulation import to_threshold_inequality
 
 
 @pytest.fixture
@@ -59,11 +60,26 @@ def percentile_cube():
     return set_up_percentile_cube(data, percentiles=[25, 50, 75])
 
 
+@pytest.fixture(params=[5, 9, 13])
+def threshold_cube(request):
+    thresholds = np.linspace(280, 284, 5)
+    thresholds_interp = np.linspace(280, 284, request.param)
+    probs = np.array([1.0, 0.7, 0.5, 0.45, 0.0])
+    probs_interp = np.interp(thresholds_interp, thresholds, probs)
+    data = np.broadcast_to(
+        probs_interp[:, np.newaxis, np.newaxis], [len(thresholds_interp), 3, 2]
+    ).astype(np.float32)
+    return set_up_probability_cube(data, thresholds=thresholds_interp)
+
+
 @pytest.fixture
-def threshold_cube():
-    probs = np.array([1.0, 0.7, 0.5, 0.45, 0.0], dtype=np.float32)
-    data = np.broadcast_to(probs[:, np.newaxis, np.newaxis], [5, 3, 2])
-    return set_up_probability_cube(data, thresholds=[280, 281, 282, 283, 284])
+def unequal_threshold_cube():
+    thresholds = np.array([272, 280.75, 281, 282, 282.5, 284, 291])
+    probs = np.array([1.0, 0.99, 0.7, 0.5, 0.45, 0.0, 0.0])
+    data = np.broadcast_to(probs[:, np.newaxis, np.newaxis], [7, 3, 2]).astype(
+        np.float32
+    )
+    return set_up_probability_cube(data, thresholds=thresholds)
 
 
 def test_process_realizations_basic(realizations_cube):
@@ -94,12 +110,55 @@ def test_process_percentile_basic(percentile_cube):
 
 
 def test_process_threshold_basic(threshold_cube):
-    """Check that thresholds are converted to realisations and the mean is
-    calculated."""
+    """Check basic calculation of expected value using threshold data."""
     expval = ExpectedValue().process(threshold_cube)
     # threshold probablities are asymmetric, so the mean is slightly above the
     # 282 kelvin threshold
-    assert_allclose(expval.data, 282.16, atol=0.01)
+    assert_allclose(expval.data, 282.15, atol=1e-6, rtol=0.0)
+
+
+def test_process_threshold_unequal(unequal_threshold_cube):
+    """Check calculation of expected value using unevenly spaced threshold data."""
+    expval = ExpectedValue().process(unequal_threshold_cube)
+    assert_allclose(expval.data, 282.0925, atol=1e-6, rtol=0.0)
+
+
+def test_process_threshold_abovebelow(threshold_cube):
+    """Check that probabilities above and below threshold are handled correctly."""
+    # set up cubes that are the same other than threshold greater than/less than
+    threshold_below_cube = to_threshold_inequality(threshold_cube, above=False)
+    threshold_cube_airtemp = threshold_cube.coord("air_temperature")
+    threshold_below_cube_airtemp = threshold_below_cube.coord("air_temperature")
+    np.testing.assert_array_equal(
+        threshold_cube_airtemp.points, threshold_below_cube_airtemp.points,
+    )
+    assert (
+        threshold_cube_airtemp.attributes["spp__relative_to_threshold"]
+        == "greater_than"
+    )
+    assert (
+        threshold_below_cube_airtemp.attributes["spp__relative_to_threshold"]
+        == "less_than_or_equal_to"
+    )
+    # calculate expected value for both, they should be the same
+    expval_above = ExpectedValue().process(threshold_cube)
+    expval_below = ExpectedValue().process(threshold_below_cube)
+    assert expval_above == expval_below
+
+
+def test_process_threshold_non_monotonic(threshold_cube):
+    """Check that non-monotonic threshold data raises an exception."""
+    thresholds = np.linspace(280, 284, 5)
+    probs = np.array([1.0, 0.4, 0.5, 0.6, 0.0])
+    probs_interp = np.interp(
+        threshold_cube.coord("air_temperature").points, thresholds, probs
+    )
+    threshold_cube.data = np.broadcast_to(
+        probs_interp[:, np.newaxis, np.newaxis],
+        [threshold_cube.coord("air_temperature").shape[0], 3, 2],
+    ).astype(np.float32)
+    with pytest.raises(Exception, match="monotonic"):
+        ExpectedValue().process(threshold_cube)
 
 
 def test_process_non_probabilistic(realizations_cube, percentile_cube):
