42903688c6eeb28187f389538d5c252b29f4c7a0
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-07-01 16:38:03 +0100

    Adds copyright

--- improver/__init__.py
+++ improver/__init__.py
@@ -28,7 +28,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""Module containing plugin base class."""
+"""Module containing version attribute."""
 
 from pkg_resources import DistributionNotFound, get_distribution
 
