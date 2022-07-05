0df5606f48d0b66f776bd02a1fc4e1691429582d
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-07-05 10:05:55 +0100

    Sphinx - disables long module names

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -143,7 +143,7 @@ autodoc_mock_imports = ["numba"]
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 #
-# add_module_names = True
+add_module_names = False
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
