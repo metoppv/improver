c168dadb516919ed46692bab39bd9e15082aa68a
Daniel Mentiplay <daniel.mentiplay@bom.gov.au>
2022-07-18 11:14:26 +1000

    Add test setting var_name and standard_name
    This test sets a different var_name to the standard_name and checks that
    both are present.

--- improver_tests/regrid/test_grid.py
+++ improver_tests/regrid/test_grid.py
@@ -240,3 +240,29 @@ def test_create_regrid_cube_non_standard_name():
     )
     assert cube_v.standard_name is None
     assert cube_v.long_name == "not_a_standard_name"
+
+
+def test_create_regrid_cube_different_var_name():
+    """Test the create_regrid_cube function with a different var_name"""
+
+    source_cube_latlon = set_up_variable_cube(
+        np.ones((2, 5, 5), dtype=np.float32),
+        name="air_temperature",
+        time=datetime(2018, 11, 10, 8, 0),
+        frt=datetime(2018, 11, 10, 0, 0),
+    )
+    source_cube_latlon.var_name = "some_var_name"
+    target_cube_equalarea = set_up_variable_cube(
+        np.ones((10, 10), dtype=np.float32), spatial_grid="equalarea"
+    )
+    data = np.repeat(1.0, 200).reshape(2, 10, 10)
+    cube_v = create_regrid_cube(data, source_cube_latlon, target_cube_equalarea)
+    assert cube_v.shape == (2, 10, 10)
+    assert cube_v.coord(axis="x").standard_name == "projection_x_coordinate"
+    assert cube_v.coord(axis="y").standard_name == "projection_y_coordinate"
+    assert cube_v.coord("forecast_reference_time") == source_cube_latlon.coord(
+        "forecast_reference_time"
+    )
+    assert cube_v.standard_name == "air_temperature"
+    assert cube_v.long_name is None
+    assert cube_v.var_name == "some_var_name"
