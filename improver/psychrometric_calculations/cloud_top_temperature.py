eb0d774c90c90a3eba1867970cf29330cf082d90
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-07-22 10:27:36 +0100

    Improves comments

--- improver/psychrometric_calculations/cloud_top_temperature.py
+++ improver/psychrometric_calculations/cloud_top_temperature.py
@@ -42,19 +42,19 @@ class CloudTopTemperature(BasePlugin):
             self.ccl.data, self.ccl.coord("air_pressure").points
         )
         ccl_with_mask = np.ma.where(True, self.ccl.data, False)
-        for p, t in zip(
-            p_coord.points,
-            t_data,
-        ):
+        for p, t in zip(p_coord.points, t_data):
             t_dry = dry_adiabatic_temperature(
                 self.ccl.data, self.ccl.coord("air_pressure").points, p
             )
             t_2, _ = adjust_for_latent_heat(t_dry, q_at_ccl, p)
+            # Mask out points where parcel temperature, t_2, is less than atmosphere temperature, t,
+            # but only after the parcel pressure, p, becomes lower than the cloud base pressure.
             ccl_with_mask = np.ma.masked_where(
-                (t_2 < t) & (p < self.ccl.coord("air_pressure").points),
-                ccl_with_mask,
+                (t_2 < t) & (p < self.ccl.coord("air_pressure").points), ccl_with_mask,
             )
             cct[~ccl_with_mask.mask] = t_2[~ccl_with_mask.mask]
+        # cct is the saturated adiabatic temperature at the last atmosphere pressure level where
+        # the profile is buoyant.
         return cct
 
     def _make_cct_cube(self, data: ndarray) -> Cube:
