348ec89666b47a64f41e0701867d3db44a0a489c
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-07-22 16:22:45 +0100

    Adds CLI

---
+++ improver/cli/cloud_top_temperature.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""CLI to generate the convective cloud top temperature from CCL and temperature profile data."""
+
+from improver import cli
+
+
+# Creates the value_converter that clize needs.
+input_cube_names = cli.create_constrained_inputcubelist_converter(
+    cli.name_constraint(["temperature_at_convective_cloud_level"]),
+    cli.name_constraint(["temperature_on_pressure_levels"]),
+)
+
+
+@cli.clizefy
+@cli.with_output
+def process(*cubes: input_cube_names, model_id_attr: str = None):
+    """Module to calculate the convective cloud top temperature from the
+    cloud condensation level temperature and pressure, and temperature
+    on pressure levels data.
+    The temperature is that of the parcel after saturated ascent at the last pressure level
+    where the parcel is buoyant.
+
+    Args:
+        cubes (iris.cube.CubeList or list of iris.cube.Cube):
+            containing:
+                ccl (iris.cube.Cube):
+                    Cube of convective_cloud_level
+                temperature (iris.cube.Cube):
+                    Cube of temperature_at_pressure_levels
+        model_id_attr (str):
+            Name of the attribute used to identify the source model for blending.
+
+    Returns:
+        iris.cube.Cube:
+            Cube of cloud top temperature (K).
+
+    """
+    from improver.psychrometric_calculations.cloud_top_temperature import CloudTopTemperature
+
+    return CloudTopTemperature(model_id_attr=model_id_attr)(cubes)
