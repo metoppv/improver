1c4d83bc3ca5b1ee722ed6a6512ba5df7fe871b2
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2022-07-25 08:06:38 +0100

    Improves doc-strings

--- improver/psychrometric_calculations/cloud_top_temperature.py
+++ improver/psychrometric_calculations/cloud_top_temperature.py
@@ -19,7 +19,7 @@ from improver.psychrometric_calculations.psychrometric_calculations import (
 class CloudTopTemperature(BasePlugin):
     """Plugin to calculate the convective cloud top temperature from the
     cloud condensation level temperature and pressure, and temperature
-    on pressure levels data.
+    on pressure levels data using saturated ascent.
     The temperature is that of the parcel after saturated ascent at the last pressure level
     where the parcel is buoyant. The interpolation required to get closer is deemed expensive.
     """
@@ -37,7 +37,15 @@ class CloudTopTemperature(BasePlugin):
         self.temperature = Cube(None)
 
     def _calculate_cct(self) -> ndarray:
-        """Does the main bit of work. Uses mask to determine where more ascent is needed"""
+        """
+        Ascends through the pressure levels (decreasing pressure) calculating the saturated
+        ascent from the cloud condensation level until a level is reached where the
+        atmosphere profile is warmer than the ascending parcel, signifying that buoyancy
+        is negative and cloud growth has ceased.
+        CCT is the saturated adiabatic temperature at the last atmosphere pressure level where
+        the profile is buoyant.
+        Temperature data are in Kelvin, Pressure data are in pascals, humidity data are in kg/kg.
+        """
         p_coord = self.temperature.coord("pressure")
         t_data = [t.data for t in self.temperature.slices_over("pressure")]
         cct = np.ma.masked_array(self.ccl.data.copy())
@@ -56,8 +64,7 @@ class CloudTopTemperature(BasePlugin):
                 (t_2 < t) & (p < self.ccl.coord("air_pressure").points), ccl_with_mask,
             )
             cct[~ccl_with_mask.mask] = t_2[~ccl_with_mask.mask]
-        # cct is the saturated adiabatic temperature at the last atmosphere pressure level where
-        # the profile is buoyant.
+
         return cct
 
     def _make_cct_cube(self, data: ndarray) -> Cube:
@@ -89,5 +96,8 @@ class CloudTopTemperature(BasePlugin):
             Cube of cloud top temperature
         """
         self.ccl, self.temperature = cubes
+        self.temperature.convert_units("K")
+        self.ccl.convert_units("m")
+        self.ccl.coord("pressure").convert_units("Pa")
         cct = self._make_cct_cube(self._calculate_cct())
         return cct
