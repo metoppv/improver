c8d70ba50c539289313924d780403d6fabcbc1d7
Belinda Trotta <btrotta-bom@users.noreply.github.com>
2023-03-23 16:44:09 +1100

    minor updates

--- improver/calibration/rainforest_calibration.py
+++ improver/calibration/rainforest_calibration.py
@@ -67,7 +67,7 @@ from improver.utilities.save import save_netcdf
 from improver.calibration.utilities import inv_log_transform
 from improver.ensemble_copula_coupling.utilities import interpolate_pointwise
 import iris
-from improver.cli import blend_cycles_and_realizations
+from improver.cli import blend_cycles_and_realizations, weighted_blending
 
 # Passed to choose_set_of_percentiles to set of evenly spaced percentiles
 DEFAULT_ERROR_PERCENTILES_COUNT = 19
@@ -757,12 +757,12 @@ class ApplyRainForestsCalibrationLightGBM(ApplyRainForestsCalibration):
         )
 
         # Add error to forecast
-        thresholds = (
-            error_CDF.coord(
+        error_values = error_CDF.coord(
                 "forecast_error_of_lwe_thickness_of_precipitation_amount"
-            ).points[:, np.newaxis, np.newaxis]
-            + aligned_forecast.data
-        )
+            ).points
+        for i in range(len(aligned_forecast.data.shape)):
+            error_values = np.expand_dims(error_values, axis=-1)
+        thresholds = error_values + np.expand_dims(aligned_forecast.data, axis=0)
         probabilities = error_CDF.data
 
         # add bounds
@@ -787,9 +787,9 @@ class ApplyRainForestsCalibrationLightGBM(ApplyRainForestsCalibration):
         epsilon = 0.0001
         thresholds = np.concatenate(
             [
-                thresholds[[0], :, :] - epsilon,
+                thresholds[[0]] - epsilon,
                 thresholds,
-                thresholds[[-1], :, :] + epsilon,
+                thresholds[[-1]] + epsilon,
             ],
             axis=0,
         )
@@ -806,7 +806,14 @@ class ApplyRainForestsCalibrationLightGBM(ApplyRainForestsCalibration):
          # set probability to 1 for negative thresholds
         negative_threshold = thresholds <= 0
         thresholds = np.where(negative_threshold, 0, thresholds)
-        probabilities = np.where(negative_threshold, 1, probabilities)   
+        probabilities = np.where(negative_threshold, 1, probabilities)
+
+        # add zero threshold
+        # thresholds = np.concatenate([np.zeros_like(thresholds[[0]]), thresholds], axis=0)
+        # probabilities = np.concatenate([np.ones_like(probabilities[[0]]), probabilities])
+        # sort_ind = np.argsort(thresholds, axis=0)
+        # thresholds = np.take_along_axis(thresholds, sort_ind, axis=0)
+        # probabilities = np.take_along_axis(probabilities, sort_ind, axis=0)
 
         # interpolate
         output_thresholds = np.sort(output_thresholds).astype(np.float32)
@@ -848,6 +855,13 @@ class ApplyRainForestsCalibrationLightGBM(ApplyRainForestsCalibration):
         )
 
         # blend accross realizations
+        # num_realizations = len(aligned_forecast.coord("realization").points)
+        # control_weight = 0.25
+        # other_weight = ((1 - control_weight) / (num_realizations - 1))
+        # control_weight *= num_realizations
+        # other_weight *= num_realizations
+        # probability_cube.data = np.concatenate([probability_cube.data[:, [0], :]* control_weight, probability_cube.data[:, 1:, :] * other_weight], axis=1)
+        
         cycle_time = dt.datetime.utcfromtimestamp(
             probability_cube.coord("forecast_reference_time").points[0]
         ).strftime("%Y%m%dT%H%MZ")
@@ -855,6 +869,33 @@ class ApplyRainForestsCalibrationLightGBM(ApplyRainForestsCalibration):
             probability_cube, cycletime=cycle_time
         )
 
+        # control_weight = 0.5
+        # num_realizations = len(aligned_forecast.coord("realization").points)
+        # other_weight = (1 - control_weight) / (num_realizations - 1)
+        # weight_dict = {
+        #         0: {
+        #             "forecast_period": [0, 24],
+        #             "weights": [control_weight, control_weight],
+        #             "units": "hours",
+        #         }
+        #     }
+        # weight_dict.update({
+        #         i: {
+        #             "forecast_period": [0, 24],
+        #             "weights": [other_weight, other_weight],
+        #             "units": "hours",
+        #         } for i in range(1, num_realizations)
+		# })
+        # output_cube = weighted_blending.process(
+        #     probability_cube, 
+        #     coordinate="realization", 
+        #     weighting_method="dict",
+        #     weighting_coord="forecast_period",
+        #     weighting_config=weight_dict,
+        #     cycletime=cycle_time
+        # )
+        # output_cube.data = np.maximum(0, output_cube.data)
+        # output_cube.data = np.minimum(1, output_cube.data)
         return output_cube
 
 
