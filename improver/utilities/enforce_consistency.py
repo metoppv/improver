3a9c8f075294f45d48d7ff4bcc859621c2aee3b5
Ben Hooper <ben.r.hooper@metoffice.gov.uk>
2023-04-28 11:11:09 +0100

    Add plugin and CLI for general enforcement of forecast consistency. Add corresponding unit and acceptance tests.

---
+++ improver/utilities/enforce_consistency.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Provides utilities for enforcing consistency between forecasts."""
+import warnings
+from iris.cube import Cube
+import numpy as np
+
+from improver import PostProcessingPlugin
+
+
+class EnforceConsistentForecasts(PostProcessingPlugin):
+    """Enforce that the forecasts provided are no less than or no greater than some
+    linear function of a reference forecast. For example, wind speed forecasts may be
+    provided as the reference forecast for wind gust forecasts with a requirement that
+    wind gusts are not less than 110% of the corresponding wind speed forecast. Wind
+    speed forecasts of 10 m/s will mean that the resulting wind gust forecast will be
+    at least 11 m/s.
+    """
+
+    def __init__(
+            self,
+            additive_amount: float = 0.0,
+            multiplicative_amount: float = 1.0,
+            comparison_operator: str = ">=",
+            diff_for_warning: float = None,
+    ) -> None:
+        """
+        Initialise class for enforcing consistency between a forecast and a linear
+        function of a reference forecast.
+
+        Args:
+            additive_amount: The amount to be added to the reference forecast prior to
+                enforcing consistency between the forecast and reference forecast. If
+                both an additive_amount and multiplicative_amount are specified then
+                addition occurs after multiplication.
+            multiplicative_amount: The amount to multiply the reference forecast by
+                prior to enforcing consistency between the forecast and reference
+                forecast. If both an additive_amount and multiplicative_amount are
+                specified then addition occurs after multiplication.
+            comparison_operator: Determines whether the forecast is enforced to be not
+                less than or not greater than the reference forecast. Valid choices are
+                ">=", for not less than, and "<=" for not greater than.
+            diff_for_warning: If assigned, the plugin will raise a warning if any
+                absolute change in forecast value is greater than this value.
+        """
+
+        self.additive_amount = additive_amount
+        self.multiplicative_amount = multiplicative_amount
+        self.comparison_operator = comparison_operator
+        self.diff_for_warning = diff_for_warning
+
+    def process(self, forecast: Cube, reference_forecast: Cube) -> Cube:
+        """
+        Function to enforce that the values in forecast_cube are not less than or not
+        greater than a linear function of the corresponding values in
+        reference_forecast.
+
+        Args:
+            forecast: A forecast cube of probabilities or percentiles
+            reference_forecast: A reference forecast cube used to determine the bound
+                of the forecast cube.
+
+        Returns:
+            A forecast cube with identical metadata to forecast but the forecasts are
+            enforced to be not less than or not greater than a linear function of
+            reference_forecast.
+        """
+        # calculate forecast_bound by applying specified linear transformation to
+        # reference_forecast
+        forecast_bound = reference_forecast.copy()
+        forecast_bound.data = self.multiplicative_amount * forecast_bound.data
+        forecast_bound.data = self.additive_amount + forecast_bound.data
+
+        # assign forecast_bound to be either an upper or lower bound depending on input
+        # comparison_operator
+        lower_bound = None
+        upper_bound = None
+        if self.comparison_operator == ">=":
+            lower_bound = forecast_bound.data
+        elif self.comparison_operator == "<=":
+            upper_bound = forecast_bound.data
+        else:
+            msg = (
+                f"comparison_operator must be either '>=' or '<=', not "
+                f"{self.comparison_operator}."
+            )
+            raise ValueError(msg)
+
+        new_forecast = forecast.copy()
+        new_forecast.data = np.clip(new_forecast.data, lower_bound, upper_bound)
+
+        diff = new_forecast.data - forecast.data
+        max_abs_diff = np.max(np.abs(diff))
+        if self.diff_for_warning is not None and max_abs_diff > self.diff_for_warning:
+            warnings.warn(
+                f"Inconsistency between forecast {forecast.name} and "
+                f"{reference_forecast.name} is greater than {self.diff_for_warning}. "
+                f"Maximum absolute difference reported was {max_abs_diff}"
+            )
+
+        return new_forecast
---
+++ improver_tests/utilities/test_enforce_consistency.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Unit tests for enforce_consistency utilities."""
+
+import numpy as np
+import pytest
+from iris.cube import Cube
+
+from improver.synthetic_data.set_up_test_cubes import (
+    set_up_percentile_cube, set_up_probability_cube
+)
+from improver.utilities.enforce_consistency import EnforceConsistentForecasts
+
+LOCAL_MANDATORY_ATTRIBUTES = {
+    "title": "Unit Test Data",
+    "source": "Unit Test",
+    "institution": "Met Office",
+}
+
+
+def get_percentiles(value, shape):
+    """
+    Correctly shape the value provided into the desired shape. The value is used as the
+    middle of three percentiles. The value is reduced or increased by 10 to create the
+    bounding percentiles.
+    """
+    return np.broadcast_to(
+        np.expand_dims([value - 10, value, value + 10], axis=(1, 2),),
+        shape,
+    )
+
+
+def get_percentile_forecast(value, shape, name):
+    """
+    Create a percentile forecast cube.
+    """
+    data = get_percentiles(value, shape)
+    forecast_cube = set_up_percentile_cube(
+        np.full(shape, data, dtype=np.float32),
+        [10, 50, 90],
+        name=name,
+        units="m s-1",
+    )
+    return forecast_cube
+
+
+def get_probability_forecast(value, shape, name) -> Cube:
+    """
+    Create a probability forecast cube.
+    """
+    data = np.full(shape, fill_value=value, dtype=np.float32)
+    forecast_cube = set_up_probability_cube(
+        data,
+        variable_name=name,
+        thresholds=[0.3125, 0.5, 0.6875],
+        attributes=LOCAL_MANDATORY_ATTRIBUTES,
+    )
+    return forecast_cube
+
+
+def get_expected(forecast_data, bound_data, comparison_operator):
+    """
+    Calculate the expected result via a different method to that used within the plugin.
+    """
+    if comparison_operator == ">=":
+        diff = bound_data - forecast_data
+        diff = np.clip(diff, 0, None)
+    else:
+        diff = bound_data - forecast_data
+        diff = np.clip(diff, None, 0)
+    expected = forecast_data + diff
+    return expected
+
+
+@pytest.mark.parametrize("forecast_type", ("percentile", "probability"))
+@pytest.mark.parametrize("additive_amount", (-15, 0, 15))
+@pytest.mark.parametrize("multiplicative_amount", (0.5, 1, 1.5))
+@pytest.mark.parametrize("reference_value", (30,))
+@pytest.mark.parametrize("forecast_value", (20, 30))
+@pytest.mark.parametrize("comparison_operator", (">=", "<="))
+def test_basic(
+        forecast_type,
+        additive_amount,
+        multiplicative_amount,
+        reference_value,
+        forecast_value,
+        comparison_operator,
+):
+    """
+    Test that consistency between percentiles is enforced for a variety of
+    percentages, wind speeds and wind gusts.
+    """
+    shape = (3, 2, 2)
+
+    if forecast_type == "probability":
+        reference_cube_name = "cloud_area_fraction"
+        forecast_cube_name = "low_and_medium_type_cloud_area_fraction"
+        get_forecast = get_probability_forecast
+        reference_value = reference_value / 100
+        forecast_value = forecast_value / 100
+    else:
+        reference_cube_name = "wind_speed_at_10m"
+        forecast_cube_name = "wind_gust_at_10m_max-PT01H"
+        get_forecast = get_percentile_forecast
+
+    reference_cube = get_forecast(
+        reference_value, shape, reference_cube_name
+    )
+    forecast_cube = get_forecast(
+        forecast_value, shape, forecast_cube_name
+    )
+    if forecast_type == "probability":
+        forecast_cube.data[:, 1, 1] = 0.6
+
+    result = EnforceConsistentForecasts(
+        additive_amount=additive_amount,
+        multiplicative_amount=multiplicative_amount,
+        comparison_operator=comparison_operator
+    )(forecast_cube, reference_cube)
+
+    bound_data = additive_amount + (multiplicative_amount * reference_cube.copy().data)
+    expected = get_expected(
+        forecast_cube.data, bound_data, comparison_operator
+    )
+
+    assert isinstance(result, Cube)
+    assert result.name() == forecast_cube.name()
+    assert np.shape(result) == shape
+    np.testing.assert_array_almost_equal(result.data, expected)
+
+
+@pytest.mark.parametrize(
+    "forecast_type, forecast_value, reference_value, comparison_operator, "
+    "diff_for_warning",
+    (
+        ("probability", 0.3, 0.9, ">=", 0.3),  # change too big
+        ("percentile", 10, 50, ">=", 30),  # change too big
+        ("percentile", 20, 30, "=", 30),  # incorrect comparison operator
+    )
+)
+def test_exceptions(
+        forecast_type,
+        forecast_value,
+        reference_value,
+        comparison_operator,
+        diff_for_warning,
+):
+    """
+    Test that a warning is raised if the plugin changes the forecast by more than
+    diff_for_warning.
+    """
+    shape = (3, 2, 2)
+
+    if forecast_type == "probability":
+        reference_cube_name = "cloud_area_fraction"
+        forecast_cube_name = "low_and_medium_type_cloud_area_fraction"
+        get_forecast = get_probability_forecast
+    else:
+        reference_cube_name = "wind_speed_at_10m"
+        forecast_cube_name = "wind_gust_at_10m_max-PT01H"
+        get_forecast = get_percentile_forecast
+
+    reference_cube = get_forecast(
+        reference_value, shape, reference_cube_name
+    )
+    forecast_cube = get_forecast(
+        forecast_value, shape, forecast_cube_name
+    )
+
+    if comparison_operator == "=":
+        with pytest.raises(ValueError, match="comparison_operator must be either"):
+            EnforceConsistentForecasts(
+                comparison_operator=comparison_operator
+            )(forecast_cube, reference_cube)
+    else:
+        with pytest.warns(UserWarning, match="Inconsistency between forecast"):
+            EnforceConsistentForecasts(diff_for_warning=diff_for_warning)(
+                forecast_cube, reference_cube
+            )
