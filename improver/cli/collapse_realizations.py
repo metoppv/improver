2c04c90155c6a642011dfd71f97c8b61ea980eae
Belinda Trotta <btrotta-bom@users.noreply.github.com>
2023-10-13 09:21:42 +1100

    update docstring

--- improver/cli/collapse_realizations.py
+++ improver/cli/collapse_realizations.py
@@ -29,7 +29,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""Script to aggregate (and optionally collapse) dimensions of a cube."""
+"""Script to collapse the realizations dimension of a cube."""
 
 
 from improver import cli
