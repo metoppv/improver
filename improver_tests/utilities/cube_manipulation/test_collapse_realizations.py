34d70f3fb964303c3af05057acafffc234760d4d
Belinda Trotta <btrotta-bom@users.noreply.github.com>
2023-10-13 10:13:59 +1100

    Use pytest

--- improver_tests/utilities/cube_manipulation/test_collapse_realizations.py
+++ improver_tests/utilities/cube_manipulation/test_collapse_realizations.py
@@ -29,74 +29,72 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """
-Unit tests for the function collapsed.
+Unit tests for the function collapse_realizations.
 """
-
-import unittest
 
 import iris
 import numpy as np
+import pytest
 from iris.exceptions import CoordinateCollapseError
 
 from improver.synthetic_data.set_up_test_cubes import set_up_variable_cube
 from improver.utilities.cube_manipulation import collapse_realizations
 
 
-class Test_aggregate(unittest.TestCase):
+@pytest.fixture
+def temperature_cube():
+    data = 281 * np.random.random_sample((3, 3, 3)).astype(np.float32)
+    return set_up_variable_cube(data, realizations=[0, 1, 2])
 
-    """Test the collapse_realizations utility."""
 
-    def setUp(self):
-        """Use temperature cube to test with."""
-        data = 281 * np.random.random_sample((3, 3, 3)).astype(np.float32)
-        self.cube = set_up_variable_cube(data, realizations=[0, 1, 2])
-        self.expected_data = self.cube.collapsed(
-            ["realization"], iris.analysis.MEAN
-        ).data
+def test_basic(temperature_cube):
+    """Test that a collapsed cube is returned with no realization coord."""
+    result = collapse_realizations(temperature_cube)
+    assert "realization" not in [
+        coord.name() for coord in result.dim_coords + result.aux_coords
+    ]
+    expected = temperature_cube.collapsed(["realization"], iris.analysis.MEAN)
+    np.testing.assert_allclose(result.data, expected.data)
 
-    def test_basic(self):
-        """Test that a collapsed cube is returned with no realization coord"""
-        result = collapse_realizations(self.cube)
-        assert "realization" not in [
-            coord.name() for coord in result.dim_coords + result.aux_coords
-        ]
-        assert (result.data == self.expected_data).all()
 
-    def test_invalid_dimension(self):
-        """Test that an error is raised if realization dimension
-        does not exist."""
-        sub_cube = self.cube.extract(iris.Constraint(realization=0))
-        with self.assertRaises(CoordinateCollapseError):
-            collapse_realizations(sub_cube, "mean")
+def test_invalid_dimension(temperature_cube):
+    """Test that an error is raised if realization dimension
+    does not exist."""
+    sub_cube = temperature_cube.extract(iris.Constraint(realization=0))
+    with pytest.raises(CoordinateCollapseError):
+        collapse_realizations(sub_cube, "mean")
 
-    def test_different_aggregators(self):
-        """Test aggregators other than mean."""
-        aggregator_dict = {
-            "sum": iris.analysis.SUM,
-            "median": iris.analysis.MEDIAN,
-            "std_dev": iris.analysis.STD_DEV,
-            "min": iris.analysis.MIN,
-            "max": iris.analysis.MAX,
-        }
-        for key, value in aggregator_dict.items():
-            result = collapse_realizations(self.cube, key)
-            expected_data = self.cube.collapsed(["realization"], value).data
-            self.assertTrue((result.data == expected_data).all())
 
-    def test_invalid_aggregators(self):
-        """Test that an error is raised if aggregator is not
-        one of the allowed types."""
+def test_different_aggregators(temperature_cube):
+    """Test aggregators other than mean."""
+    aggregator_dict = {
+        "sum": iris.analysis.SUM,
+        "median": iris.analysis.MEDIAN,
+        "std_dev": iris.analysis.STD_DEV,
+        "min": iris.analysis.MIN,
+        "max": iris.analysis.MAX,
+    }
+    for key, value in aggregator_dict.items():
+        result = collapse_realizations(temperature_cube, key)
+        expected = temperature_cube.collapsed(["realization"], value)
+        np.testing.assert_allclose(result.data, expected.data)
 
-        msg = "method must be one of"
-        with self.assertRaises(ValueError, msg=msg):
-            collapse_realizations(self.cube, method="product")
 
-    def test_1d_std_dev(self):
-        """Test that when std_dev is calculated over a dimension of size 1,
-        output is all masked and underlying value is np.nan.
-        """
-        data = 281 * np.random.random_sample((1, 3, 3)).astype(np.float32)
-        cube_1d = set_up_variable_cube(data, realizations=[0])
-        result = collapse_realizations(cube_1d, "std_dev")
-        self.assertTrue(np.all(np.ma.getmask(result.data)))
-        self.assertTrue(np.all(np.isnan(result.data.data)))
+def test_invalid_aggregators(temperature_cube):
+    """Test that an error is raised if aggregator is not
+    one of the allowed types."""
+
+    msg = "method must be one of"
+    with pytest.raises(ValueError, match=msg):
+        collapse_realizations(temperature_cube, method="product")
+
+
+def test_1d_std_dev(temperature_cube):
+    """Test that when std_dev is calculated over a dimension of size 1,
+    output is all masked and underlying value is np.nan.
+    """
+    data = 281 * np.random.random_sample((1, 3, 3)).astype(np.float32)
+    cube_1d = set_up_variable_cube(data, realizations=[0])
+    result = collapse_realizations(cube_1d, "std_dev")
+    assert np.all(np.ma.getmask(result.data))
+    assert np.all(np.isnan(result.data.data))
