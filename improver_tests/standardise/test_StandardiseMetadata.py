939c6b5553190450b1d276dc4801e399edb47bba
Marcus Spelman <marcus.spelman@metoffice.gov.uk>
2023-10-12 10:36:32 +0100

    Update standardise function _rm_air_temperature_status_flag to work with deterministic forecast

--- improver_tests/standardise/test_StandardiseMetadata.py
+++ improver_tests/standardise/test_StandardiseMetadata.py
@@ -212,6 +212,46 @@ class Test_process(IrisTest):
         self.assertArrayEqual(result.data, target.data)
         self.assertEqual(result.coords(), target.coords())
 
+    def test_air_temperature_status_flag_coord_without_realization(self):
+        """
+        Ensure we handle cubes which now include an 'air_temperature_status flag'
+        coord to signify points below surface altitude, where previously this
+        was denoted by NaN values in the data.
+
+        Tests this standardisation works if input is a deterministic forecast without
+        a realization coordinate.
+        """
+        cube = set_up_variable_cube(
+            np.full((3, 5, 5), fill_value=282, dtype=np.float32),
+            spatial_grid="latlon",
+            standard_grid_metadata="gl_det",
+            pressure=True,
+            height_levels=[100000.0, 97500.0, 95000.0],
+        )
+        # The target cube has 'NaN' values in its data to denote points below
+        # surface altitude.
+        result_no_sf = cube.copy()
+        result_no_sf.data[:, 0, ...] = np.nan
+        target = self.plugin.process(result_no_sf)
+
+        cube_with_flags = cube.copy()
+        flag_status = np.zeros((3, 5, 5), dtype=np.int8)
+        flag_status[:, 0, ...] = 1
+        status_flag_coord = AuxCoord(
+            points=flag_status,
+            standard_name="air_temperature status_flag",
+            var_name="flag",
+            attributes={
+                "flag_meanings": "above_surface_pressure below_surface_pressure",
+                "flag_values": np.array([0, 1], dtype="int8"),
+            },
+        )
+        cube_with_flags.add_aux_coord(status_flag_coord, (0, 1, 2))
+
+        result = self.plugin.process(cube_with_flags)
+        self.assertArrayEqual(result.data, target.data)
+        self.assertEqual(result.coords(), target.coords())
+
 
 if __name__ == "__main__":
     unittest.main()
