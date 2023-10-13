e84bfa1254fb3429af7c6175016d0a0a855589dc
Belinda Trotta <btrotta-bom@users.noreply.github.com>
2023-10-13 11:44:39 +1100

    formatting

--- improver_tests/utilities/cube_manipulation/test_collapse_realizations.py
+++ improver_tests/utilities/cube_manipulation/test_collapse_realizations.py
@@ -50,9 +50,7 @@ def temperature_cube():
 def test_basic(temperature_cube):
     """Test that a collapsed cube is returned with no realization coord."""
     result = collapse_realizations(temperature_cube)
-    assert "realization" not in [
-        coord.name() for coord in result.coords()
-    ]
+    assert "realization" not in [coord.name() for coord in result.coords()]
     expected = temperature_cube.collapsed(["realization"], iris.analysis.MEAN)
     np.testing.assert_allclose(result.data, expected.data)
 
