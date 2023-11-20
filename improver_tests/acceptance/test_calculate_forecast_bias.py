b29e6daa6aa213ad399cff0f84c2bfcf32276be7
Benjamin Owen <benjamin.owen@bom.gov.au>
2023-11-20 15:17:52 +1100

    Update acc-tests to capture masked domains in calculate_forecast_bias

--- improver_tests/acceptance/test_calculate_forecast_bias.py
+++ improver_tests/acceptance/test_calculate_forecast_bias.py
@@ -75,3 +75,41 @@ def test_multiple_frt(tmp_path):
     ]
     run_cli(args)
     acc.compare(output_path, kgo_path)
+
+
+def test_single_frt_masked_inputs(tmp_path):
+    """
+    Test case where single historical forecast value provided.
+    """
+    kgo_dir = acc.kgo_root() / "calculate-forecast-bias"
+    kgo_path = kgo_dir / "single_frt_masked_inputs" / "kgo.nc"
+    inputs_path = (kgo_dir / "inputs/masked").glob("20220811T0300Z-PT00*.nc")
+    output_path = tmp_path / "output.nc"
+    args = [
+        *inputs_path,
+        "--truth-attribute",
+        "mosg__model_configuration=msas_det",
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_multiple_frt_masked_inputs(tmp_path):
+    """
+    Test case where multiple historical forecast values provided.
+    """
+    kgo_dir = acc.kgo_root() / "calculate-forecast-bias"
+    kgo_path = kgo_dir / "multiple_frt_masked_inputs" / "kgo.nc"
+    inputs_path = (kgo_dir / "inputs/masked").glob("202208*T0300Z-PT00*.nc")
+    output_path = tmp_path / "output.nc"
+    args = [
+        *inputs_path,
+        "--truth-attribute",
+        "mosg__model_configuration=msas_det",
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
