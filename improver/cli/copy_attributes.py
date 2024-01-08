62321688881acc66ec9805b93a60980113d31fc6
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2024-01-08 17:16:47 +0000

    Adds CLI to copy attributes from one NetCDF file to another. Includes acceptance test.

---
+++ improver/cli/copy_attributes.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Script to copy attributes from template_cube to cube"""
+
+from improver import cli
+
+
+@cli.clizefy
+@cli.with_output
+def process(
+    cube: cli.inputcube,
+    template_cube: cli.inputcube,
+    *,
+    attributes: cli.comma_separated_list,
+):
+    """
+    Copy attribute values from template_cube to cube, overwriting any existing values.
+
+    Args:
+        cube (iris.cube.Cube):
+            Source cube to be updated.
+        template_cube (iris.cube.Cube):
+            Source cube to get attribute values from.
+        attributes (list):
+            List of names of attributes to copy. If any are not present on template_cube, a
+            KeyError will be raised.
+
+    Returns:
+        iris.cube.Cube
+    """
+    from improver.metadata.amend import amend_attributes
+
+    new_attributes = {k: template_cube.attributes[k] for k in attributes}
+    amend_attributes(cube, new_attributes)
+    return cube
