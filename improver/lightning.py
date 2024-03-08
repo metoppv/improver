b73a6f438fd8e5010a5de5fe5adbc62b0b432c50
Tim A. Bonin <Timothy.Bonin@ll.mit.edu>
2024-03-08 15:08:13 -0500

    Adding the lightning probability algorithm dependent on multivariant inputs based on regression statistics.

--- improver/lightning.py
+++ improver/lightning.py
@@ -179,7 +179,9 @@ class LightningFromCapePrecip(PostProcessingPlugin):
 
 
 def latitude_to_threshold(
-    latitude: np.ndarray, midlatitude: float, tropics: float,
+    latitude: np.ndarray,
+    midlatitude: float,
+    tropics: float,
 ) -> np.ndarray:
     """
     Rescale a latitude range into a range of threshold values suitable for
@@ -204,3 +206,218 @@ def latitude_to_threshold(
         rescale(latitude, (50.0, 10), (midlatitude, tropics), clip=True),
         rescale(latitude, (-50.0, -10), (midlatitude, tropics), clip=True),
     )
+
+
+class LightningMultivariateProbability(PostProcessingPlugin):
+    """
+    The algorithm outputs the probability of at least one lightning strike within
+    20 km of the location in a three hour period. The probabilities are calculated
+    for each individual ensemble member, and then they are averaged for the ensemble
+    probability forecast.
+
+    Inputs:
+        CAPE (cape; calculated with water loading and latent heat of freezing estimated)
+        Lifted Index (liftidx)
+        Precipitable Water (pwat in mm)
+        CIN (cin)
+        3-hour Accumulated Precipitation (apcp in inches)
+    Outputs:
+        20 km lightning probability over the valid time of the accumulated precipitation
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Regression equation when CAPE and APCP are greater than zero:
+        lprob= cape*APCP
+        lprob=0.13*alog(lprob+0.7)+0.05
+
+    If APCP is very low, a separate regression equation is used to predict lightning probability.
+    The definition of “very low” is raised slightly when PWAT values are high. This is because
+    the model often produces showery precipitation that doesn’t access the actual instability in
+    very moist environments:
+        lprob_noprecip=CAPE/(CIN+100.0)
+        lprob_noprecip=0.025*alog(lprob_noprecip+0.31)+0.03
+        APCP=APCP-(PWAT/1000)
+
+        IF APCP is less than 0.01 THEN lprob=lprob_noprecip
+    If there is no CAPE but the atmosphere is “close” to unstable, lightning does sometimes occur,
+    especially when heavy precipitation may have stabilized the atmosphere in the model. Unstable
+    values of lifted index are positive here:
+        LIFTIDX=LIFTIDX+4.0
+        IF LIFTIDX is less than 0 THEN LIFTIDX=0.0
+        IF CAPE is less than 0 THEN lprob =0.2*(LIFTIDX*APCP)^0.5
+
+    Finally, the probability of lightning is reduced when there is not much PW, because graupel
+    cannot form and start the whole charging process. Therefore we reduce the probability:
+        IF PWAT is less than 20 THEN lprob=lprob*(PWAT/20.0)
+
+    Limit final probabilities to 95% as that is as skillful as the regression equations could get:
+        IF lprob is greater than 0.95 THEN lprob=0.95
+
+    Make percentage:
+        lprob=lprob*100.0
+    """
+
+    @staticmethod
+    def _get_inputs(cubes: CubeList) -> Tuple[Cube, Cube]:
+        """
+        Separates CAPE, LI, PW, CIN, and APCP cubes and checks that the following
+        match: forecast_reference_time, spatial coords, time-bound interval and
+        that CAPE time is at the lower bound of precipitation accumulation time.
+        """
+        cape = cubes.extract(
+            iris.Constraint(
+                cube_func=lambda cube: "atmosphere_specific_convective_available_potential_energy"
+                in cube.name()
+            )
+        )
+        if cape:
+            cape = cape.merge_cube()
+        else:
+            raise ValueError(
+                f"No cube named atmosphere_specific_convective_available_potential_energy found "
+                f"in {cubes}"
+            )
+        liftidx = cubes.extract(
+            iris.Constraint(
+                cube_func=lambda cube: "temperature_difference_between_ambient_air_and_air_lifted_adiabatically"
+                in cube.name()
+            )
+        )
+        if liftidx:
+            liftidx = liftidx.merge_cube()
+        else:
+            raise ValueError(
+                f"No cube named temperature_difference_between_ambient_air_and_air_lifted_adiabatically found "
+                f"in {cubes}"
+            )
+        pwat = cubes.extract(
+            iris.Constraint(cube_func=lambda cube: "precipitable_water" in cube.name())
+        )
+        if pwat:
+            pwat = pwat.merge_cube()
+        else:
+            raise ValueError(f"No cube named precipitable_water found " f"in {cubes}")
+        cin = cubes.extract(
+            iris.Constraint(
+                cube_func=lambda cube: "atmosphere_specific_convective_inhibition"
+                in cube.name()
+            )
+        )
+        if cin:
+            cin = cin.merge_cube()
+        else:
+            raise ValueError(
+                f"No cube named atmosphere_specific_convective_inhibition found "
+                f"in {cubes}"
+            )
+        apcp = cubes.extract(
+            iris.Constraint(
+                cube_func=lambda cube: "precipitation_amount" in cube.name()
+            )
+        )
+        if apcp:
+            apcp = apcp.merge_cube()
+        else:
+            raise ValueError(f"No cube named precipitation_amount found in {cubes}")
+
+        (cape_time,) = list(cape.coord("time").cells())
+        (apcp_time,) = list(apcp.coord("time").cells())
+        if cape_time.point != apcp_time.bound[0]:
+            raise ValueError(
+                f"CAPE cube time ({cape_time.point}) should be valid at the "
+                f"precipitation_accumulation cube lower bound ({apcp_time.bound[0]})."
+            )
+        if np.diff(apcp_time.bound) not in [timedelta(hours=3)]:
+            raise ValueError(
+                f"Precipitation_accumulation cube time window must be three hours, "
+                f"not {np.diff(apcp_time.bound)}."
+            )
+        # Following time/space checks should be made for all diagnostics
+        if cape.coord("forecast_reference_time") != apcp.coord(
+            "forecast_reference_time"
+        ):
+            raise ValueError(
+                "Supplied cubes must have the same forecast reference times"
+            )
+        if not spatial_coords_match([cape, apcp]):
+            raise ValueError("Supplied cubes do not have the same spatial coordinates")
+
+        return cape, liftidx, pwat, cin, apcp
+
+    def process(self, cubes: CubeList, model_id_attr: str = None) -> Cube:
+        """
+        From the supplied CAPE, LIFTIDX, PWAT, CIN, APCP cubes, calculate a probability
+        of lightning cube.
+
+        Args:
+            cubes:
+                Cubes of CAPE, LIFTIDX, PWAT, CIN, APCP.
+            model_id_attr:
+                The name of the dataset attribute to be used to identify the source
+                model when blending data from different models.
+
+        Returns:
+            Cube of lightning data
+
+        Raises:
+            ValueError:
+                If one of the cubes is not found or doesn't match the other
+        """
+        cape, liftidx, pwat, cin, apcp = self._get_inputs(cubes)
+
+        # Regression equations require math on cubes with incompatible units, so strip data from cubes
+        templ = apcp.copy()
+        cape = cape.data
+        liftidx = liftidx.data
+        pwat = pwat.data
+        cin = cin.data
+        apcp = apcp.data / 1000 * 39.3701  # convert (kg m-3) to (inches)
+
+        # Regression equation when CAPE and APCP are greater than zero:
+        lprob = cape * apcp
+        lprob = 0.13 * np.log(lprob + 0.7) + 0.05
+
+        # If APCP is very low, a separate regression equation is used to predict lightning probability.
+        # The definition of “very low” is raised slightly when PWAT values are high. This is because
+        # the model often produces showery precipitation that doesn’t access the actual instability in
+        # very moist environments:
+        lprob_noprecip = cape / (cin + 100.0)
+        lprob_noprecip = 0.025 * np.log(lprob_noprecip + 0.31) + 0.03
+        apcp = apcp - (pwat / 1000)
+
+        lprob[np.where(apcp < 0.01)] = lprob_noprecip[np.where(apcp < 0.01)]
+
+        # If there is no CAPE but the atmosphere is “close” to unstable, lightning does sometimes occur,
+        # especially when heavy precipitation may have stabilized the atmosphere in the model. Unstable
+        # values of lifted index are positive here:
+        liftidx = liftidx + 4.0
+
+        liftidx[liftidx < 0] = 0
+
+        lprob[np.where(cape <= 0)] = (
+            0.2 * (liftidx[np.where(cape <= 0)] * apcp[np.where(cape <= 0)]) ** 0.5
+        )
+
+        # Finally, the probability of lightning is reduced when there is not much PWAT, because graupel
+        # cannot form and start the whole charging process. Therefore we reduce the probability:
+        lprob[np.where(pwat < 20)] = lprob[np.where(pwat < 20)] * (
+            pwat[np.where(pwat < 20)] / 20.0
+        )
+
+        # Limit final probabilities to 95% as that is as skillful as the regression equations could get:
+        lprob[lprob > 0.95] = 0.95
+
+        # Make percentage:
+        data = lprob * 100.0
+
+        cube = create_new_diagnostic_cube(
+            name="20_km_lightning_probability_over_the_valid_time_of_the_accumulated_precipitation",
+            units="1",
+            template_cube=templ,
+            data=data.astype(FLOAT_DTYPE),
+            mandatory_attributes=generate_mandatory_attributes(
+                cubes, model_id_attr=model_id_attr
+            ),
+        )
+
+        return cube
---
+++ improver_tests/lightning/test_LightningMultivariateProbability.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# (C) British Crown copyright. The Met Office.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Test methods in lightning.LightningUSAF"""
+
+from datetime import datetime
+
+import numpy as np
+import pytest
+from iris.cube import Cube, CubeList
+
+from improver.lightning import LightningMultivariateProbability
+from improver.metadata.constants import FLOAT_DTYPE
+from improver.metadata.constants.attributes import MANDATORY_ATTRIBUTE_DEFAULTS
+from improver.metadata.utilities import create_new_diagnostic_cube
+from improver.synthetic_data.set_up_test_cubes import (
+    add_coordinate,
+    set_up_probability_cube,
+    set_up_variable_cube,
+)
+
+
+@pytest.fixture(name="cape_cube")
+def cape_cube_fixture() -> Cube:
+    """
+    Set up a CAPE cube for use in tests.
+    Has 2 realizations, 7 latitudes spanning from 60S to 60N and 3 longitudes.
+    """
+
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=4000)
+    cube = set_up_variable_cube(
+        data,
+        name="atmosphere_specific_convective_available_potential_energy",
+        units="J kg-1",
+        time=datetime(2017, 11, 10, 3, 0),
+        time_bounds=None,
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+@pytest.fixture(name="cin_cube")
+def cin_cube_fixture() -> Cube:
+    """
+    Set up a liftidx cube for use in tests.
+    Has 2 realizations, 7 latitudes spanning from 60S to 60N and 3 longitudes.
+    """
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=-0.25)
+    cube = set_up_variable_cube(
+        data,
+        name="atmosphere_specific_convective_inhibition",
+        units="J kg-1",
+        time=datetime(2017, 11, 10, 3, 0),
+        time_bounds=None,
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+@pytest.fixture(name="liftidx_cube")
+def liftidx_cube_fixture() -> Cube:
+    """
+    Set up a liftidx cube for use in tests.
+    Has 2 realizations, 7 latitudes spanning from 60S to 60N and 3 longitudes.
+    """
+
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=10)
+    cube = set_up_variable_cube(
+        data,
+        name="temperature_difference_between_ambient_air_and_air_lifted_adiabatically",
+        units="K",
+        time=datetime(2017, 11, 10, 3, 0),
+        time_bounds=None,
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+@pytest.fixture(name="pwat_cube")
+def pwat_cube_fixture() -> Cube:
+    """
+    Set up a pwat cube for use in tests.
+    Has 2 realizations, 7 latitudes spanning from 60S to 60N and 3 longitudes.
+    """
+
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=3)
+    cube = set_up_variable_cube(
+        data,
+        name="precipitable_water",
+        units="kg m-2",
+        time=datetime(2017, 11, 10, 3, 0),
+        time_bounds=None,
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+@pytest.fixture(name="apcp_cube")
+def apcp_cube_fixture() -> Cube:
+    """
+    Set up a apcp cube for use in tests.
+    Has 2 realizations, 7 latitudes spanning from 60S to 60N and 3 longitudes.
+    """
+
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=6)
+    cube = set_up_variable_cube(
+        data,
+        name="precipitation_amount",
+        units="kg m-2",
+        time=datetime(2017, 11, 10, 6, 0),
+        time_bounds=(datetime(2017, 11, 10, 3, 0), datetime(2017, 11, 10, 6, 0)),
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+@pytest.fixture(name="expected_cube")
+def expected_cube_fixture() -> Cube:
+    """
+    Set up the Lightning cube that we expect to get from the plugin
+    """
+
+    data = np.full((2, 7, 3), dtype=np.float32, fill_value=14.111012)
+    cube = set_up_variable_cube(
+        data,
+        name="20_km_lightning_probability_over_the_valid_time_of_the_accumulated_precipitation",
+        units="1",
+        time=datetime(2017, 11, 10, 4, 30),
+        time_bounds=(datetime(2017, 11, 10, 3, 0), datetime(2017, 11, 10, 6, 0)),
+        attributes=None,
+        standard_grid_metadata="gl_ens",
+        domain_corner=(-60, 0),
+        grid_spacing=20,
+    )
+    return cube
+
+
+def test_basic(cape_cube, liftidx_cube, pwat_cube, cin_cube, apcp_cube, expected_cube):
+    """Run the plugin and check the result cube matches the expected_cube"""
+    result = LightningMultivariateProbability()(
+        CubeList([cape_cube, liftidx_cube, pwat_cube, cin_cube, apcp_cube]), None
+    )
+    assert np.allclose(result.data, expected_cube.data)
