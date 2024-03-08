004ebe9763a98224f0289b721794b3b29a691ab9
peter.jordan <pjordan@vld217.cmpd1.metoffice.gov.uk>
2024-03-08 15:42:26 +0000

    Prepared to implement equalarea logic. Wrote failing test.

--- improver_tests/utilities/test_DistanceBetweenGridPoints.py
+++ improver_tests/utilities/test_DistanceBetweenGridPoints.py
@@ -51,6 +51,37 @@ X_GRID_SPACING_AT_20_DEGREES_NORTH = 1044735  # Meters
 Y_GRID_SPACING = 1111949  # Meters
 
 
+
+
+#Todo: Need to think about how to handle equalarea cubes. Considerations are:
+'''
+Calculation already exists for equalarea cube (spatial.calculate_grid_spacing()) but this returns a float, whereas I need a cube.
+Options are:
+1. Use this function in my Distance class... seems hacky.
+2. Copy the logic from this function into my class :'(
+3. Make my class just handle the latlon projection and have the gradient class call the function for equalarea then boost the float up to a cube.
+4. Move the function logic into the class and then have the function use the class for backwards compatibility.   - Maybe the best option. Function logic could be in a public static method so that both function and class can use it.
+
+ToDo:
+Move function logic into static method on class. Make sure the function's tests all still pass.
+Add logic for class to check which projection we're using and route accordingly.
+Make sure latlong tests still pass.
+Write test for equal area projection.
+Implement logic in class to make this test pass.
+Add edge case tests and bugfix as needed.
+
+Cup of tea.
+
+Go back to gradient class and make it use the distance class. Existing test should give a good starting point.
+Edge case tests for gradient.
+Tidying up.
+'''
+
+def make_equalarea_cube(shape, grid_spacing):
+    data = np.ones(shape, dtype=np.float32)
+    cube = set_up_variable_cube(data, spatial_grid="equalarea", grid_spacing=grid_spacing)
+    return cube
+
 def make_latlon_test_cube(shape, latitudes, longitudes):
     example_data = np.ones(shape, dtype=np.float32)
     dimcoords = [(DimCoord(latitudes, standard_name="latitude", units="degrees", coord_system=GeogCS), 0),
@@ -103,3 +134,14 @@ def test_latlon_cube_nonuniform_spacing():
         assert result.units == "meters"
         np.testing.assert_allclose(result.data, expected.data, rtol=2e-3,
                                    atol=0)  # Allowing 0.2% error for difference between the spherical earth assumption used by the implementation and the full haversine equation used to generate the test data.
+
+
+def test_equalarea_cube():
+    input_cube = make_equalarea_cube((3, 3), grid_spacing=1000)
+    expected_x_distances = np.full((3,2), 1000)
+    expected_y_distances = np.full((2, 3), 1000)
+    calculated_x_distances_cube, calculated_y_distances_cube = DistanceBetweenGridSquares()(input_cube)
+    for result, expected in zip((calculated_x_distances_cube, calculated_y_distances_cube),
+                                (expected_x_distances, expected_y_distances)):
+        assert result.units == "meters"
+        np.testing.assert_allclose(result.data, expected.data, rtol=2e-5, atol=0)
