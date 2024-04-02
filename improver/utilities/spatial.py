c3eaa4ee6b9597277d908221fbbf96aa00cdbb79
MO-PeterJordan <52462411+MO-PeterJordan@users.noreply.github.com>
2024-04-02 15:44:37 +0100

    And another
    Co-authored-by: Stephen Moseley <stephen.moseley@metoffice.gov.uk>

--- improver/utilities/spatial.py
+++ improver/utilities/spatial.py
@@ -265,7 +265,7 @@ class DistanceBetweenGridSquares(BasePlugin):
 
         Returns:
             A cube containing the horizontal distances between the grid points of the input
-            cube in meters.
+            cube in metres.
         """
         lats, longs = cls._get_latlon_cube_points(cube)
 
