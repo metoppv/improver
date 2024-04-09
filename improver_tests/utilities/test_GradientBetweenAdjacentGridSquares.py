77877fff2b4d297cd675447fec11642e28c988bb
peter.jordan <pjordan@vld217.cmpd1.metoffice.gov.uk>
2024-04-09 09:38:26 +0100

    Got part way to testing wrap-around interpolaiton. Found a bug with non wrap-around calculations for large angles along the way, and got part way through fixing it

--- improver_tests/utilities/test_GradientBetweenAdjacentGridSquares.py
+++ improver_tests/utilities/test_GradientBetweenAdjacentGridSquares.py
@@ -58,11 +58,11 @@ EXAMPLE_INPUT_DATA_4 = np.array(
 
 EQUAL_AREA_GRID_SPACING = 1000  # Metres
 LATITUDE_GRID_SPACING = 10  # Degrees
-LONGITUDE_GRID_SPACING = 120  # Degrees
+LONGITUDE_GRID_SPACING = 15 # 5 # 10 # 120  # Degrees
 # Distances covered when travelling 10 degrees north-south or east-west:
-X_GRID_SPACING_AT_EQUATOR = 13343391  # Metres/10 degrees * 12 = meters/120 degrees
-X_GRID_SPACING_AT_10_DEGREES_NORTH = 13015378  # Metres/10 degrees * 12 = meters/120 degrees
-X_GRID_SPACING_AT_20_DEGREES_NORTH = 12113276  # Metres/10 degrees * 12 = meters/120 degrees # TODO: the * 12 is probaly unneccesarily confusing.
+X_GRID_SPACING_AT_EQUATOR = 13343391 / 8 # 1223144 # 555975 # 1111949 # 13343391  # Metres/10 degrees * 12 = meters/120 degrees
+X_GRID_SPACING_AT_10_DEGREES_NORTH = 13015378  / 8 # 1204506 # 547523 # 1095014 # 13015378  # Metres/10 degrees * 12 = meters/120 degrees
+X_GRID_SPACING_AT_20_DEGREES_NORTH = 12113276 / 8 # 1149172 # 522426 # 1044735 # 12113276  # Metres/10 degrees * 12 = meters/120 degrees # TODO: the * 12 is probaly unneccesarily confusing.
 Y_GRID_SPACING = 1111949  # Metres/10 degrees
 
 
@@ -248,7 +248,7 @@ def test_gradient_equal_area_coords(make_input, make_expected, grid, input_data)
         EXAMPLE_INPUT_DATA_4,
     ],
 )
-@pytest.mark.parametrize("wrap_around_meridian", [False])
+@pytest.mark.parametrize("wrap_around_meridian", [False])  # TODO: Also True!
 def test_gradient_lat_lon_coords(make_input, make_expected, grid, input_data, wrap_around_meridian):
     """
     Check calculating the gradient with and without regridding
