a1e112be1c5240404183976492ac2779739c20d8
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2024-05-10 09:09:50 +0100

    Change test paramerisation to help with testing spot forecast subsetting in subsequent PR.

--- improver_tests/spotdata/test_SpotManipulation.py
+++ improver_tests/spotdata/test_SpotManipulation.py
@@ -51,14 +51,13 @@ from improver.synthetic_data.set_up_test_cubes import (
 ATTRIBUTES = {"mosg__grid_domain": "global", "mosg__grid_type": "standard"}
 
 
-@pytest.fixture
-def gridded_variable(grid_data):
+def gridded_variable(forecast_data):
     """Create a gridded variable cube from which to extract spot foreasts."""
     height_crd = DimCoord(
         np.array([1.5], dtype=np.float32), standard_name="height", units="m"
     )
     return set_up_variable_cube(
-        grid_data, include_scalar_coords=[height_crd], attributes=ATTRIBUTES,
+        forecast_data, include_scalar_coords=[height_crd], attributes=ATTRIBUTES,
     )
 
 
@@ -77,20 +76,18 @@ def gridded_lapse_rate(lapse_rates):
     )
 
 
-@pytest.fixture
-def gridded_percentiles(grid_data):
+def gridded_percentiles(forecast_data):
     """Create a gridded percentile cube from which to extract spot foreasts."""
-    n_percentiles = grid_data.shape[0]
+    n_percentiles = forecast_data.shape[0]
     percentiles = np.linspace(20, 80, n_percentiles)
-    return set_up_percentile_cube(grid_data, percentiles, attributes=ATTRIBUTES,)
+    return set_up_percentile_cube(forecast_data, percentiles, attributes=ATTRIBUTES,)
 
 
-@pytest.fixture
-def gridded_probabilities(grid_data):
+def gridded_probabilities(forecast_data):
     """Create a gridded probability cube from which to extract spot foreasts."""
-    n_thresholds = grid_data.shape[0]
+    n_thresholds = forecast_data.shape[0]
     thresholds = np.linspace(273, 283, n_thresholds)
-    return set_up_probability_cube(grid_data, thresholds, attributes=ATTRIBUTES,)
+    return set_up_probability_cube(forecast_data, thresholds, attributes=ATTRIBUTES,)
 
 
 @pytest.fixture
@@ -141,61 +138,43 @@ def add_grid_hash(target, source):
 
 
 @pytest.mark.parametrize(
-    "grid_data,neighbour_data,expected",
+    "ftype,forecast_data,neighbour_data,kwargs,expected",
     [
+        # Simple extraction of forecast variable from grid.
         (
+            gridded_variable,
             np.arange(273, 282).reshape(3, 3),
             np.array([[[1, 2], [0, 0], [5, 10]]]),
+            {},
             np.array([274, 275]),
         ),
+        # Another simple extraction for a different point.
         (
+            gridded_variable,
             np.arange(273, 282).reshape(3, 3),
             np.array([[[1, 2], [2, 2], [5, 10]]]),
+            {},
             np.array([280, 281]),
         ),
-    ],
-)
-def test_basic_extraction(gridded_variable, neighbour_cube, expected):
-    """Test that basic spot extraction via the SpotManipulation plugin returns
-    the expected values."""
-
-    add_grid_hash(neighbour_cube, gridded_variable)
-    result = SpotManipulation()([gridded_variable, neighbour_cube])
-    assert_array_equal(result.data, expected)
-    assert result.attributes.get("model_grid_hash", None) is None
-
-
-@pytest.mark.parametrize(
-    "grid_data,neighbour_data,kwargs,expected",
-    [
+        # Extract existing percentiles from a percentile input cube.
         (
+            gridded_percentiles,
             np.arange(273, 291).reshape(2, 3, 3),
             np.array([[[1, 2], [0, 0], [5, 10]]]),
             {"extract_percentiles": [20, 80]},
             np.array([[274, 275], [283, 284]]),
         ),
+        # Resample existing percentiles to extract target percentile.
         (
+            gridded_percentiles,
             np.arange(273, 291).reshape(2, 3, 3),
             np.array([[[1, 2], [0, 0], [5, 10]]]),
             {"extract_percentiles": [50]},
             np.array([278.5, 279.5]),
         ),
-    ],
-)
-def test_percentile_extraction(gridded_percentiles, neighbour_cube, kwargs, expected):
-    """Test that percentiles can be extracted, either extracting existing
-    percentiles on a percentile input cube, or resampling existing percentiles
-    if needed."""
-
-    add_grid_hash(neighbour_cube, gridded_percentiles)
-    result = SpotManipulation(**kwargs)([gridded_percentiles, neighbour_cube])
-    assert_array_equal(result.data, expected)
-
-
-@pytest.mark.parametrize(
-    "grid_data,neighbour_data,kwargs,expected",
-    [
+        # Extract single percentile from a probability input cube.
         (
+            gridded_probabilities,
             np.stack(
                 [
                     np.full((3, 3), 1.0, dtype=np.float32),
@@ -209,7 +188,9 @@ def test_percentile_extraction(gridded_percentiles, neighbour_cube, kwargs, expe
             {"extract_percentiles": [50]},
             np.array([278, 278]),
         ),
+        # Extract multiple percentiles from a probability input cube.
         (
+            gridded_probabilities,
             np.stack(
                 [
                     np.full((3, 3), 1.0, dtype=np.float32),
@@ -225,64 +206,49 @@ def test_percentile_extraction(gridded_percentiles, neighbour_cube, kwargs, expe
         ),
     ],
 )
-def test_percentiles_from_probabilities(
-    gridded_probabilities, neighbour_cube, kwargs, expected
-):
-    """Test that percentiles can be extracted from a probability cube."""
+def test_extraction(ftype, forecast_data, neighbour_cube, kwargs, expected):
+    """Test extraction options from simple variable cubes, percentile cubes,
+    and probability cubes."""
 
-    add_grid_hash(neighbour_cube, gridded_probabilities)
-    result = SpotManipulation(**kwargs)([gridded_probabilities, neighbour_cube])
+    forecast = ftype(forecast_data)
+
+    add_grid_hash(neighbour_cube, forecast)
+    result = SpotManipulation(**kwargs)([forecast, neighbour_cube])
     assert_array_equal(result.data, expected)
+    assert result.attributes.get("model_grid_hash", None) is None
 
 
-@pytest.mark.parametrize(
-    "grid_data,neighbour_data,kwargs,expected",
-    [
-        (
-            np.arange(273, 291).reshape(2, 3, 3),
-            np.array([[[1, 2], [0, 0], [5, 10]]]),
-            {},
-            np.array([[274, 275], [283, 284]]),
-        ),
-        (
-            np.arange(273, 291).reshape(2, 3, 3),
-            np.array([[[1, 2], [0, 0], [5, 10]]]),
-            {"suppress_warnings": True},
-            np.array([[274, 275], [283, 284]]),
-        ),
-    ],
-)
-def test_unknown_prob_type_warning(
-    gridded_percentiles, neighbour_cube, kwargs, expected
-):
+@pytest.mark.parametrize("neighbour_data", [np.array([[[1, 2], [0, 0], [5, 10]]])])
+def test_unknown_prob_type_warning(neighbour_cube):
     """Test a warning is raised if percentiles are requested from a cube from
     which they cannot be extracted and that all data is returned in spot
     format. In this case that is the 20th and 80th pecentiles are both
     returned rather than the requested 50th percentile. If the
     suppress_warnings option is set then test that no warning is raised."""
 
-    add_grid_hash(neighbour_cube, gridded_percentiles)
-    gridded_percentiles.coord("percentile").rename("kittens")
+    expected = np.array([[274, 275], [283, 284]])
+    forecast = gridded_percentiles(np.arange(273, 291).reshape(2, 3, 3))
+    forecast.coord("percentile").rename("kittens")
+    add_grid_hash(neighbour_cube, forecast)
 
-    if "suppress_warnings" in kwargs.keys():
-        with warnings.catch_warnings():
-            warnings.simplefilter("error")
-            result = SpotManipulation(extract_percentiles=[50], **kwargs)(
-                [gridded_percentiles, neighbour_cube]
-            )
-    else:
-        with pytest.warns(
-            UserWarning, match="Diagnostic cube is not a known probabilistic type"
-        ):
-            result = SpotManipulation(extract_percentiles=[50], **kwargs)(
-                [gridded_percentiles, neighbour_cube]
-            )
+    # Test with warning suppression
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        result = SpotManipulation(extract_percentiles=[50], suppress_warnings=True)(
+            [forecast, neighbour_cube]
+        )
+    assert_array_equal(result.data, expected)
 
+    # Test without warning suppression
+    with pytest.warns(
+        UserWarning, match="Diagnostic cube is not a known probabilistic type"
+    ):
+        result = SpotManipulation(extract_percentiles=[50])([forecast, neighbour_cube])
     assert_array_equal(result.data, expected)
 
 
 @pytest.mark.parametrize(
-    "grid_data,lapse_rates,neighbour_data,kwargs,expected",
+    "forecast_data,lapse_rates,neighbour_data,kwargs,expected",
     [
         (
             np.arange(273, 282).reshape(3, 3),
@@ -301,22 +267,23 @@ def test_unknown_prob_type_warning(
     ],
 )
 def test_lapse_rate_correction(
-    gridded_variable, gridded_lapse_rate, neighbour_cube, kwargs, expected
+    forecast_data, gridded_lapse_rate, neighbour_cube, kwargs, expected
 ):
     """Test the application of lapse rates using a lapse rate cube and a fixed
     lapse rate value."""
 
-    add_grid_hash(neighbour_cube, gridded_variable)
-    inputs = [gridded_variable, gridded_lapse_rate, neighbour_cube]
+    forecast = gridded_variable(forecast_data)
+    add_grid_hash(neighbour_cube, forecast)
+    inputs = [forecast, gridded_lapse_rate, neighbour_cube]
     if "fixed_lapse_rate" in kwargs.keys():
-        inputs = [gridded_variable, neighbour_cube]
+        inputs = [forecast, neighbour_cube]
 
     result = SpotManipulation(**kwargs)(inputs)
     assert_array_equal(result.data, expected)
 
 
 @pytest.mark.parametrize(
-    "grid_data,neighbour_data,kwargs,expected",
+    "forecast_data,neighbour_data,kwargs,expected",
     [
         (
             np.arange(273, 282).reshape(3, 3),
@@ -332,21 +299,22 @@ def test_lapse_rate_correction(
         ),
     ],
 )
-def test_missing_lapse_rate_warning(gridded_variable, neighbour_cube, kwargs, expected):
+def test_missing_lapse_rate_warning(forecast_data, neighbour_cube, kwargs, expected):
     """Test a warning is raised if no lapse rate is specified whilst the
     apply_lapse_rate_correction kwarg is set to True. If the
     suppress_warnings option is set then test that no warning is raised."""
 
-    add_grid_hash(neighbour_cube, gridded_variable)
+    forecast = gridded_variable(forecast_data)
+    add_grid_hash(neighbour_cube, forecast)
 
     if "suppress_warnings" in kwargs.keys():
         with warnings.catch_warnings():
             warnings.simplefilter("error")
-            result = SpotManipulation(**kwargs)([gridded_variable, neighbour_cube])
+            result = SpotManipulation(**kwargs)([forecast, neighbour_cube])
     else:
         with pytest.warns(
             UserWarning, match="A lapse rate cube or fixed lapse rate was not provided"
         ):
-            result = SpotManipulation(**kwargs)([gridded_variable, neighbour_cube])
+            result = SpotManipulation(**kwargs)([forecast, neighbour_cube])
 
     assert_array_equal(result.data, expected)
