a2db8fc570f52ea665a04928d2206f510d34cced
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2024-09-25 15:18:36 +0100

    licence

--- improver_tests/test_api.py
+++ improver_tests/test_api.py
@@ -1,3 +1,7 @@
+# (C) Crown copyright, Met Office. All rights reserved.
+#
+# This file is part of IMPROVER and is released under a BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
 import pytest
 
 from improver import api
