2114b4d3c1823596e7f8d7923a33735e4b797769
Stephen Moseley <stephen.moseley@metoffice.gov.uk>
2024-10-04 11:12:25 +0100

    Adds acceptance test

--- improver/cli/gradient_between_adjacent_grid_squares.py
+++ improver/cli/gradient_between_adjacent_grid_squares.py
@@ -3,7 +3,7 @@
 #
 # This file is part of IMPROVER and is released under a BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
-"""CLI to generate hail size."""
+"""CLI to calculate the gradient between adjacent grid squares."""
 from improver import cli
 
 
