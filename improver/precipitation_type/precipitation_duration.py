320b13b04b8fbe330a6e2b775591b805444e27d9
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2025-01-15 11:40:19 +0000

    Precipitation classification plugin in a working and tidied state. Still needs unit tests, a CLI, and associated acceptance tests.

--- improver/precipitation_type/precipitation_duration.py
+++ improver/precipitation_type/precipitation_duration.py
@@ -4,22 +4,25 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Module containing the PrecipitationDuration class."""
 
-from typing import Union, Tuple
+from numbers import Number
+from typing import Tuple, Union
 
 import numpy as np
-from iris import analysis
-from iris.cube import Cube, CubeList
+from iris import Constraint, analysis
 from iris.coords import AuxCoord
-from iris import Constraint
+from iris.cube import Cube, CubeList
+from numpy import ndarray
 
 from improver import PostProcessingPlugin
+from improver.metadata.probabilistic import (
+    get_threshold_coord_name_from_probability_name,
+)
 from improver.metadata.utilities import (
     create_new_diagnostic_cube,
     generate_mandatory_attributes,
 )
-from improver.metadata.probabilistic import get_threshold_coord_name_from_probability_name
 from improver.utilities.common_input_handle import as_cubelist
-from improver.utilities.cube_manipulation import MergeCubes
+from improver.utilities.cube_manipulation import MergeCubes, collapse_time
 
 
 class PrecipitationDuration(PostProcessingPlugin):
@@ -27,32 +30,30 @@ class PrecipitationDuration(PostProcessingPlugin):
     Classifies periods of precipitation intensity using a mix of the maximum
     precipitation rate in the period and the accumulation in the period. These
     classified periods are then used to determine what fraction of a
-    constructed longer period would be classified as such. If the target
-    period is the same as the native period of the inputs then the returned
-    fractions will be binary.
+    constructed longer period would be classified as such.
     """
 
     def __init__(
-            self,
-            min_accumulation_per_hour: float,
-            critical_rate: float,
-            target_period: float,
-            rate_diagnostic: str = "probability_of_lwe_precipitation_rate_above_threshold",
-            accumulation_diagnostic: str = "probability_of_lwe_thickness_of_precipitation_amount_above_threshold",
-        ) -> None:
+        self,
+        min_accumulation_per_hour: float,
+        critical_rate: float,
+        target_period: float,
+        rate_diagnostic: str = "probability_of_lwe_precipitation_rate_above_threshold",
+        accumulation_diagnostic: str = "probability_of_lwe_thickness_of_precipitation_amount_above_threshold",
+    ) -> None:
         """
         Initialise the class.
 
         Args:
             min_accumulation_per_hour:
-                The minimum accumulation per hour in the period to define that
-                period as light, heavy, etc. To be classified as such the
-                critical_rate threshold below must also be satisfied.
+                The minimum accumulation per hour in the period, or a list
+                thereof, used to classify the period. The accumulation is
+                used in conjuction wuth the critical rate.
                 Units of mm.
             critical_rate:
-                A rate threshold, which if the maximum rate in the period
-                is in excess of contributes to defining the period as being
-                light, heavy, etc. Units of mm/hr.
+                A rate threshold, or list of rate thresholds, which if the
+                maximum rate in the period is in excess of contributes to
+                classifying the period. Units of mm/hr.
             target_period:
                 The period in hours that the final diagnostic represents.
                 This should be equivalent to the period covered by the inputs.
@@ -67,12 +68,19 @@ class PrecipitationDuration(PostProcessingPlugin):
                 The expected diagnostic name for the accumulation in period
                 diagnostic.
         """
+        if isinstance(min_accumulation_per_hour, Number):
+            min_accumulation_per_hour = [min_accumulation_per_hour]
+        if isinstance(critical_rate, Number):
+            critical_rate = [critical_rate]
+
         self.min_accumulation_per_hour = min_accumulation_per_hour
         self.critical_rate = critical_rate
         self.target_period = target_period
         self.rate_diagnostic = rate_diagnostic
         self.accumulation_diagnostic = accumulation_diagnostic
         self.period = None
+        self.acc_threshold = None
+        self.rate_threshold = None
 
     def _period_in_hours(self, cubes: CubeList) -> None:
         """Get the periods of the input cubes, check they are equal and
@@ -86,16 +94,18 @@ class PrecipitationDuration(PostProcessingPlugin):
         """
         periods = []
         for cube in cubes:
-            cube_periods = [np.diff(ctime.bound)[0].seconds for ctime in cube.coord("time").cells()]
+            cube_periods = [
+                np.diff(ctime.bound)[0].seconds for ctime in cube.coord("time").cells()
+            ]
             periods.extend(set(cube_periods))
 
         try:
-            period, = set(periods)
+            (period,) = set(periods)
         except ValueError as err:
             raise ValueError(
                 "Cube with multiple times with inconsistent periods. Cannot "
                 f"return a single time period. Periods are: {period}."
-                ) from err
+            ) from err
 
         self.period = period / 3600
 
@@ -105,23 +115,21 @@ class PrecipitationDuration(PostProcessingPlugin):
         up by the input cube period. So a 0.1 mm accumulation threshold
         will become 0.3 mm for a 3-hour input cube, converted to metres.
 
-        Args:
-            period:
-                Period of input cubes in hours.
-
         Returns:
             Tuple of float values, one the accumulation threshold, the other
             the rate threshold.
         """
 
-        min_accumulation_per_hour = AuxCoord([self.min_accumulation_per_hour], units="mm")
-        critical_rate = AuxCoord([self.critical_rate], units="mm/hr")
+        min_accumulation_per_hour = AuxCoord(self.min_accumulation_per_hour, units="mm")
+        critical_rate = AuxCoord(self.critical_rate, units="mm/hr")
         min_accumulation_per_hour.convert_units("m")
         critical_rate.convert_units("m/s")
 
-        return min_accumulation_per_hour.points[0] * self.period, critical_rate.points[0]
+        return min_accumulation_per_hour.points * self.period, critical_rate.points
 
-    def construct_constraints(self, accumulation_threshold: float, rate_threshold: float) -> Tuple[Constraint, Constraint]:
+    def _construct_constraints(
+        self, accumulation_threshold: float, rate_threshold: float
+    ) -> Tuple[Constraint, Constraint]:
         """Construct constraints to use in extracting the accumulation and
         rate data, at relevant thresholds, from the input cubes.
 
@@ -133,28 +141,52 @@ class PrecipitationDuration(PostProcessingPlugin):
         Returns:
             Tuple containing an accumulation and rate constraint.
         """
-        threshold_name = get_threshold_coord_name_from_probability_name(self.accumulation_diagnostic)
+        self.acc_threshold = get_threshold_coord_name_from_probability_name(
+            self.accumulation_diagnostic
+        )
         accumulation_constraint = Constraint(
             self.accumulation_diagnostic,
-            coord_values={threshold_name: lambda cell: np.isclose(cell.point, accumulation_threshold).any()}
+            coord_values={
+                self.acc_threshold: lambda cell: np.isclose(
+                    cell.point, accumulation_threshold
+                ).any()
+            },
         )
-        threshold_name = get_threshold_coord_name_from_probability_name(self.rate_diagnostic)
+        self.rate_threshold = get_threshold_coord_name_from_probability_name(
+            self.rate_diagnostic
+        )
         rate_constraint = Constraint(
             self.rate_diagnostic,
-            coord_values={threshold_name: lambda cell: np.isclose(cell.point, rate_threshold).any()}
+            coord_values={
+                self.rate_threshold: lambda cell: np.isclose(
+                    cell.point, rate_threshold
+                ).any()
+            },
         )
         return accumulation_constraint, rate_constraint
 
-    def _create_output_cube(self, data, cubes):
+    def _create_output_cube(self, data: ndarray, cubes: Tuple[Cube, Cube]) -> Cube:
+        """Create an output cube for the final diagnostic, which is the
+        fraction of the constructed period that has been classified as wet
+        using the various accumulation and peak rate thresholds.
 
+        Args:
+            data:
+                The data to be stored in the cube.
+            cubes:
+                Cubes from which to generate attributes, and the first of
+                which is used as a template from which to take coordinates
+                etc.
+        Returns:
+            A Cube with a suitable name and metadata for the diagnostic data
+            stored within it.
+        """
         classified_precip_cube = create_new_diagnostic_cube(
             "fraction_of_periods_classified_as_wet",
             "1",
             template_cube=cubes[0],
             mandatory_attributes=generate_mandatory_attributes(cubes),
             optional_attributes={
-                "minimum_peak_rate (mm/hr)": self.critical_rate,
-                "accumulation_minimum (mm)": np.around(self.min_accumulation_per_hour * self.period, decimals=3),
                 "input_period_in_hours": self.period,
             },
             data=data,
@@ -162,23 +194,29 @@ class PrecipitationDuration(PostProcessingPlugin):
         return classified_precip_cube
 
     def process(self, *cubes: Union[Cube, CubeList]) -> Cube:
-        """Calculates the hail fraction using the maximum vertical updraught,
-        the hail_size, the cloud condensation level temperature, the convective cloud
-        top temperature, the altitude of the hail to rain phase change and the
-        orography.
+        """Produce a diagnostic that provides the fraction of the day that
+        can be classified as exceeding the given thresholds. The fidelity of
+        the returned product depends upon the period of the input cubes.
 
         Args:
             cubes:
-                max_precip_rate: Maximum preciptation rate in a period.
-                precip_accumulation: Precipitation accumulation in a period.
+                Cubes covering the expected period that include cubes of:
+                    max_precip_rate: Maximum preciptation rate in a period.
+                    precip_accumulation: Precipitation accumulation in a period.
 
         Returns:
-            Something.
+            A cube of fraction of the target period that is classified as
+            exceeding the user specified thresholds. The short period inputs
+            are compared against these thresholds to be classified before
+            determining how many such periods (what fraction) of the target
+            period has been so classified.
         """
         cubes = as_cubelist(*cubes)
         self._period_in_hours(cubes)
         accumulation_threshold, rate_threshold = self._construct_thresholds()
-        accumulation_constraint, rate_constraint = self.construct_constraints(accumulation_threshold, rate_threshold)
+        accumulation_constraint, rate_constraint = self._construct_constraints(
+            accumulation_threshold, rate_threshold
+        )
 
         precip_accumulation = MergeCubes()(cubes.extract(accumulation_constraint))
         max_precip_rate = MergeCubes()(cubes.extract(rate_constraint))
@@ -190,11 +228,9 @@ class PrecipitationDuration(PostProcessingPlugin):
             )
 
         total_period = (
-            (
-                max_precip_rate.coord("time").cell(-1).bound[-1] -
-                max_precip_rate.coord("time").cell(0).bound[0]
-            ).total_seconds() / 3600
-        )
+            max_precip_rate.coord("time").cell(-1).bound[-1]
+            - max_precip_rate.coord("time").cell(0).bound[0]
+        ).total_seconds() / 3600
         if not total_period == self.target_period:
             raise ValueError(
                 "Input cubes do not combine to create the expected target "
@@ -202,16 +238,27 @@ class PrecipitationDuration(PostProcessingPlugin):
                 f"{total_period} hours."
             )
 
-        n_periods, = max_precip_rate.coord("time").shape
-        short_period_classifications = max_precip_rate.copy(data=max_precip_rate.data * precip_accumulation.data)
-        total_period = short_period_classifications.collapsed("time", analysis.SUM)
+        (n_periods,) = max_precip_rate.coord("time").shape
+        classifications = CubeList()
+        # Slice over all thresholds and combine to build all the classification
+        # combinations.
+        for acc_slice in precip_accumulation.slices_over(self.acc_threshold):
+            for rate_slice in max_precip_rate.slices_over(self.rate_threshold):
+                # Use the accumulation slice as a template and add the rate
+                # threshold coordinate so that a rate threshold coordinate can
+                # be reconstructed on merging the cube list. The cube will end
+                # up with two threshold coordinates.
+                rate_coord = rate_slice.coord(self.rate_threshold)
+                classified = acc_slice.copy(data=rate_slice.data * acc_slice.data)
+                classified.add_aux_coord(rate_coord)
+                classifications.append(classified)
+
+        classifications = classifications.merge_cube()
+        total_period = collapse_time(classifications, "time", analysis.SUM)
         total_period_data = total_period.data / n_periods
 
-        classification_fractions = self._create_output_cube(total_period_data, (total_period, precip_accumulation))
+        classification_fractions = self._create_output_cube(
+            total_period_data, (total_period, precip_accumulation)
+        )
 
         return classification_fractions
-
-
-
-
-
