828eccf2e1daab80abfec6a354df2415728c0010
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2025-01-31 15:54:53 +0000

    Percentile generation matches numpy method. Still the output looks very different.

--- improver/precipitation_type/precipitation_duration.py
+++ improver/precipitation_type/precipitation_duration.py
@@ -9,9 +9,11 @@ from typing import Optional, Tuple, Union
 
 import numpy as np
 from iris import Constraint, analysis
-from iris.coords import AuxCoord
+from iris.coords import AuxCoord, DimCoord
 from iris.cube import Cube, CubeList
+from iris.util import squeeze
 from numpy import ndarray
+import itertools
 
 from improver import PostProcessingPlugin
 from improver.metadata.probabilistic import (
@@ -22,7 +24,7 @@ from improver.metadata.utilities import (
     generate_mandatory_attributes,
 )
 from improver.utilities.common_input_handle import as_cubelist
-from improver.utilities.cube_manipulation import MergeCubes, collapse_time
+from improver.utilities.cube_manipulation import MergeCubes, collapse_time, enforce_coordinate_ordering
 
 
 class PrecipitationDuration(PostProcessingPlugin):
@@ -86,6 +88,7 @@ class PrecipitationDuration(PostProcessingPlugin):
         self.period = None
         self.acc_threshold = None
         self.rate_threshold = None
+        self.percentiles = np.array([5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 85, 90, 95])
 
     def _period_in_hours(self, cubes: CubeList) -> None:
         """Get the periods of the input cubes, check they are equal and
@@ -170,7 +173,7 @@ class PrecipitationDuration(PostProcessingPlugin):
         )
         return accumulation_constraint, rate_constraint
 
-    def _create_output_cube(self, data: ndarray, cubes: Tuple[Cube, Cube]) -> Cube:
+    def _create_output_cube(self, data: ndarray, cubes: Tuple[Cube, Cube], acc_thresh, rate_thresh) -> Cube:
         """Create an output cube for the final diagnostic, which is the
         fraction of the constructed period that has been classified as wet
         using the various accumulation and peak rate thresholds.
@@ -186,28 +189,43 @@ class PrecipitationDuration(PostProcessingPlugin):
             A Cube with a suitable name and metadata for the diagnostic data
             stored within it.
         """
-        classified_precip_cube = create_new_diagnostic_cube(
-            "fraction_of_period_classified_as_wet",
-            "1",
-            template_cube=cubes[0],
-            mandatory_attributes=generate_mandatory_attributes(
-                cubes,
-                model_id_attr=self.model_id_attr,
-            ),
-            optional_attributes={
-                "input_period_in_hours": self.period,
-            },
-            data=data,
+        percentile_coord = DimCoord(self.percentiles, long_name="percentile", units="%")
+
+        mandatory_attributes = generate_mandatory_attributes(
+            cubes,
+            model_id_attr=self.model_id_attr
         )
+        mandatory_attributes["input_period_in_hours"] = self.period
+
+        time_coords = [cubes[0].coord("forecast_reference_time").copy()]
+        for crd in ["time", "forecast_period"]:
+            time_crd = cubes[0].coord(crd).copy()
+            time_coords.append(time_crd.collapsed())
+
+        classification_percentiles = Cube(
+            data.astype(np.float32),
+            dim_coords_and_dims=[
+                (percentile_coord, 0),
+                (acc_thresh.copy(), 1),
+                (rate_thresh.copy(), 2),
+                (cubes[0].coord(axis="y"), 3),
+                (cubes[0].coord(axis="x"), 4),
+            ],
+            # aux_coords_and_dims=max_precip_rate.aux_coords,
+            units="1",
+            attributes=mandatory_attributes,
+        )
+        for crd in time_coords:
+            classification_percentiles.add_aux_coord(crd)
         # Remove any coordinate var names that identify a coordinate as a
         # threshold. We want to generate percentiles by collapsing the
         # realization coordinate, not attempting to use ECC for a probability
         # diagnostic.
-        for crd in classified_precip_cube.coords():
+        for crd in classification_percentiles.coords():
             if crd.var_name == "threshold":
                 crd.var_name = None
 
-        return classified_precip_cube
+        return squeeze(classification_percentiles)
 
     def process(self, *cubes: Union[Cube, CubeList]) -> Cube:
         """Produce a diagnostic that provides the fraction of the day that
@@ -267,34 +285,51 @@ class PrecipitationDuration(PostProcessingPlugin):
             )
 
         (n_periods,) = max_precip_rate.coord("time").shape
-        classifications = CubeList()
         # Slice over all thresholds and combine to build all the classification
         # combinations.
-        for acc_slice in precip_accumulation.slices_over(self.acc_threshold):
-            for rate_slice in max_precip_rate.slices_over(self.rate_threshold):
-                # Use the rate slice as a template and add the accumulation
-                # threshold coordinate so that an accumulation threshold
-                # coordinate can be reconstructed on merging the cube list.
-                # The cube will end up with two threshold coordinates.
-                acc_coord = acc_slice.coord(self.acc_threshold).copy()
-                # All thresholds on the final output are given relative to a
-                # 1-hour period.
-                acc_coord.points = np.around(acc_coord.points / self.period, decimals=6)
+        acc_thresh = precip_accumulation.coord(var_name="threshold")
+        rate_thresh = max_precip_rate.coord(var_name="threshold")
 
-                # We are working with binary probabilities, so make these bool
-                # type to reduce memory usage.
-                classified = rate_slice.copy(
-                    data=rate_slice.data.astype(bool) * acc_slice.data.astype(bool)
+        enforce_coordinate_ordering(precip_accumulation, ["realization", acc_thresh.name(), "time"])
+        enforce_coordinate_ordering(max_precip_rate, ["realization", rate_thresh.name(), "time"])
+
+        acc_len = acc_thresh.shape[0]
+        rate_len = rate_thresh.shape[0]
+        combinations = list(itertools.product(list(range(acc_len)), list(range(rate_len))))
+        realizations = list(range(max_precip_rate.coord("realization").shape[0]))
+
+        possible_values=list(range(0, n_periods + 1))
+        fractions = np.arange(0, 1.0001, 1/n_periods)
+
+        lookup_percentiles = self.percentiles * (len(realizations) - 1) / 100
+        generated_percentiles = np.empty((len(self.percentiles), acc_len, rate_len, *max_precip_rate.shape[-2:]))
+
+        for acc_index, rate_index in combinations:
+            hit_count = np.zeros((len(possible_values), *max_precip_rate.shape[-2:]))
+            for realization in realizations:
+                result = precip_accumulation[realization, acc_index].data.astype(bool) * max_precip_rate[realization, rate_index].data.astype(bool)
+                result = np.sum(result, axis=0)
+                for index, value in enumerate(possible_values):
+                    hit_count[index, result == value] += 1
+
+            cumulated = np.cumsum(hit_count, axis=0)
+            resulting_percentiles = []
+            for percentile in lookup_percentiles:
+                percentile_indices_lower = (cumulated <= np.floor(percentile)).sum(axis=0)
+                percentile_indices_upper = (cumulated <= np.ceil(percentile)).sum(axis=0)
+                interp_fraction = percentile - np.floor(percentile)
+
+                percentile_values = (
+                    fractions[percentile_indices_lower] + interp_fraction *
+                    (fractions[percentile_indices_upper] - fractions[percentile_indices_lower])
                 )
-                classified.add_aux_coord(acc_coord)
-                classifications.append(classified)
+                resulting_percentiles.append(percentile_values)
 
-        classifications = classifications.merge_cube()
-        total_period = collapse_time(classifications, "time", analysis.SUM)
-        total_period_data = total_period.data.astype(np.float32) / n_periods
+            resulting_percentiles = np.array(resulting_percentiles)
+            generated_percentiles[:, acc_index, rate_index] = resulting_percentiles
 
-        classification_fractions = self._create_output_cube(
-            total_period_data, (total_period, precip_accumulation)
+        # Need to make a cube of the right shape.
+        classification_percentiles = self._create_output_cube(
+            generated_percentiles, (max_precip_rate, precip_accumulation), acc_thresh, rate_thresh
         )
-
-        return classification_fractions
+        return classification_percentiles
