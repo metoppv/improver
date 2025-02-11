0654fe556463a8504855a0438b10e94d64a8506d
Belinda Trotta <belinda.trotta@bom.gov.au>
2025-02-11 19:15:59 +1100

    formatting

--- improver/calibration/beta_recalibration.py
+++ improver/calibration/beta_recalibration.py
@@ -14,11 +14,13 @@ from scipy.stats import beta
 
 from improver import PostProcessingPlugin
 from improver.metadata.probabilistic import is_probability
+
 """
 .. See the documentation for a more detailed discussion of this plugin.
 .. include:: extended_documentation/calibration/beta_recalibration/
     beta_recalibration.rst
 """
+
 
 class BetaRecalibrate(PostProcessingPlugin):
     """Recalibrate probabilities using the cumulative distribution function
