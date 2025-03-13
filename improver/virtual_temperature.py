f5940f4fbf96b2715cc5e252d32181f0fcca64dd
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2025-03-13 16:11:32 +0000

    updated PR with latest findings

--- improver/virtual_temperature.py
+++ improver/virtual_temperature.py
@@ -32,9 +32,8 @@ class VirtualTemperature(BasePlugin):
         # Calculate the virtual temperature
         virtual_temperature = temperature * (1 + 0.61 * humidity_mixing_ratio)
 
-        # Update the cube metadata including adding the required units (Kelvin)
+        # Update the cube metadata
         virtual_temperature.rename("virtual_temperature")
-        virtual_temperature.units = "K"
         virtual_temperature.attributes["units_metadata"] = "on-scale"
 
         return virtual_temperature
