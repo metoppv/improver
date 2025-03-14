00076f0b0c88e61eae32d261f249783abfcea70d
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2025-03-14 09:27:47 +0000

    debug 4

--- improver/virtual_temperature.py
+++ improver/virtual_temperature.py
@@ -34,7 +34,7 @@ class VirtualTemperature(BasePlugin):
 
         # Update the cube metadata
         virtual_temperature.rename("virtual_temperature")
-        virtual_temperature.attributes["units_metadata"] = "on-scale"
+        # virtual_temperature.attributes["units_metadata"] = "on-scale"
         print(virtual_temperature.units)
         print(virtual_temperature)
 
