56fadf1e80c4e9a7fdd5046d19946e92a640c797
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2025-03-14 15:08:40 +0000

    ensure virtual temperature calculation via copy

--- improver/virtual_temperature.py
+++ improver/virtual_temperature.py
@@ -30,11 +30,13 @@ class VirtualTemperature(BasePlugin):
             Cube of virtual_temperature.
         """
         # Calculate the virtual temperature
-        virtual_temperature = temperature * (1 + 0.61 * humidity_mixing_ratio)
+        #
+        virtual_temperature = temperature.copy(
+            temperature.data * (1 + 0.61 * humidity_mixing_ratio.data)
+        )
 
         # Update the cube metadata
         virtual_temperature.rename("virtual_temperature")
-        virtual_temperature.units("K")
         virtual_temperature.attributes["units_metadata"] = "on-scale"
 
         return virtual_temperature
