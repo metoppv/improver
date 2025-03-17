75edfa8a6faf4d0a522e0b0d0bd80da145ed9777
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2025-03-17 08:36:45 +0000

    remove copy in favour of scheduler processes

--- improver/virtual_temperature.py
+++ improver/virtual_temperature.py
@@ -30,10 +30,7 @@ class VirtualTemperature(BasePlugin):
             Cube of virtual_temperature.
         """
         # Calculate the virtual temperature
-        #
-        virtual_temperature = temperature.copy(
-            temperature.data * (1 + 0.61 * humidity_mixing_ratio.data)
-        )
+        virtual_temperature = temperature.data * (1 + 0.61 * humidity_mixing_ratio.data)
 
         # Update the cube metadata
         virtual_temperature.rename("virtual_temperature")
