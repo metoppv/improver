9cf993b4580c9e8a5ea278350de9c50a4a4c5ced
Marcus Spelman <marcus.spelman@metoffice.gov.uk>
2025-04-15 09:51:10 +0100

    Renaming some variables

--- improver_tests/precipitation/snow_probability_at_surface/test_snow_probability_at_surface.py
+++ improver_tests/precipitation/snow_probability_at_surface/test_snow_probability_at_surface.py
@@ -20,8 +20,8 @@ ATTRIBUTES = {
 
 
 @pytest.fixture()
-def wet_bulb_temperature():
-    """Create a test cube of wet bulb temperature at surface"""
+def wet_bulb_temperature_integral():
+    """Create a test cube of wet bulb temperature integral at surface"""
     data = np.full(
         (2, 3), 100, dtype=np.float32
     )  # dummy data that will be overriden in tests
@@ -39,11 +39,11 @@ def wet_bulb_temperature():
     "wb_temp_data , expected_probability",
     ((-3, 1), (0, 1), (56.25, 0.75), (112.5, 0.5), (168.75, 0.25), (225, 0), (255, 0)),
 )
-def test_scenarios(wet_bulb_temperature, wb_temp_data, expected_probability):
+def test_scenarios(wet_bulb_temperature_integral, wb_temp_data, expected_probability):
     """Test the snow probability at surface plugin for a range of input wet bulb integral temperatures."""
-    wet_bulb_temperature.data.fill(wb_temp_data)
+    wet_bulb_temperature_integral.data.fill(wb_temp_data)
 
-    result = SnowProbabilityAtSurface()(wet_bulb_temperature)
+    result = SnowProbabilityAtSurface()(wet_bulb_temperature_integral)
     assert np.all(result.data == expected_probability)
     assert result.name() == "probability_of_snow_at_surface"
     assert result.units == "1"
