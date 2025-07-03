20df59ac9cfac5ce98120b8812a40d708ff0b8b4
Gavin Evans <gavin.evans@metoffice.gov.uk>
2025-07-03 14:18:27 +0100

    Enforce input to cube.collapsed is the same type as the output.

--- improver/categorical/modal_code.py
+++ improver/categorical/modal_code.py
@@ -320,6 +320,7 @@ class ModalCategory(BaseModalCategory):
             result = cube
         else:
             result = cube.collapsed("time", self.aggregator_instance)
+            result.data = result.data.astype(cube.data.dtype)
         self._set_blended_times(result)
 
         result = self._prepare_result_cube(
