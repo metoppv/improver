9f67239f80b9ad53ad9c0f830114ffb8712f8dea
benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>
2025-08-29 12:30:21 +0100

    Add utility for extracting site list from a neighbour cube. This is useful as the neighbour cube has missing altitudes filled in from an orography, ensuring all of the definitions are complete.

--- improver/spotdata/utilities.py
+++ improver/spotdata/utilities.py
@@ -5,6 +5,8 @@
 
 """Spot data utilities."""
 
+from iris.cube import Cube
+
 
 def get_neighbour_finding_method_name(land_constraint: bool, minimum_dz: bool) -> str:
     """
@@ -21,3 +23,37 @@ def get_neighbour_finding_method_name(land_constraint: bool, minimum_dz: bool) -
         "_minimum_dz" if minimum_dz else "",
     )
     return method_name
+
+
+def extract_site_json(neighbours: Cube):
+    """
+    Extract the site definition JSON from a neighbour cube. This
+    produces a JSON file that can be used to recreate a neighbour cube,
+    for example on a different domain / projection.
+
+    Args:
+        neighbours:
+            A cube containing the neighbour information.
+
+    Returns:
+        A list of JSON dictionaries containing the site definitions.
+    """
+    neighbour_slice = next(neighbours.slices("spot_index"))
+    n_sites = neighbour_slice.shape[0]
+    site_specific_crds = [
+        crd for crd in neighbour_slice.aux_coords if crd.shape[0] == n_sites
+    ]
+    keys = [crd.name() for crd in site_specific_crds]
+
+    site_definitions = []
+    for site_index in range(neighbour_slice.shape[0]):
+        site_definition = {}
+        for crd in keys:
+            site_definition[crd] = neighbour_slice.coord(crd).points[site_index].item()
+            if site_definition[crd] == "None":
+                site_definition[crd] = None
+            if isinstance(site_definition[crd], str):
+                site_definition[crd] = int(site_definition[crd])
+        site_definitions.append(site_definition)
+
+    return site_definitions
--- improver_tests/spotdata/test_utilities.py
+++ improver_tests/spotdata/test_utilities.py
@@ -5,9 +5,14 @@
 
 """Unit tests for spot data utilities"""
 
+import numpy as np
 import pytest
 
-from improver.spotdata.utilities import get_neighbour_finding_method_name
+from improver.spotdata.build_spotdata_cube import build_spotdata_cube
+from improver.spotdata.utilities import (
+    extract_site_json,
+    get_neighbour_finding_method_name,
+)
 
 
 @pytest.mark.parametrize(
@@ -25,3 +30,57 @@ def test_get_neighbour_finding_method_name(land_constraint, minimum_dz, expected
 
     result = get_neighbour_finding_method_name(land_constraint, minimum_dz)
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "wmo_ids",
+    [
+        np.arange(4),
+        np.array(["00000", "00001", "00002", "00003"]),
+        np.array(["00000", "00001", "00002", "None"]),
+    ],
+)
+def test_extract_site_json(wmo_ids):
+    """Test the function for extracting site JSON from the neighbour cube."""
+
+    # Neighbour cube copied from spot_extraction tests
+    neighbours = np.array(
+        [
+            [[0.0, 0.0, 2.0, 2.0], [0.0, 0.0, 2.0, 2.0], [0.0, -1.0, 0.0, 1.0]],
+            [[1.0, 1.0, 2.0, 2.0], [1.0, 1.0, 2.0, 2.0], [-1.0, 0.0, 0.0, 1.0]],
+        ]
+    )
+    altitudes = np.array([0, 1, 3, 2])
+    latitudes = np.array([10, 10, 20, 20])
+    longitudes = np.array([10, 10, 20, 20])
+    unique_site_id = wmo_ids
+    unique_site_id_key = "met_office_site_id"
+    grid_attributes = ["x_index", "y_index", "vertical_displacement"]
+    neighbour_methods = ["nearest", "nearest_land"]
+    neighbour_cube = build_spotdata_cube(
+        neighbours,
+        "grid_neighbours",
+        1,
+        altitudes,
+        latitudes,
+        longitudes,
+        wmo_ids,
+        unique_site_id=unique_site_id,
+        unique_site_id_key=unique_site_id_key,
+        grid_attributes=grid_attributes,
+        neighbour_methods=neighbour_methods,
+    )
+
+    result = extract_site_json(neighbour_cube)
+
+    expected_wmo = []
+    for id in wmo_ids:
+        expected_wmo.append(int(id) if id != "None" else None)
+
+    assert isinstance(result, list)
+    assert all(isinstance(site, dict) for site in result)
+    assert [item["altitude"] for item in result] == altitudes.tolist()
+    assert [item["latitude"] for item in result] == latitudes.tolist()
+    assert [item["longitude"] for item in result] == longitudes.tolist()
+    assert [item["wmo_id"] for item in result] == expected_wmo
+    assert [item[unique_site_id_key] for item in result] == expected_wmo
