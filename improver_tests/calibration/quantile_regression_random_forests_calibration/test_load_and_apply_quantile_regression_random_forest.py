1d02dd9a475548fb8d1be63f0761c6065ab11a45
gavinevans <gavin.evans@metoffice.gov.uk>
2025-10-16 11:47:52 +0100

    Ensure consistent temporal dimensions for QRF (#2210)
    * Modifications to ensure that all inputs are rebadged with a consistent forecast_reference_time and forecast_period.
    
    * Add tests.
    
    * Amendments.
    
    * Edits following review.
    
    * Fix up the forecast period bounds setting in the qrf application script.
    
    * style fixes.
    
    * Add forecast_period parametrization to the load and apply qrf tests to test the route in which forecast_period is taken from the cubes rather than being provided. This captures the bug in the bounds setting which has now been addressed.
    
    ---------
    
    Co-authored-by: benjamin.ayliffe <benjamin.ayliffe@metoffice.gov.uk>

--- improver_tests/calibration/quantile_regression_random_forests_calibration/test_load_and_apply_quantile_regression_random_forest.py
+++ improver_tests/calibration/quantile_regression_random_forests_calibration/test_load_and_apply_quantile_regression_random_forest.py
@@ -5,6 +5,7 @@
 """Unit tests for the PrepareAndApplyQRF plugin."""
 
 import numpy as np
+import pandas as pd
 import pytest
 from iris.coords import AuxCoord
 from iris.cube import Cube, CubeList
@@ -105,6 +106,121 @@ def set_up_for_unexpected():
     )
 
 
+def set_up_for_expected(
+    feature_config,
+    n_estimators,
+    max_depth,
+    random_state,
+    transformation,
+    pre_transform_addition,
+    extra_kwargs,
+    include_dynamic,
+    include_static,
+    include_nans,
+    include_latlon_nans,
+    percentile_input,
+    site_id,
+    quantiles,
+    alt_cycletime=None,
+):
+    """Set up common elements for the tests where the expected test result will be
+    checked.
+
+    Args:
+        feature_config: Feature configuration dictionary.
+        n_estimators: Number of trees for the random forest.
+        max_depth: Maximum depth of each tree.
+        random_state: Random seed for reproducibility.
+        transformation: Transformation to be applied to the data before fitting.
+        pre_transform_addition: Value to be added before transformation.
+        extra_kwargs: Extra keyword arguments for the random forest.
+        include_dynamic: Whether to include an additional dynamic feature.
+        include_static: Whether to include an additional static feature.
+        include_nans: Whether to include NaNs in the input data.
+        include_latlon_nans: Whether to include NaNs in the latitude and longitude.
+        percentile_input: Whether the input forecast cube uses percentiles.
+        site_id: List of strings defining the unique site ID keys.
+        quantiles: List of quantiles to be predicted.
+        alt_cycletime: Alternative cycletime to be used for the dynamic feature.
+    """
+    if not alt_cycletime:
+        alt_cycletime = "20170103T0000Z"
+
+    qrf_model = _run_train_qrf(
+        feature_config,
+        n_estimators,
+        max_depth,
+        random_state,
+        transformation,
+        pre_transform_addition,
+        extra_kwargs,
+        include_static,
+        forecast_reference_times=[
+            "20170101T0000Z",
+            "20170102T0000Z",
+        ],
+        validity_times=[
+            "20170101T1200Z",
+            "20170102T1200Z",
+        ],
+        realization_data=[2, 6, 10],
+        truth_data=[4.2, 6.2, 4.1, 5.1],
+        site_id=site_id,
+    )
+
+    frt = "20170103T0000Z"
+    vt = "20170103T1200Z"
+    data = np.arange(6, (len(quantiles) * 6) + 1, 6)
+    day_of_training_period = 2
+    cube_inputs = CubeList()
+    forecast_cube = _create_forecasts(frt, vt, data, return_cube=True)
+
+    forecast_cube = _add_day_of_training_period_to_cube(
+        forecast_cube, day_of_training_period, "forecast_reference_time"
+    )
+
+    if percentile_input:
+        forecast_cube = RebadgeRealizationsAsPercentiles()(forecast_cube)
+
+    cube_inputs.append(forecast_cube)
+    if include_dynamic:
+        dynamic_cube = _create_forecasts(
+            alt_cycletime,
+            vt,
+            data + 0.5,  # Slightly different data to the target feature
+            return_cube=True,
+        )
+        dynamic_cube.rename("air_temperature")
+        dynamic_cube = _add_day_of_training_period_to_cube(
+            dynamic_cube, day_of_training_period, "forecast_reference_time"
+        )
+        if percentile_input:
+            dynamic_cube = RebadgeRealizationsAsPercentiles()(dynamic_cube)
+        cube_inputs.append(dynamic_cube)
+
+    if include_static:
+        ancil_cube = _create_ancil_file(return_cube=True)
+        cube_inputs.append(ancil_cube)
+
+    if include_nans:
+        # Add some NaNs to the input data to check that they are handled
+        for cube in cube_inputs:
+            if cube.name() == "distance_to_water":
+                cube.data[0] = np.nan
+            else:
+                cube.data[0, 0] = np.nan
+
+    if include_latlon_nans:
+        # Add some NaNs to the latitude and longitude to check that they are handled
+        for cube in cube_inputs:
+            cube.coord("latitude").points[1] = np.nan
+            cube.coord("longitude").points[1] = np.nan
+    return (
+        qrf_model,
+        cube_inputs,
+    )
+
+
 # Disable ruff formatting to keep the parameter combinations aligned for readability.
 # fmt: off
 @pytest.mark.parametrize("percentile_input", [True, False])
@@ -159,73 +275,11 @@ def test_prepare_and_apply_qrf(
     if include_static:
         feature_config["distance_to_water"] = ["static"]
 
-    qrf_model = _run_train_qrf(
-        feature_config,
-        n_estimators,
-        max_depth,
-        random_state,
-        transformation,
-        pre_transform_addition,
-        extra_kwargs,
-        include_static,
-        forecast_reference_times=[
-            "20170101T0000Z",
-            "20170102T0000Z",
-        ],
-        validity_times=[
-            "20170101T1200Z",
-            "20170102T1200Z",
-        ],
-        realization_data=[2, 6, 10],
-        truth_data=[4.2, 6.2, 4.1, 5.1],
-        site_id=site_id,
-    )
-
-    frt = "20170103T0000Z"
-    vt = "20170103T1200Z"
-    data = np.arange(6, (len(quantiles) * 6) + 1, 6)
-    day_of_training_period = 2
-    cube_inputs = CubeList()
-    forecast_cube = _create_forecasts(frt, vt, data, return_cube=True)
-
-    forecast_cube = _add_day_of_training_period_to_cube(
-        forecast_cube, day_of_training_period, "forecast_reference_time"
-    )
-
-    if percentile_input:
-        forecast_cube = RebadgeRealizationsAsPercentiles()(forecast_cube)
-
-    cube_inputs.append(forecast_cube)
-    if include_dynamic:
-        dynamic_cube = _create_forecasts(
-            frt,
-            vt,
-            data + 0.5,  # Slightly different data to the target feature
-            return_cube=True,
-        )
-        dynamic_cube.rename("air_temperature")
-        dynamic_cube = _add_day_of_training_period_to_cube(
-            dynamic_cube, day_of_training_period, "forecast_reference_time"
-        )
-        cube_inputs.append(dynamic_cube)
-
-    if include_static:
-        ancil_cube = _create_ancil_file(return_cube=True)
-        cube_inputs.append(ancil_cube)
-
-    if include_nans:
-        # Add some NaNs to the input data to check that they are handled
-        for cube in cube_inputs:
-            if cube.name() == "distance_to_water":
-                cube.data[0] = np.nan
-            else:
-                cube.data[0, 0] = np.nan
-
-    if include_latlon_nans:
-        # Add some NaNs to the latitude and longitude to check that they are handled
-        for cube in cube_inputs:
-            cube.coord("latitude").points[1] = np.nan
-            cube.coord("longitude").points[1] = np.nan
+    qrf_model, cube_inputs = set_up_for_expected(
+        feature_config, n_estimators, max_depth, random_state,
+        transformation, pre_transform_addition, extra_kwargs,
+        include_dynamic, include_static, include_nans, include_latlon_nans,
+        percentile_input, site_id, quantiles)
 
     result = PrepareAndApplyQRF(
         feature_config,
@@ -254,6 +308,88 @@ def test_prepare_and_apply_qrf(
         assert result.coords("realization")
         assert result.coord("realization").units == "1"
         assert np.allclose(result.coord("realization").points, range(len(quantiles)))
+
+@pytest.mark.parametrize("forecast_period", [None, 13])
+@pytest.mark.parametrize(
+    "n_estimators,max_depth,random_state,cycletime,include_dynamic,include_static,add_fp_bounds,expected",
+    [
+        (2, 2, 55, None, False, False, False, [4.1, 5.65]),  # Basic test case
+        (2, 2, 55, "20170103T0000Z", False, False, False, [4.1, 5.65]),  # Specify cycletime
+        (2, 2, 55, "20170102T2300Z", True, False, False, [4.1, 4.6]),  # Cycletime with dynamic feature
+        (2, 2, 55, "20170102T2300Z", False, True, False, [4.1, 5.65]),  # Cycletime with static feature
+        (2, 2, 55, "20170102T2300Z", True, True, False, [4.1, 4.6]),  # Cycletime with dynamic and static feature
+        (2, 2, 55, "20170102T2300Z", True, False, True, [4.1, 4.6]),  # Cycletime with dynamic feature and forecast period bounds
+    ],
+)
+def test_mismatching_temporal_coordinates(
+    forecast_period,
+    n_estimators,
+    max_depth,
+    random_state,
+    cycletime,
+    include_dynamic,
+    include_static,
+    add_fp_bounds,
+    expected
+):
+    """Test the PrepareAndApplyQRF plugin where the temporal coordinates i.e.
+    forecast_period or forecast_reference_time do not match between the different
+    features. In these tests, if a dynamic feature is present, the
+    cycletime of this feature will be different to that of the target feature.
+    This test modifies the cycletime (forecast_reference_time) and forecast period,
+    but these values are only used for merging the DataFrames within the plugin.
+    The forecast reference time and forecast period on the resulting cube,
+    are however, unchanged, because these are taken from the input target feature cube
+    without modification."""
+    feature_config = {"wind_speed_at_10m": ["mean", "std", "latitude", "longitude"]}
+    transformation = None
+    pre_transform_addition = 0
+    extra_kwargs = {}
+    include_nans = False
+    include_latlon_nans = False
+    percentile_input = False
+    site_id = ["wmo_id"]
+    quantiles = [0.5]
+    if not cycletime or cycletime == "20170103T0000Z":
+        forecast_period = 12 * 3600
+    elif forecast_period is not None:
+        forecast_period = forecast_period * 3600
+
+    if include_dynamic:
+        feature_config["air_temperature"] = ["mean", "std"]
+    if include_static:
+        feature_config["distance_to_water"] = ["static"]
+
+    qrf_model, cube_inputs = set_up_for_expected(
+        feature_config, n_estimators, max_depth, random_state,
+        transformation, pre_transform_addition, extra_kwargs,
+        include_dynamic, include_static, include_nans, include_latlon_nans,
+        percentile_input, site_id, quantiles, alt_cycletime=cycletime)
+
+    if add_fp_bounds:
+        fp_point = cube_inputs[0].coord("forecast_period").points[0]
+        cube_inputs[0].coord("forecast_period").bounds = [[fp_point - 3600, fp_point]]
+
+    result = PrepareAndApplyQRF(
+        feature_config,
+        "wind_speed_at_10m",
+        unique_site_id_keys=site_id,
+        cycletime=cycletime,
+        forecast_period=forecast_period,
+    )(cube_inputs, (qrf_model, transformation, pre_transform_addition))
+
+    assert isinstance(result, Cube)
+    assert result.data.shape == (len(quantiles), 2)
+    assert np.allclose(result.data, expected, rtol=1e-2)
+
+    # Check that the metadata is as expected
+    assert result.name() == "wind_speed_at_10m"
+    assert result.units == "m s-1"
+    assert result.coords("realization")
+    assert result.coord("realization").units == "1"
+    assert np.allclose(result.coord("realization").points, range(len(quantiles)))
+    assert result.coord("forecast_reference_time").cell(0) == pd.Timestamp("20170103T0000Z")
+    assert result.coord("forecast_period").points[0] == 12 * 3600
 
 
 def test_no_model_output(set_up_for_unexpected):
@@ -378,6 +514,41 @@ def test_missing_dynamic_feature(set_up_for_unexpected):
     plugin.feature_config = feature_config
     with pytest.raises(ValueError, match="The number of cubes loaded."):
         plugin(CubeList([forecast_cube]), qrf_descriptors=qrf_descriptors)
+
+
+def test_nonmatching_representation(set_up_for_unexpected):
+    """Test PrepareAndApplyQRF plugin behaviour when the input cubes contain a mix
+    of realization and percentile coordinates."""
+    (
+        qrf_model,
+        transformation,
+        pre_transform_addition,
+        cube_inputs,
+        forecast_cube,
+        ancil_cube,
+        plugin,
+    ) = set_up_for_unexpected
+
+    qrf_descriptors = (qrf_model, transformation, pre_transform_addition)
+    feature_config = {
+        "wind_speed_at_10m": ["mean", "std", "latitude", "longitude"],
+        "air_temperature": ["mean", "std"],
+    }
+    plugin.feature_config = feature_config
+    dynamic_cube = _create_forecasts(
+        "20170103T0000Z",
+        "20170103T1200Z",
+        np.arange(6.5, 12.5, 6),
+        return_cube=True,
+    )
+    dynamic_cube.rename("air_temperature")
+    dynamic_cube = _add_day_of_training_period_to_cube(
+        dynamic_cube, 2, "forecast_reference_time"
+    )
+    dynamic_cube = RebadgeRealizationsAsPercentiles()(dynamic_cube)
+    with pytest.raises(ValueError, match="The input cubes contain a mix of realization"):
+        plugin(CubeList([forecast_cube, dynamic_cube, ancil_cube]), qrf_descriptors=qrf_descriptors)
+
 
 def test_no_quantile_forest_package(set_up_for_unexpected):
     """Test PrepareAndApplyQRF plugin behaviour when the quantile_forest package is not installed."""
