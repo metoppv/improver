5a7aa3c601d76f500c927e65b96e9dfa81d1e1ce
Gavin Evans <gavin.evans@metoffice.gov.uk>
2025-11-11 10:43:08 +0000

    Minor edits.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,10 +62,7 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = [".rst", ".md"]
-# source_suffix = {
-#     ".rst": "restructuredtext",
-#     ".ipynb": "nbsphinx",
-# }
+source_suffix = ".rst"
 
 # The encoding of source files.
 #
