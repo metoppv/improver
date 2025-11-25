fb7bce71e6f9173b8a2b8001902641dc8c3bdbfc
Luke Hoffmann <luke.hoffmann@bom.gov.au>
2025-11-26 10:35:15 +1100

    Bump up test coverage

--- improver_tests/calibration/rainforests_training/test_CompileRainForestsModel.py
+++ improver_tests/calibration/rainforests_training/test_CompileRainForestsModel.py
@@ -3,6 +3,7 @@
 # This file is part of 'IMPROVER' and is released under the BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
 
+import shutil
 from pathlib import Path
 
 import pytest
@@ -40,3 +41,33 @@ def test_process(rainforests_model_files, tmp_path):
         compiler.process(model_file, output_dir)
 
         assert Path.exists(output_dir / f"{model_file.stem}.so")
+
+
+def test_process_wrong_extension(rainforests_model_files, tmp_path):
+    """Test models are not compiled when model file has unexpected extension."""
+
+    compiler = CompileRainForestsModel(parallel_comp=8)
+
+    output_dir = Path(tmp_path) / "compiled"
+    output_dir.mkdir(exist_ok=True)
+
+    model_file = Path(rainforests_model_files[0])
+    wrong_extension = model_file.with_suffix(".dat")
+
+    shutil.copyfile(model_file, wrong_extension)
+
+    with pytest.raises(ValueError):
+        compiler.process(wrong_extension, output_dir)
+
+
+def test_process_invalid_dir(rainforests_model_files, tmp_path):
+    """Test models are not compiled when output dir doesn't exist."""
+
+    compiler = CompileRainForestsModel(parallel_comp=8)
+
+    model_file = Path(rainforests_model_files[0])
+    missing_dir = Path(tmp_path) / "missing"
+    assert not missing_dir.is_dir()
+
+    with pytest.raises(ValueError):
+        compiler.process(model_file, missing_dir)
