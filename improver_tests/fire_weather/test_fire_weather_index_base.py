6c6a30c64d3be830a054c5b416b89d6b092ad624
Phil Relton <phil.relton@metoffice.gov.uk>
2025-12-17 16:53:46 +0000

    Removing merge conflict line

--- improver_tests/fire_weather/test_fire_weather_index_base.py
+++ improver_tests/fire_weather/test_fire_weather_index_base.py
@@ -2,10 +2,7 @@
 #
 # This file is part of 'IMPROVER' and is released under the BSD 3-Clause license.
 # See LICENSE in the root of the repository for full licensing details.
-<<<<<<< HEAD
 import warnings
-=======
->>>>>>> 14a9476d (Changes from ABC refactor)
 
 import numpy as np
 import pytest
