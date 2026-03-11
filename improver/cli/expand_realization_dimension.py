35fc7e87bfe261a4c44b616b2baa83097c43a209
Max White <max.white@metoffice.gov.uk>
2026-03-11 14:49:48 +0000

    Add CLI

---
+++ improver/cli/expand_realization_dimension.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of 'IMPROVER' and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
+"""Script to collapse the realizations dimension of a cube."""
+
+from improver import cli
+
+
+@cli.clizefy
+@cli.with_output
+def process(cube: cli.inputcube, n_realizations_required: int):
+    """Expand the realization dimension of a cube.
+
+    Args:
+        cube:
+            Cube to be expanded.
+        n_realizations_required:
+            Number of realizations required in the expanded cube.
+
+    Returns:
+        iris.cube.Cube
+            Expanded cube. Dimensions are the same as input cube, with the realization
+            dimension expanded to the specified size.
+
+    Raises:
+        ValueError: if realization is not a dimension coordinate.
+    """
+    if not (cube.coords("realization", dim_coords=True)):
+        raise ValueError("realization must be a dimension coordinate.")
+
+    from improver.utilities.expand_realization_dimension import (
+        ExpandRealizationDimension,
+    )
+
+    plugin = ExpandRealizationDimension(n_realizations_required=n_realizations_required)
+
+    return plugin.process(cube)
