6d4918b9d64794db9dd45167d76406e12405428e
Max White <max.white@metoffice.gov.uk>
2026-03-11 14:50:23 +0000

    Add tests

---
+++ improver_tests/acceptance/test_expand_realization_dimension.py
@@ -0,0 +1,52 @@
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of 'IMPROVER' and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
+"""Tests for the expand-realization-dimension CLI"""
+
+import pytest
+
+from . import acceptance as acc
+
+pytestmark = [pytest.mark.acc, acc.skip_if_kgo_missing]
+CLI = acc.cli_name_with_dashes(__file__)
+run_cli = acc.run_cli(CLI)
+
+
+def test_basic(tmp_path):
+    """
+    Test expand realization dimension.
+    """
+
+    kgo_dir = acc.kgo_root() / "expand-realization-dimension"
+    kgo_path = kgo_dir / "kgo.nc"
+    input_path = kgo_dir / "input.nc"
+    output_path = tmp_path / "output.nc"
+    args = [
+        input_path,
+        "--n-realizations-required",
+        "24",
+        "--output",
+        output_path,
+    ]
+    run_cli(args)
+    acc.compare(output_path, kgo_path)
+
+
+def test_no_realization_coord(tmp_path):
+    """
+    Test that an error is raised if there is no realization dimension.
+    """
+
+    kgo_dir = acc.kgo_root() / "expand-realization-dimension"
+    input_path = kgo_dir / "input_no_realization.nc"
+    output_path = tmp_path / "output.nc"
+    args = [
+        input_path,
+        "--n-realizations-required",
+        "24",
+        "--output",
+        output_path,
+    ]
+    with pytest.raises(ValueError):
+        run_cli(args)
