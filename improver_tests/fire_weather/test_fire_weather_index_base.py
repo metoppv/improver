b9e821674abd84c040b2312bb4b2889ffc55c49f
ryan-cocking-mo <ryan.cocking@metoffice.gov.uk>
2026-03-30 13:54:41 +0100

    reduce whitespace in pytest parameterise

--- improver_tests/fire_weather/test_fire_weather_index_base.py
+++ improver_tests/fire_weather/test_fire_weather_index_base.py
@@ -691,22 +691,10 @@ def test_process_unpacked_cubes_and_kwargs() -> None:
 @pytest.mark.parametrize(
     "cubes, expected_match",
     [
-        (
-            (None, None),
-            r"Object .* cannot be put in a cubelist, as it is not a Cube.",
-        ),
-        (
-            ("Foobar", "Spam"),
-            r"Object .* cannot be put in a cubelist, as it is not a Cube.",
-        ),
-        (
-            (Cube,),
-            r"Expected .* cubes, found 1",
-        ),
-        (
-            (),
-            r"One or more cubes should be provided.",
-        ),
+        ((None, None), r"as it is not a Cube."),
+        (("Foobar", "Spam"), r"as it is not a Cube."),
+        ((Cube,), r"Expected .* cubes, found 1"),
+        ((), r"One or more cubes should be provided."),
     ],
 )
 def test_process_invalid_cubes_raises_error(cubes, expected_match: str) -> None:
