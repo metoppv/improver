00721f7302bba17d5f2706cd8c87c5554dc5e434
Luke Hoffmann <luke.hoffmann@bom.gov.au>
2026-06-22 14:53:30 +0930

    Change RainForestsModelConfig to a class

--- improver/calibration/rainforest_model_config.py
+++ improver/calibration/rainforest_model_config.py
@@ -1,43 +1,48 @@
-from typing import Literal, TypeAlias
+from typing import Literal
 
-"""Define the config structure used to describe the high-level RainForests model structure;
 
-RainForestsModelConfig dictionary is of format:
-
-.. code-block:: json
-
-    {
-    "24": {
-        "0.000010": {
-            "lightgbm_model": "<path_to_lightgbm_model_object>",
-            "treelite_model": "<path_to_treelite_model_object>"
-        },
-        "0.000050": {
-            "lightgbm_model": "<path_to_lightgbm_model_object>",
-            "treelite_model": "<path_to_treelite_model_object>"
-        },
-        "0.000100": {
-            "lightgbm_model": "<path_to_lightgbm_model_object>",
-            "treelite_model": "<path_to_treelite_model_object>"
-        },
-    }
-
-- top level key describes the lead-hour,
-- next level key describes the threshold,
-- corresponding values locate the associated model file.
-
-"""
-
-# Definition for the model config object, and json file
-RainForestsModelConfig: TypeAlias = dict[
-    str,  # lead time in hours, (integer converted to str)
+class RainForestsModelConfig(
     dict[
-        str,  # threshold in metres (formatted to "08.6f")
+        # lead time in hours, (integer converted to str)
+        str,
         dict[
-            Literal[
-                "lightgbm_model", "treelite_model"
-            ],  # literal "lightgbm_model" or "treelite_model"
-            str,  # path to output file
+            # threshold in metres (formatted to "08.6f")
+            str,
+            dict[
+                # literal "lightgbm_model" or "treelite_model"
+                Literal["lightgbm_model", "treelite_model"],
+                # path to output file
+                str,
+            ],
         ],
-    ],
-]
+    ]
+):
+    """Define the config structure used to describe the high-level RainForests model structure.
+
+    RainForestsModelConfig is a nested dictionar of format:
+
+    .. code-block:: json
+
+        {
+        "24": {
+            "0.000010": {
+                "lightgbm_model": "<path_to_lightgbm_model_object>",
+                "treelite_model": "<path_to_treelite_model_object>"
+            },
+            "0.000050": {
+                "lightgbm_model": "<path_to_lightgbm_model_object>",
+                "treelite_model": "<path_to_treelite_model_object>"
+            },
+            "0.000100": {
+                "lightgbm_model": "<path_to_lightgbm_model_object>",
+                "treelite_model": "<path_to_treelite_model_object>"
+            },
+        }
+
+    - top level key describes the lead-hour,
+    - next level key describes the threshold,
+    - corresponding values locate the associated model file.
+    """
+
+    def __init__(self, *arg, **kw):
+        super(RainForestsModelConfig, self).__init__(*arg, **kw)
